84ab581d1b9cae257fb64b56a904760e746ca714
Pradel Léo <pradel.leo@gmail.com>
2019-03-08 16:12:54 +0100

    Updates for blockstack.js 19

---
+++ src/utils/blockstack.ts
@@ -0,0 +1,5 @@
+import { AppConfig, UserSession } from 'blockstack';
+
+export const appConfig = new AppConfig(['store_write', 'publish_data']);
+
+export const userSession = new UserSession(appConfig);
