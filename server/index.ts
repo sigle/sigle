2b1ba0cbbeac9d48710d2c2fac02765cef5dd259
pradel <pradel.leo@gmail.com>
2019-03-29 17:27:50 +0100

    Use express to serve the app

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
-import { createServer } from 'http';
-import { parse } from 'url';
+import express from 'express';
 import next from 'next';
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
@@ -8,18 +7,26 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    const { pathname, query } = parsedUrl;
+  const server = express();
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query);
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(port, () => {
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/a', req.query);
+  });
+
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/b', req.query);
+  });
+
+  server.get('/posts/:id', (req, res) => {
+    return app.render(req, res, '/posts', { id: req.params.id });
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err?: Error) => {
+    if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
