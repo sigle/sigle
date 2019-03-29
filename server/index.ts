b76d943d5b54e9888771bbb8a3e2f31e42bd7cc3
pradel <pradel.leo@gmail.com>
2019-03-29 15:50:49 +0100

    Setup next.js with custom server and typescript

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query);
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
