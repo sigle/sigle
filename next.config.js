3304209e72d5c0d2fb1edc61b55bb6871c1e2581
pradel <pradel.leo@gmail.com>
2019-03-29 16:33:02 +0100

    @zeit/next-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+const withPlugins = require('next-compose-plugins');
 const withTypescript = require('@zeit/next-typescript');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
-module.exports = withTypescript();
+module.exports = withPlugins([
+  [withTypescript],
+  [
+    withBundleAnalyzer,
+    {
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/server.html',
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/client.html',
+        },
+      },
+    },
+  ],
+]);
