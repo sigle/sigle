faa59684955ff506af3b715a92cdfe1e39eccfba
Pradel Léo <pradel.leo@gmail.com>
2019-04-07 15:48:06 +0200

    Landing hero

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,41 +1,75 @@
 import React from 'react';
-import Link from 'next/link';
 import styled from 'styled-components';
-import { Box, Heading } from 'rebass';
-import { Button, Container } from '../components';
 import tw from 'tailwind.macro';
+import { Button, Container } from '../components';
+import { Header } from '../modules/layout/components/Header';
 
-let Test = tw.div`bg-blue-500 text-white p-2`;
+const Hero = styled.div`
+  background-color: #fcf8f3;
+`;
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
+const HeroContainer = styled.div`
+  ${tw`flex flex-wrap items-center pt-16 pb-16 flex-row-reverse`};
+
+  .block {
+    ${tw`w-full lg:w-1/2`};
+  }
+
+  .article-container {
+    ${tw`mx-auto`};
+    max-width: 30rem;
+  }
+
+  .title {
+    ${tw`text-4xl leading-snug mb-8 font-baskerville`};
+  }
+
+  .text {
+    ${tw`mb-2`};
+  }
+
+  .button {
+    border-radius: 2rem;
+  }
+
+  .figure-container {
+    ${tw`flex item-center justify-center`};
+  }
+
+  img {
+    width: 460px;
+    height: 100%;
+  }
 `;
 
 export const Home = () => (
   <React.Fragment>
-    <Test>Yo</Test>
-    <ul>
-      <li>
-        <Link href="/a" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/b" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-      <li>
-        <Title>Heyo</Title>
-      </li>
-    </ul>
-    <Container>
-      <Heading>Button</Heading>
-      <Button>Button</Button>
-      <Button color="black">Button</Button>
-      <Button color="primary">Button</Button>
-      <Button variant="outline">Button</Button>
-    </Container>
+    <Hero>
+      <Header />
+      <Container>
+        <HeroContainer>
+          <figure className="block">
+            <div className="figure-container">
+              <img src="/static/images/work.png" alt="Julia working" />
+            </div>
+          </figure>
+          <article className="block">
+            <div className="article-container">
+              <h2 className="title">
+                A decentralized & open source platform for writers
+              </h2>
+              <p className="text">
+                Write, save, share. Easy... and free.
+                <br />
+                Sign in and start writting
+              </p>
+              <Button color="primary" className="button">
+                Try it now
+              </Button>
+            </div>
+          </article>
+        </HeroContainer>
+      </Container>
+    </Hero>
   </React.Fragment>
 );
