f4efb7e4f40e947cf57c082b36937b92658efe78
Pradel Léo <pradel.leo@gmail.com>
2019-04-17 23:31:42 +0200

    Start me page

---
+++ client/pages/Me.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import tw from 'tailwind.macro';
+import { Button, Container, Link } from '../components';
+import { Header } from '../modules/layout/components/Header';
+import { Footer } from '../modules/layout/components/Footer';
+
+// TODO active style
+const MeMenu = styled.ul`
+  ${tw`flex justify-around`};
+
+  a {
+    ${tw`py-2 px-2 block`};
+  }
+`;
+
+const MeProfile = styled.div`
+  img {
+    ${tw`w-20 h-20 rounded-full mb-2`};
+  }
+
+  h2 {
+    ${tw`text-xl`};
+  }
+
+  p {
+    ${tw`lg:text-sm`};
+  }
+`;
+
+// TODO protect this page, user needs to be connected
+export const Me = () => (
+  <React.Fragment>
+    <Header />
+    <Container>
+      <MeMenu>
+        <li>
+          <Link href="/me">My stories</Link>
+        </li>
+        <li>
+          <Link href="/me/stats">Stats</Link>
+        </li>
+        <li>
+          <Link href="/me/settings">Settings</Link>
+        </li>
+      </MeMenu>
+
+      <MeProfile>
+        <img src="https://source.unsplash.com/random/100x100" alt="TODO" />
+        <h2>John Doe</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis
+          accumsan arcu.
+        </p>
+      </MeProfile>
+    </Container>
+    <Footer />
+  </React.Fragment>
+);
---
+++ pages/me.tsx
@@ -0,0 +1,3 @@
+import { Me } from '../client/pages/Me';
+
+export default Me;
