2d0f2fe4f65484fd44f5502b04646188b9303b92
Pradel Léo <pradel.leo@gmail.com>
2019-06-10 15:38:04 +0200

    Fix link in Editor

--- client/pages/Editor.tsx
+++ client/pages/Editor.tsx
@@ -24,7 +24,7 @@ export const Editor = (props: Props) => {
     <React.Fragment>
       <Header />
       <Container>
-        <StyledLink href="/">
+        <StyledLink href="/me">
           <StyledArrowBack /> Back to my stories
         </StyledLink>
         <SigleEditor storyId={props.storyId} />
