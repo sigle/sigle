3d68e3503ffa6a7d3c82e58925d8ba2178aece60
Pradel Léo <pradel.leo@gmail.com>
2019-06-23 17:26:10 +0200

    Link page discover

--- client/pages/Discover.tsx
+++ client/pages/Discover.tsx
@@ -1,78 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
+import { graphql } from 'react-relay';
+import { withData } from '../lib/withData';
 import { Container } from '../components';
 import { Header } from '../modules/layout/components/Header';
 import { Footer } from '../modules/layout/components/Footer';
-
-const story = {
-  title: 'Can coffee make you a better developer?',
-  content:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis accumsan arcu. Sed diam tellus, sollicitudin quis leo consequat, efficitur mattis ex. Sed sit amet volutpat ipsum, ut consequat mauris. Mauris tellus sapien, varius et vestibulum sit amet ...',
-};
-
-const StoryContainer = styled.div`
-  ${tw`w-full lg:flex py-4 border-b border-grey`};
-  &:last-child {
-    border: none;
-  }
-`;
-
-const StoryCover = styled.div`
-  ${tw`h-48 lg:h-auto lg:w-64 flex-none bg-cover bg-center overflow-hidden mb-4 lg:mb-0 lg:mr-4`};
-  background-image: url('https://source.unsplash.com/random/256x183');
-`;
-
-const StoryContent = styled.div`
-  ${tw`flex flex-col justify-between leading-normal`};
-`;
-
-const StoryTitle = styled.div`
-  ${tw`text-xl font-medium`};
-`;
-
-const StoryDate = styled.div`
-  ${tw`text-grey-darker mb-2 text-sm`};
-`;
-
-const StoryText = styled.div`
-  ${tw`mb-2 lg:text-sm`};
-`;
-
-const StoryTags = styled.div`
-  ${tw`text-grey-darker font-light text-sm italic`};
-`;
-
-const StoryProfile = styled.div`
-  ${tw`flex items-center`};
-`;
-
-const StoryProfileImage = styled.img`
-  ${tw`w-8 h-8 rounded-full mr-2`};
-`;
-
-const StoryProfileName = styled.div`
-  ${tw`text-sm`};
-`;
-
-export const Story = () => (
-  <StoryContainer>
-    <StoryCover />
-    <StoryContent>
-      <StoryTitle>{story.title}</StoryTitle>
-      <StoryDate>January 26, 2017</StoryDate>
-      <StoryText>{story.content}</StoryText>
-      <StoryTags>Travel, lifestyle</StoryTags>
-      <StoryProfile>
-        <StoryProfileImage
-          alt="Profile image of TODO"
-          src="https://source.unsplash.com/random/100x100"
-        />
-        <StoryProfileName>John Doe</StoryProfileName>
-      </StoryProfile>
-    </StoryContent>
-  </StoryContainer>
-);
+import { DiscoverQueryResponse } from './__generated__/DiscoverQuery.graphql';
+import { PublicStoryItem } from '../modules/publicStories/components/PublicStoryItem';
 
 const DiscoverContainer = styled(Container)`
   ${tw`py-4`};
@@ -82,21 +17,36 @@ const DiscoverTitle = styled.h1`
   ${tw`text-lg mb-4`};
 `;
 
-export const Discover = () => {
+interface Props extends DiscoverQueryResponse {}
+
+export const DiscoverComponent = ({ publicStories }: Props) => {
   return (
     <React.Fragment>
       <Header />
       <DiscoverContainer>
         <DiscoverTitle>Discover the latest stories</DiscoverTitle>
 
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
+        {publicStories!.edges!.map(data => (
+          <PublicStoryItem key={data!.node!.id} story={data!.node!} />
+        ))}
       </DiscoverContainer>
       <Footer />
     </React.Fragment>
   );
 };
+
+export const Discover = withData(DiscoverComponent, {
+  query: graphql`
+    query DiscoverQuery {
+      publicStories {
+        totalCount
+        edges {
+          node {
+            id
+            ...PublicStoryItem_story
+          }
+        }
+      }
+    }
+  `,
+});
