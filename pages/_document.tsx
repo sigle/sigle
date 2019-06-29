cec954eccd704d5f3c8dd2d28dceb297a85e42f3
Pradel Léo <pradel.leo@gmail.com>
2019-06-29 15:40:19 +0200

    Change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -68,7 +68,6 @@ export default class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicon-16x16.png"
           />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
           <link
