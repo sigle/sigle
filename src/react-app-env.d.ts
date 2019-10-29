f4008bbed30a3362dd00bafcd399b4630d0cf250
pradel <pradel.leo@gmail.com>
2019-10-29 16:22:05 +0100

    Update react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="react-scripts" />
 
-declare module 'the-platform';
 declare module 'tailwind.macro' {
   function tailwindMacro(param: TemplateStringsArray);
   export = tailwindMacro;
