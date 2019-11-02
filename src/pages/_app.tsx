769df719969cb8bf72566f0b16e00c434672cce5
Pradel Léo <pradel.leo@gmail.com>
2019-11-02 12:50:51 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,16 @@ import React, { useEffect } from 'react';
 import App from 'next/app';
 import Router from 'next/router';
 import Fathom from 'fathom-client';
+import { createGlobalStyle } from 'styled-components';
 import '../generated/tailwind.css';
 import { config } from '../config';
 
-Router.events.on('routeChangeComplete', () => {
-  Fathom.trackPageview();
-});
+/**
+ * Fathom
+ */
 
-const Layout: any = (props: any) => {
+// Track when page is loaded
+const FathomTrack = () => {
   useEffect(() => {
     if (config.fathomSiteId) {
       Fathom.load();
@@ -18,16 +20,33 @@ const Layout: any = (props: any) => {
     }
   }, []);
 
-  return <React.Fragment {...props} />;
+  return <React.Fragment />;
 };
+
+// Track on each page change
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
+/**
+ * Global style
+ */
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
+      <React.Fragment>
+        <GlobalStyle />
+        <FathomTrack />
         <Component {...pageProps} />
-      </Layout>
+      </React.Fragment>
     );
   }
 }
