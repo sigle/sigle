9f824bc19ca4c293e02f54bfafadfd5852db8a82
Pradel Léo <pradel.leo@gmail.com>
2019-11-03 10:12:16 +0100

    Bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,13 +1,38 @@
 const dotenv = require('dotenv');
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
 dotenv.config();
 
-module.exports = withPlugins([[withCSS]], {
-  env: {
-    APP_URL: process.env.APP_URL || 'http://localhost:3000',
-    SENTRY_DSN_CLIENT: process.env.SENTRY_DSN_CLIENT,
-    FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
-  },
-});
+module.exports = withPlugins(
+  [
+    [withCSS],
+    [
+      withBundleAnalyzer,
+      {
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(
+          process.env.BUNDLE_ANALYZE
+        ),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/server.html',
+          },
+          browser: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/client.html',
+          },
+        },
+      },
+    ],
+  ],
+  {
+    env: {
+      APP_URL: process.env.APP_URL || 'http://localhost:3000',
+      SENTRY_DSN_CLIENT: process.env.SENTRY_DSN_CLIENT,
+      FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
+    },
+  }
+);
