80cf667c8ec7d5b65cfa5841d0047db5f9b205c6
Pradel Léo <pradel.leo@gmail.com>
2019-11-03 08:53:18 +0100

    delete some files

--- src/react-app-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module 'tailwind.macro' {
-  function tailwindMacro(param: TemplateStringsArray);
-  export = tailwindMacro;
-}
-declare module 'slate-soft-break';
