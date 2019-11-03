5c47b2dcc9702f9f21daaa4b1c9bd13a01ca1a50
Pradel Léo <pradel.leo@gmail.com>
2019-11-03 11:27:13 +0100

    Defqult seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import App from 'next/app';
 import Router from 'next/router';
 import Fathom from 'fathom-client';
 import { createGlobalStyle } from 'styled-components';
+import { DefaultSeo } from 'next-seo';
 import { ToastContainer } from 'react-toastify';
 // TODO add tippy.js only on the pages that are using it
 import 'tippy.js/dist/tippy.css';
@@ -46,8 +47,23 @@ const GlobalStyle = createGlobalStyle`
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+    const seoTitle = 'Sigle | Decentralized blogging platform';
+    const seoDescription =
+      'A secure, decentralized and open source blogging platform on top of blockstack';
+
     return (
       <React.Fragment>
+        <DefaultSeo
+          title={seoTitle}
+          description={seoDescription}
+          openGraph={{
+            type: 'website',
+            title: seoTitle,
+            description: seoDescription,
+            images: [{ url: `${config.appUrl}/static/images/share.jpg` }],
+          }}
+          twitter={{ site: '@sigleapp', cardType: 'summary_large_image' }}
+        />
         <GlobalStyle />
         <FathomTrack />
         <Component {...pageProps} />
