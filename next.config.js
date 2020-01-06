9cbf18d98c64fa8e439563e3f1b07f36aa36dbdf
Pradel Léo <pradel.leo@gmail.com>
2020-01-06 22:25:27 +0100

    Decrease bundle size by excluding BIP39 non-english json file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const webpack = require('webpack');
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
@@ -33,6 +34,14 @@ module.exports = withPlugins(
       APP_URL: process.env.APP_URL || 'http://localhost:3000',
       SENTRY_DSN_CLIENT: process.env.SENTRY_DSN_CLIENT,
       FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
+    },
+    webpack: config => {
+      // See https://github.com/blockstack/blockstack.js/pull/683
+      // BIP39 includes ~240KB of non-english json that we don't currently use.
+      config.plugins.push(
+        new webpack.IgnorePlugin(/\.\/wordlists\/(?!english\.json)/)
+      );
+      return config;
     },
   }
 );
