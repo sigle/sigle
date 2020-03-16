33b07daa59717ae33d5f6eeb4af2fdc79f815b66
pradel <pradel.leo@gmail.com>
2020-03-16 16:46:50 +0100

    lint

--- src/modules/settings/SettingsForm.tsx
+++ src/modules/settings/SettingsForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 import { toast } from 'react-toastify';
