f898ff48c22ecb00d23bde396e9d1128c3415405
pradel <pradel.leo@gmail.com>
2020-03-27 15:39:45 +0100

    feat: add rss feed

---
+++ src/pages/api/feed/[username].ts
@@ -0,0 +1,1 @@
+export { apiFeed as default } from '../../../pages-lib/api/feed/[username]';
---
+++ src/utils/migrations/stories.ts
@@ -0,0 +1,11 @@
+import { StoryFile } from '../../types';
+
+export const migrationStories = (
+  file?: StoryFile | null | undefined
+): StoryFile => {
+  // File do not exist on the storage at all
+  if (!file) {
+    return { stories: [] };
+  }
+  return file;
+};
