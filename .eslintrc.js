9f70a4016c4d053feed31f26945ed978f0c338a9
pradel <pradel.leo@gmail.com>
2020-06-13 16:54:52 +0200

    disable @typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 2,
     '@typescript-eslint/no-use-before-define': 'off',
   },
