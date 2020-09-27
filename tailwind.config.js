1ab9907043b218f92c93afeabea7792e77dadd51
pradel <pradel.leo@gmail.com>
2020-09-27 23:20:24 +0200

    chore: use tailwind extend feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,24 @@
-const defaultConfig = require('tailwindcss/stubs/defaultConfig.stub');
-
 module.exports = {
-  ...defaultConfig,
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
   theme: {
-    ...defaultConfig.theme,
-    fontFamily: {
-      ...defaultConfig.theme.fontFamily,
-      roboto: "'Roboto', sans-serif",
-      baskerville: "'Libre Baskerville', serif",
-    },
-    colors: {
-      ...defaultConfig.theme.colors,
-      grey: {
-        ...defaultConfig.theme.colors.gray,
-        darker: '#838383',
-        dark: '#bbbaba',
-        default: '#ededed',
-        light: '#f7f7f7',
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto', sans-serif",
+        baskerville: "'Libre Baskerville', serif",
       },
-      pink: {
-        default: '#ff576a',
+      colors: {
+        grey: {
+          darker: '#838383',
+          dark: '#bbbaba',
+          default: '#ededed',
+          light: '#f7f7f7',
+        },
+        pink: {
+          default: '#ff576a',
+        },
       },
     },
   },
