41b5a9d249092c5b35ef990538254e43c747266c
pradel <pradel.leo@gmail.com>
2020-10-05 15:12:15 +0200

    upgrade slate and fix autoscroll

--- src/modules/editor/components/NewEditor.tsx
+++ src/modules/editor/components/NewEditor.tsx
@@ -133,6 +133,34 @@ export const NewEditor = () => {
             }
           }
         }}
+        onSelect={() => {
+          // Currently autoscroll is broken in slate, see https://github.com/ianstormtaylor/slate/issues/3750
+          /**
+           * Chrome doesn't scroll at bottom of the page. This fixes that.
+           */
+          if (!(window as any).chrome) return;
+          if (editor.selection == null) return;
+          try {
+            /**
+             * Need a try/catch because sometimes you get an error like:
+             *
+             * Error: Cannot resolve a DOM node from Slate node: {"type":"p","children":[{"text":"","by":-1,"at":-1}]}
+             */
+            const domPoint = ReactEditor.toDOMPoint(
+              editor,
+              editor.selection.focus
+            );
+            const node = domPoint[0];
+            if (node == null) return;
+            const element = node.parentElement;
+            if (element == null) return;
+            element.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+          } catch (e) {
+            /**
+             * Empty catch. Do nothing if there is an error.
+             */
+          }
+        }}
       />
     </Slate>
   );
