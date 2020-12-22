f0a819e608b5b8b9194dff20626df6bd950400b0
pradel <pradel.leo@gmail.com>
2020-12-22 17:17:04 +0100

    fix types

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -5,20 +5,15 @@ import { sigleConfig } from '../config';
 export const initSentry = () => {
   if (sigleConfig.sentryDsn) {
     const integrations = [];
-    if (
-      process.env.NEXT_IS_SERVER === 'true' &&
-      process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR
-    ) {
+    const nextRootDir = process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR;
+    if (process.env.NEXT_IS_SERVER === 'true' && nextRootDir) {
       // For Node.js, rewrite Error.stack to use relative paths, so that source
       // maps starting with ~/_next map to files in Error.stack with path
       // app:///_next
       integrations.push(
         new RewriteFrames({
           iteratee: (frame) => {
-            frame.filename = frame.filename?.replace(
-              process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR!,
-              'app:///'
-            );
+            frame.filename = frame.filename?.replace(nextRootDir, 'app:///');
             frame.filename = frame.filename?.replace('.next', '_next');
             return frame;
           },
