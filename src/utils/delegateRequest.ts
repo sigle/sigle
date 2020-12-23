8bf38b7842a1254c359ccbe8ab19de96110e34fd
pradel <pradel.leo@gmail.com>
2020-12-23 17:45:11 +0100

    fix lint

--- src/utils/delegateRequest.ts
+++ src/utils/delegateRequest.ts
@@ -10,7 +10,7 @@ export const delegateRequest = async (
   data: Record<string, any>
 ) => {
   const message = JSON.stringify(data);
-  var options = {
+  const options = {
     hostname: host,
     method: 'POST',
     path: path,
@@ -20,7 +20,7 @@ export const delegateRequest = async (
     },
   };
   await new Promise<void>((resolve, reject) => {
-    let req = https.request(options);
+    const req = https.request(options);
     req.on('error', (e) => {
       console.error(`problem with request: ${e.message}`);
       reject(e);
