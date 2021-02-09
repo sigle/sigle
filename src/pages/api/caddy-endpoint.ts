6002245e8be266a69a16088b214e72676188b7b6
pradel <pradel.leo@gmail.com>
2021-02-09 17:33:09 +0100

    chore: restrict allowed custom domain for testing

--- src/pages/api/caddy-endpoint.ts
+++ src/pages/api/caddy-endpoint.ts
@@ -1,10 +1,23 @@
 import { NextApiHandler } from 'next';
 
-const caddyEndpoint: NextApiHandler = async (req, res) => {
-  console.log(req.headers);
+const allowedDomains = new Set([
+  'test.leopradel.com',
+  'sigle-blog.leopradel.com',
+]);
 
-  // TODO validate domain against db
-  res.status(200).json(true);
+const caddyEndpoint: NextApiHandler = async (req, res) => {
+  const domain = req.query.domain as string | undefined;
+
+  if (domain) {
+    if (allowedDomains.has(domain)) {
+      // TODO validate domain against db
+      res.status(200).json(true);
+      return;
+    }
+    console.log(`${domain} is not allowed`);
+  }
+
+  res.status(503).send('Invalid domain');
 };
 
 export default caddyEndpoint;
