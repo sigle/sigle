950ee867f450f707728d2a0d780cbc9e3873aa61
pradel <pradel.leo@gmail.com>
2021-02-13 19:02:12 +0100

    protect app to run only on allowed domains

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
 import { Protected } from '../modules/auth/Protected';
 import { Home } from '../modules/home';
+
+// Map the domain to the user blockstack id
+const customDomains: Record<string, string> = {
+  'https://blog.sigle.io': 'leopradel.id.blockstack',
+};
+
+export const getServerSideProps: GetServerSideProps<{}> = async ({ req }) => {
+  const appUrl = `${req.headers['x-forwarded-proto'] || 'http'}://${
+    req.headers['host']
+  }`;
+
+  // If domain is not allowed, redirect the user to the root domain
+  if (appUrl !== process.env.APP_URL && !customDomains[appUrl]) {
+    return {
+      redirect: {
+        destination: process.env.APP_URL,
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+
+  return { props: {} };
+};
 
 const HomePage = () => {
   return (
