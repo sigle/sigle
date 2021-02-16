5ff7353a66742eebe54b8149553ec69941c4481f
pradel <pradel.leo@gmail.com>
2021-02-16 18:14:23 +0100

    do some basic logic for accessing the app

--- blog-viewer/src/pages/index.tsx
+++ blog-viewer/src/pages/index.tsx
@@ -1,5 +1,35 @@
+import { GetServerSideProps } from 'next';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+
+// Map the domain to the user blockstack id
+const customDomains: Record<string, string> = {
+  'https://blog.sigle.io': 'leopradel.id.blockstack',
+  'http://localhost:3001': 'leopradel.id.blockstack',
+};
+
+export const getServerSideProps: GetServerSideProps<{}> = async ({ req }) => {
+  const appUrl = `${req.headers['x-forwarded-proto'] || 'http'}://${
+    req.headers['host']
+  }`;
+
+  const resolvedUsername = customDomains[appUrl];
+
+  // If domain is not allowed, redirect the user to the root domain
+  if (!resolvedUsername) {
+    return {
+      redirect: {
+        destination: process.env.APP_URL,
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+
+  console.log({ resolvedUsername });
+
+  return { props: {} };
+};
 
 const Title = styled.h1`
   color: red;
