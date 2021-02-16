0c6f766db57822ac0dd7b8118e60329e9e803880
pradel <pradel.leo@gmail.com>
2021-02-16 09:31:53 +0100

    setup twin.macro

--- blog-viewer/src/pages/index.tsx
+++ blog-viewer/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import tw from 'twin.macro';
 
 const Title = styled.h1`
   color: red;
@@ -6,5 +7,17 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
-  return <Title>My page</Title>;
+  return (
+    <>
+      <Title>My page</Title>
+      <div
+        css={[
+          tw`flex flex-col items-center justify-center h-screen`,
+          tw`bg-gradient-to-b from-gray-300 to-red-400`,
+        ]}
+      >
+        Yo
+      </div>
+    </>
+  );
 }
