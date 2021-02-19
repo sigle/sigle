15b6f80f83c2f78cece46dee624c5f2704d6934e
pradel <pradel.leo@gmail.com>
2021-02-19 20:33:59 +0100

    seo

--- blog-viewer/src/pages/_app.tsx
+++ blog-viewer/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import App from 'next/app';
 import Router from 'next/router';
 import { createGlobalStyle } from 'styled-components';
 import * as Fathom from 'fathom-client';
+import { DefaultSeo } from 'next-seo';
 import { initSentry } from '../utils/sentry';
 import GlobalStyles from './../components/GlobalStyles';
 import '../styles/fonts.scss';
@@ -42,8 +43,24 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
+    const seoTitle = 'Sigle | Decentralized blogging platform';
+    const seoDescription =
+      'A secure, decentralized and open source blogging platform on top of blockstack';
+
     return (
       <>
+        <DefaultSeo
+          title={seoTitle}
+          description={seoDescription}
+          openGraph={{
+            type: 'website',
+            site_name: 'Sigle',
+            title: seoTitle,
+            description: seoDescription,
+            images: [{ url: `https://app.sigle.io/static/images/share.jpg` }],
+          }}
+          twitter={{ site: '@sigleapp', cardType: 'summary_large_image' }}
+        />
         <GlobalStyles />
         <GlobalStyle />
         <FathomTrack />
