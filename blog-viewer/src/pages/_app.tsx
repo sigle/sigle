3b8cb2ba3a89a4a94d59f6a4a75fed701089b668
pradel <pradel.leo@gmail.com>
2021-02-19 21:50:09 +0100

    add nprogress

--- blog-viewer/src/pages/_app.tsx
+++ blog-viewer/src/pages/_app.tsx
@@ -2,9 +2,13 @@ import { useEffect } from 'react';
 import App from 'next/app';
 import Router from 'next/router';
 import { createGlobalStyle } from 'styled-components';
+import tw from 'twin.macro';
 import * as Fathom from 'fathom-client';
 import { DefaultSeo } from 'next-seo';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import { initSentry } from '../utils/sentry';
+import { colors } from '../utils/colors';
 import GlobalStyles from './../components/GlobalStyles';
 import '../styles/fonts.scss';
 import { sigleConfig } from '../config';
@@ -34,9 +38,29 @@ Router.events.on('routeChangeComplete', () => {
   Fathom.trackPageview();
 });
 
+/**
+ * Loading bar
+ */
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Roboto', sans-serif;
+  }
+
+  /* For the nprogress bar */
+  #nprogress .bar {
+    ${tw`bg-pink`};
+  }
+  #nprogress .peg {
+    box-shadow: 0 0 10px ${colors.pink}, 0 0 5px ${colors.pink};
+  }
+  #nprogress .spinner-icon { 
+    border-top-color: ${colors.pink};
+    border-left-color: ${colors.pink};
   }
 `;
 export default class MyApp extends App {
---
+++ blog-viewer/src/utils/colors.ts
@@ -0,0 +1,3 @@
+export const colors = {
+  pink: '#ff576a',
+};
