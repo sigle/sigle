c992e86f4e220626a0dc0e804e594be962804111
pradel <pradel.leo@gmail.com>
2021-02-25 16:49:20 +0100

    fix lint rule

--- sigle/.eslintrc.js
+++ sigle/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'eslint/no-use-before-define': 'off',
     'react/no-unescaped-entities': 'off',
     'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
