8de9db93fcaccfb99e1c1fc55b66f3cc6b3105e5
pradel <pradel.leo@gmail.com>
2021-06-20 16:36:27 +0200

    dry run option

--- sigle/next.config.js
+++ sigle/next.config.js
@@ -4,27 +4,32 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 dotenv.config();
 
-module.exports = withPlugins([[withSentryConfig, {}]], {
-  env: {
-    APP_URL: process.env.APP_URL,
-    FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
-    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+module.exports = withSentryConfig(
+  {
+    env: {
+      APP_URL: process.env.APP_URL,
+      FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
+      NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    },
+    headers: async () => {
+      return [
+        {
+          source: '/manifest.json',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: '*',
+            },
+          ],
+        },
+      ];
+    },
   },
-  headers: async () => {
-    return [
-      {
-        source: '/manifest.json',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
-});
+  {
+    dryRun: process.env.NEXT_PUBLIC_APP_ENV !== 'production',
+  }
+);
