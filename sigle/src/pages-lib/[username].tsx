f2cd8a73e8ac140fb14d3dfbfd326e278a467fa1
Pradel Léo <pradel.leo@gmail.com>
2021-07-28 11:55:25 +0200

    chore: upgrade yarn berry to v3

--- sigle/src/pages-lib/[username].tsx
+++ sigle/src/pages-lib/[username].tsx
@@ -86,13 +86,11 @@ export const getServerSideProps: GetServerSideProps<PublicHomePageProps> = async
     }
   }
 
+  console.log({ userProfile });
+
   // If deployed on vercel we want to get the deployment url to be able to test unmerged pr's
   // If client side we use window.location.origin
-  const appUrl = req.headers['x-forwarded-host']
-    ? `${req.headers['x-forwarded-proto']}://${req.headers['x-forwarded-host']}`
-    : req.headers.host === 'localhost:3000'
-    ? 'http://localhost:3000'
-    : sigleConfig.appUrl;
+  const appUrl = sigleConfig.appUrl;
 
   const bucketUrl = userProfile && userProfile.apps && userProfile.apps[appUrl];
   // If the user already used the app we try to get the public list
