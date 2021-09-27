339969559f82c36c683efc78dc1dcb669b84f7a8
Pradel Léo <pradel.leo@gmail.com>
2021-09-27 21:04:36 +0200

    Update login.tsx

--- sigle/src/pages/login.tsx
+++ sigle/src/pages/login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import * as Fathom from 'fathom-client';
