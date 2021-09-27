ae5f2a1df0c316005f31ddfc17551168009cca6e
Pradel Léo <pradel.leo@gmail.com>
2021-09-27 14:09:20 +0200

    more

--- sigle/src/pages/design-system.tsx
+++ sigle/src/pages/design-system.tsx
@@ -1,9 +1,10 @@
 import { styled } from '../stitches.config';
-import { Container, Heading, Text } from '../ui';
+import { Box, Container, Heading, Text, Button } from '../ui';
 
 const Separator = styled('div', {
   borderBottom: '1px solid $gray7',
   mt: '$2',
+  mb: '$4',
 });
 
 const SectionHeading = styled(Heading, {
@@ -19,9 +20,7 @@ const DesignSystem = () => {
       <SectionHeading size="4xl">Heading</SectionHeading>
       <Separator />
 
-      <Heading size="5xl" css={{ mt: '$2' }}>
-        (5xl) {typographyText}
-      </Heading>
+      <Heading size="5xl">(5xl) {typographyText}</Heading>
       <Heading size="4xl" css={{ mt: '$2' }}>
         (4xl) {typographyText}
       </Heading>
@@ -37,15 +36,38 @@ const DesignSystem = () => {
 
       <SectionHeading size="4xl">Text</SectionHeading>
       <Separator />
-      <Text size="md" css={{ mt: '$2' }}>
-        (md) {typographyText}
-      </Text>
+      <Text size="md">(md) {typographyText}</Text>
       <Text size="sm" css={{ mt: '$2' }}>
         (sm) {typographyText}
       </Text>
       <Text size="xs" css={{ mt: '$2' }}>
         (xs) {typographyText}
       </Text>
+
+      <SectionHeading size="4xl">Button</SectionHeading>
+      <Separator />
+      <Box css={{ display: 'flex', alignItems: 'center', gap: '$3' }}>
+        <Text size="sm">Sizes:</Text>
+        <Button size="md" color="gray">
+          (md) Button
+        </Button>
+        <Button size="lg" color="gray">
+          (lg) Button
+        </Button>
+      </Box>
+      <Box css={{ display: 'flex', alignItems: 'center', gap: '$3', mt: '$4' }}>
+        <Text size="sm">Colors:</Text>
+        <Button color="ghost" css={{ mr: '$4' }}>
+          (ghost) Button
+        </Button>
+        <Button color="gray" css={{ mr: '$4' }}>
+          (gray) Button
+        </Button>
+        <Button color="orange">(orange) Button</Button>
+      </Box>
+
+      <SectionHeading size="4xl">Dropdown menu</SectionHeading>
+      <Separator />
     </Container>
   );
 };
