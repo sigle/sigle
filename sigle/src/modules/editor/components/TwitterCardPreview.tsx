754419779a0f24db22937625b09d306066f00de4
Pradel Léo <pradel.leo@gmail.com>
2021-09-29 12:59:11 +0200

    image style

--- sigle/src/modules/editor/components/TwitterCardPreview.tsx
+++ sigle/src/modules/editor/components/TwitterCardPreview.tsx
@@ -39,24 +39,52 @@ const MetaLink = styled('div', {
   alignItems: 'center',
 });
 
+// TODO take props
+// TODO style without image
+
 export const TwitterCardPreview = () => {
   return (
-    <>
-      <MetaContainer>
-        <Flex direction="column" css={{ gap: 2, padding: 12 }}>
-          <MetaTitle>Sigle</MetaTitle>
-          <MetaDescription>
-            Sigle is a decentralised, open-source platform empowering creators.
-            Write, share, build your audience and earn Bitcoin.
-          </MetaDescription>
-          <MetaLink>
-            <Link2Icon />
-            <Box as="span" css={{ ml: 4 }}>
-              sigle.io
-            </Box>
-          </MetaLink>
-        </Flex>
-      </MetaContainer>
-    </>
+    <MetaContainer>
+      <Box css={{ overflow: 'hidden', position: 'relative' }}>
+        <Box css={{ paddingBottom: '52.356%' }} />
+        <Box
+          css={{
+            height: '100%',
+            width: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <Box
+            css={{
+              backgroundImage:
+                'url("https://pbs.twimg.com/card_img/1442525177884000258/U7x0MmsK?format=jpg&name=small")',
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
+              backgroundColor: 'rgba(0, 0, 0, 0)',
+              height: '100%',
+              width: '100%',
+            }}
+          />
+        </Box>
+      </Box>
+
+      <Flex direction="column" css={{ gap: 2, padding: 12 }}>
+        <MetaTitle>Sigle</MetaTitle>
+        <MetaDescription>
+          Sigle is a decentralised, open-source platform empowering creators.
+          Write, share, build your audience and earn Bitcoin.
+        </MetaDescription>
+        <MetaLink>
+          <Link2Icon />
+          <Box as="span" css={{ ml: 4 }}>
+            sigle.io
+          </Box>
+        </MetaLink>
+      </Flex>
+    </MetaContainer>
   );
 };
