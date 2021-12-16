d8c273b393df9fe3020ea7e54a0d10d7476f5c3e
Pradel Léo <pradel.leo@gmail.com>
2021-12-16 16:47:50 +0100

    create markdown

--- packages/slate-to-markdown/src/index.ts
+++ packages/slate-to-markdown/src/index.ts
@@ -1,5 +1,8 @@
+import { serialize } from 'remark-slate';
 import { migrateSchema } from './migrate';
 
 export const convert = (value: any): string => {
-  return migrateSchema(value);
+  return migrateSchema(value)
+    .map((v: any) => serialize(v))
+    .join('');
 };
