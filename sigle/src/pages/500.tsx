ab7b609ef9ae93efddf215de492c5a99f24c8c91
gregogun <gregogun97@gmail.com>
2022-01-20 16:24:00 +0000

    chore: remove static 500 page

--- sigle/src/pages/500.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { MyError } from '../pages-lib/_error';
-
-const Custom500 = () => {
-  return <MyError statusCode={500} errorMessage="An error has occured" />;
-};
-
-export default Custom500;
