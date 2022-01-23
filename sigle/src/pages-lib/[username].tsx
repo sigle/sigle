56c3896fcc34314855e2c1ab5bd5d181ae1adb2d
Léo Pradel <pradel.leo@gmail.com>
2022-01-23 15:52:58 +0100

    fix: fix dependabots alerts (#286)

--- sigle/src/pages-lib/[username].tsx
+++ sigle/src/pages-lib/[username].tsx
@@ -57,65 +57,65 @@ const fetchSettings = async (bucketUrl: string) => {
   return { file, statusCode };
 };
 
-export const getServerSideProps: GetServerSideProps<PublicHomePageProps> =
-  async ({ req, res, params }) => {
-    const username = params?.username as string;
-    let file = null;
-    let settings = null;
-    let statusCode: boolean | number = false;
-    let errorMessage: string | null = null;
-    let userProfile;
-    try {
-      userProfile = await lookupProfile({ username });
-    } catch (error) {
-      // This will happen if there is no blockstack user with this name
-      if (error.message === 'Name not found') {
-        statusCode = 404;
-      } else {
-        statusCode = 500;
-        errorMessage = `Blockstack lookupProfile returned error: ${error.message}`;
-        Sentry.withScope((scope) => {
-          scope.setExtras({
-            username,
-          });
-          Sentry.captureException(error);
-        });
-      }
-    }
-
-    // If deployed on vercel we want to get the deployment url to be able to test unmerged pr's
-    // If client side we use window.location.origin
-    const appUrl = req.headers['x-forwarded-host']
-      ? `${req.headers['x-forwarded-proto']}://${req.headers['x-forwarded-host']}`
-      : req.headers.host === 'localhost:3000'
-      ? 'http://localhost:3000'
-      : sigleConfig.appUrl;
-
-    const bucketUrl =
-      userProfile && userProfile.apps && userProfile.apps[appUrl];
-    // If the user already used the app we try to get the public list
-    if (bucketUrl) {
-      const [dataPublicStories, dataSettings] = await Promise.all([
-        fetchPublicStories(bucketUrl),
-        fetchSettings(bucketUrl),
-      ]);
-
-      file = dataPublicStories.file;
-      if (dataPublicStories.statusCode) {
-        statusCode = dataPublicStories.statusCode;
-      }
-
-      settings = dataSettings.file;
-    } else if (!statusCode) {
+export const getServerSideProps: GetServerSideProps<
+  PublicHomePageProps
+> = async ({ req, res, params }) => {
+  const username = params?.username as string;
+  let file = null;
+  let settings = null;
+  let statusCode: boolean | number = false;
+  let errorMessage: string | null = null;
+  let userProfile;
+  try {
+    userProfile = await lookupProfile({ username });
+  } catch (error) {
+    // This will happen if there is no blockstack user with this name
+    if (error.message === 'Name not found') {
       statusCode = 404;
+    } else {
+      statusCode = 500;
+      errorMessage = `Blockstack lookupProfile returned error: ${error.message}`;
+      Sentry.withScope((scope) => {
+        scope.setExtras({
+          username,
+        });
+        Sentry.captureException(error);
+      });
+    }
+  }
+
+  // If deployed on vercel we want to get the deployment url to be able to test unmerged pr's
+  // If client side we use window.location.origin
+  const appUrl = req.headers['x-forwarded-host']
+    ? `${req.headers['x-forwarded-proto']}://${req.headers['x-forwarded-host']}`
+    : req.headers.host === 'localhost:3000'
+    ? 'http://localhost:3000'
+    : sigleConfig.appUrl;
+
+  const bucketUrl = userProfile && userProfile.apps && userProfile.apps[appUrl];
+  // If the user already used the app we try to get the public list
+  if (bucketUrl) {
+    const [dataPublicStories, dataSettings] = await Promise.all([
+      fetchPublicStories(bucketUrl),
+      fetchSettings(bucketUrl),
+    ]);
+
+    file = dataPublicStories.file;
+    if (dataPublicStories.statusCode) {
+      statusCode = dataPublicStories.statusCode;
     }
 
-    // If statusCode is not false we set the http response code
-    if (statusCode && res) {
-      res.statusCode = statusCode as number;
-    }
+    settings = dataSettings.file;
+  } else if (!statusCode) {
+    statusCode = 404;
+  }
 
-    return {
-      props: { statusCode, errorMessage, file, settings },
-    };
+  // If statusCode is not false we set the http response code
+  if (statusCode && res) {
+    res.statusCode = statusCode as number;
+  }
+
+  return {
+    props: { statusCode, errorMessage, file, settings },
   };
+};
