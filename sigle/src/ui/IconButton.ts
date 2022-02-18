76c47e32232eee4a78d301c7e69afd455e72b4a8
gregogun <gregogun97@gmail.com>
2022-02-18 17:13:09 +0000

    feat: use design system color for icon button

--- sigle/src/ui/IconButton.ts
+++ sigle/src/ui/IconButton.ts
@@ -6,8 +6,16 @@ export const IconButton = styled('button', {
   alignItems: 'center',
   fontSize: '$2',
 
+  '& svg': {
+    color: '$gray11',
+  },
+
   variants: {
     size: {
+      sm: {
+        p: '$2',
+        br: '$1',
+      },
       md: {
         p: '$2',
         br: '$1',
