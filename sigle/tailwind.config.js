b7c4427e1dc6b040082630bfc2b05264adb54ee8
Léo Pradel <pradel.leo@gmail.com>
2022-02-21 12:02:06 +0100

    Update tailwind.config.js

--- sigle/tailwind.config.js
+++ sigle/tailwind.config.js
@@ -17,10 +17,28 @@ module.exports = {
           DEFAULT: '#ff576a',
         },
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '',
+            },
+            'code::after': {
+              content: '',
+            },
+            code: {
+              fontWeight: '400',
+            },
+            blockquote: {
+              quotes: 'none',
+            },
+          },
+        },
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
