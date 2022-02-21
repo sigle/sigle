0d7cf5e18cd1ff6b3334538690e271f2a12281d7
Léo Pradel <pradel.leo@gmail.com>
2022-02-21 18:44:56 +0100

    26 26 26

--- sigle/src/modules/editor/TipTapEditor.tsx
+++ sigle/src/modules/editor/TipTapEditor.tsx
@@ -73,7 +73,7 @@ const globalStylesCustomEditor = globalCss({
     padding: 0,
     backgroundColor: '$gray1',
     boxShadow:
-      '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+      '0px 10px 38px -10px rgba(26, 26, 26, 0.35), 0px 10px 20px -15px rgba(26, 26, 26, 0.2)',
     br: '$1',
     minWidth: '280px',
   },
