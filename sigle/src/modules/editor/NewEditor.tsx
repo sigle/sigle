8a87fc9c4b0ad5e696bbab29968d583ae4805098
Léo Pradel <pradel.leo@gmail.com>
2022-02-24 16:05:39 +0100

    feat: improve cover image behavior new editor

--- sigle/src/modules/editor/NewEditor.tsx
+++ sigle/src/modules/editor/NewEditor.tsx
@@ -4,9 +4,10 @@ import { toast } from 'react-toastify';
 import NProgress from 'nprogress';
 import * as Fathom from 'fathom-client';
 import posthog from 'posthog-js';
+import { CameraIcon } from '@radix-ui/react-icons';
 import { styled } from '../../stitches.config';
 import { Story } from '../../types';
-import { Container, Text } from '../../ui';
+import { Box, Button, Container, Flex, Text } from '../../ui';
 import { EditorHeader } from './EditorHeader';
 import { PublishDialog } from './PublishDialog';
 import { TipTapEditor } from './TipTapEditor';
@@ -196,6 +197,12 @@ export const NewEditor = ({ story }: NewEditorProps) => {
           }}
           placeholder="Title"
         />
+        <Box css={{ mt: '$5' }}>
+          <Button variant="ghost" css={{ color: '$gray9', gap: '$1' }}>
+            <CameraIcon />
+            Add cover image
+          </Button>
+        </Box>
         <TipTapEditor ref={editorRef} story={story} />
       </EditorContainer>
 
