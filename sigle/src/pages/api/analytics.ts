e8b613f9438af0b2af88a0964614225ec0e99407
Léo Pradel <pradel.leo@gmail.com>
2022-03-01 20:21:26 +0100

    create client

--- sigle/src/pages/api/analytics.ts
+++ sigle/src/pages/api/analytics.ts
@@ -1,35 +1,30 @@
 import { NextApiHandler } from 'next';
 import * as Sentry from '@sentry/nextjs';
+import { initFathomClient } from '../../utils/fathomApi';
+
+const fathomClient = initFathomClient({
+  apiToken: process.env.FATHOM_API_TOKEN!,
+  entityId: process.env.FATHOM_ENTITY_ID!,
+});
 
 const analyticsEndpoint: NextApiHandler = async (_, res) => {
   // TODO proper error handling
   // TODO entity_id from env variable
 
-  const params = {
+  const data = await fathomClient.aggregate({
     entity: 'pageview',
-    entity_id: 'DLJCUFGQ',
     aggregates: 'visits,pageviews',
     date_grouping: 'month',
     field_grouping: 'pathname',
-    filters: JSON.stringify([
+    sort_by: 'timestamp:asc',
+    filters: [
       {
         property: 'pathname',
         operator: 'is',
         value: '/sigleapp.id.blockstack',
       },
-    ]),
-  };
-  const urlParams = new URLSearchParams(params).toString();
-  const response = await fetch(
-    `https://api.usefathom.com/v1/aggregations?${urlParams}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.FATHOM_API_TOKEN}`,
-      },
-    }
-  );
-
-  const data = await response.json();
+    ],
+  });
 
   const filter = data.filter((record: { pathname: string }) =>
     record.pathname?.startsWith('/sigleapp.id.blockstack')
---
+++ sigle/src/utils/fathomApi.ts
@@ -0,0 +1,47 @@
+export const initFathomClient = ({
+  apiToken,
+  entityId,
+}: {
+  apiToken: string;
+  entityId: string;
+}) => {
+  return {
+    /**
+     * https://usefathom.com/api#aggregation
+     */
+    aggregate: async (params: {
+      entity: 'pageview' | 'event';
+      aggregates: string;
+      date_grouping?: 'day' | 'month';
+      field_grouping?: 'hostname' | 'pathname';
+      sort_by?: 'timestamp:asc' | 'timestamp:desc';
+      date_from?: string;
+      date_to?: string;
+      limit?: number;
+      filters: {
+        property: 'hostname' | 'pathname';
+        operator: 'is' | 'is not';
+        value: string;
+      }[];
+    }) => {
+      const urlParams = new URLSearchParams({
+        ...params,
+        entity_id: entityId,
+        filters: JSON.stringify(params.filters),
+      } as any).toString();
+
+      // TODO proper error handling
+      const response = await fetch(
+        `https://api.usefathom.com/v1/aggregations?${urlParams}`,
+        {
+          headers: {
+            Authorization: `Bearer ${apiToken}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      return data;
+    },
+  };
+};
