5546d18ed72ea41821c746f7e0382a0fba60fdf8
gregogun <gregogun97@gmail.com>
2022-03-07 13:20:56 +0000

    feat: add footer to login page

---
+++ .changeset/thin-cycles-shop.md
@@ -0,0 +1,5 @@
+---
+'@sigle/app': minor
+---
+
+Add footer to login page.
--- sigle/src/pages/login.tsx
+++ sigle/src/pages/login.tsx
@@ -3,20 +3,38 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import * as Fathom from 'fathom-client';
 import { useConnect } from '@stacks/connect-react';
-import { ArrowTopRightIcon } from '@radix-ui/react-icons';
+import {
+  ArrowTopRightIcon,
+  DiscordLogoIcon,
+  GitHubLogoIcon,
+  TwitterLogoIcon,
+} from '@radix-ui/react-icons';
 import { sigleConfig } from '../config';
 import { Goals } from '../utils/fathom';
 import { useAuth } from '../modules/auth/AuthContext';
 import { styled } from '../stitches.config';
-import { Box, Button, Container, Heading, Text } from '../ui';
+import { Box, Button, Container, Flex, Heading, IconButton, Text } from '../ui';
+
+const Footer = styled('footer', {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  pt: '$6',
+  boxShadow: ' 0 0 0 1px $colors$gray6',
+});
 
 const FullScreen = styled('div', {
   height: '100vh',
   display: 'flex',
+  flexDirection: 'column',
   alignItems: 'center',
+  justifyContent: 'space-between',
 });
 
 const LoginContainer = styled(Container, {
+  my: 'auto',
   '@xl': {
     maxWidth: '1024px',
   },
@@ -115,6 +133,113 @@ const Login = () => {
           </BlockIllustration>
         </Grid>
       </LoginContainer>
+      <Footer>
+        <Flex css={{ mb: '$5' }} gap="3">
+          <IconButton
+            as="a"
+            href={sigleConfig.twitterUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TwitterLogoIcon />
+          </IconButton>
+          <IconButton
+            as="a"
+            href={sigleConfig.discordUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <DiscordLogoIcon />
+          </IconButton>
+          <IconButton
+            as="a"
+            href={sigleConfig.twitterUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubLogoIcon />
+          </IconButton>
+        </Flex>
+        <Flex
+          css={{
+            boxShadow: '0 1px 0 0 $colors$gray6',
+            pb: '$3',
+            justifyContent: 'center',
+            width: '100%',
+          }}
+          gap="5"
+        >
+          <Text
+            size="sm"
+            as="a"
+            target="_blank"
+            rel="noreferrer"
+            href={sigleConfig.blogUrl}
+            css={{
+              color: '$gray12',
+              '&:hover': {
+                boxShadow: '0 1px 0 0px $colors$gray12',
+              },
+            }}
+          >
+            Blog
+          </Text>
+          <Text
+            size="sm"
+            as="a"
+            target="_blank"
+            rel="noreferrer"
+            href={sigleConfig.documentationUrl}
+            css={{
+              color: '$gray12',
+              '&:hover': {
+                boxShadow: '0 1px 0 0px $colors$gray12',
+              },
+            }}
+          >
+            Documentation
+          </Text>
+          <Text
+            size="sm"
+            as="a"
+            target="_blank"
+            rel="noreferrer"
+            href={sigleConfig.discordUrl}
+            css={{
+              color: '$gray12',
+              '&:hover': {
+                boxShadow: '0 1px 0 0px $colors$gray12',
+              },
+            }}
+          >
+            Support
+          </Text>
+          <Text
+            size="sm"
+            as="a"
+            target="_blank"
+            rel="noreferrer"
+            href={sigleConfig.feedbackUrl}
+            css={{
+              color: '$gray12',
+              '&:hover': {
+                boxShadow: '0 1px 0 0px $colors$gray12',
+              },
+            }}
+          >
+            Feedback
+          </Text>
+        </Flex>
+        <Text
+          size="xs"
+          css={{
+            py: '$5',
+            color: '$gray12',
+          }}
+        >
+          © Sigle {new Date().getFullYear()} - SAS Incognito Lab
+        </Text>
+      </Footer>
     </FullScreen>
   );
 };
