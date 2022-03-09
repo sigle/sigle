72ebcb58485cec2044f8a22962865723467dab3b
gregogun <gregogun97@gmail.com>
2022-03-09 17:23:02 +0000

    fix: remove useEffect import

--- sigle/src/pages/register-username.tsx
+++ sigle/src/pages/register-username.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Flex, Heading, Text } from '../ui';
 import { LoginLayout } from '../modules/layout/components/LoginLayout';
 import { keyframes, styled } from '../stitches.config';
