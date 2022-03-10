f2f5ef88b58fa98fafe85baede4429c1bf7d03dc
Léo Pradel <pradel.leo@gmail.com>
2022-03-10 19:05:06 +0100

    try

---
+++ sigle/next-sitemap.js
@@ -0,0 +1,17 @@
+// TODO generate a list of subdomains
+// TODO check if these subdomains are using Sigle (get profile and check apps entry)
+// TODO get list of stories.json and add them to the sitemap
+// TODO Get from settings if website should not be listed
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://app.sigle.io',
+  generateRobotsTxt: true,
+  exclude: [
+    // Exclude all the protected routes
+    '/login',
+    '/register-username',
+    '/published',
+    '/settings',
+  ],
+};
