2e7b847ec577a175a793108f583c7ba030c2b9ee
Léo Pradel <pradel.leo@gmail.com>
2022-03-11 12:26:28 +0100

    Update next-sitemap.js

--- sigle/next-sitemap.js
+++ sigle/next-sitemap.js
@@ -1,9 +1,7 @@
-const urls = require('../packages/subdomain-list/urls.json');
+const urls = require('../packages/sitemap-scripts/urls.json');
 const appUrl = 'https://app.sigle.io';
 
 // TODO generate a list of subdomains
-// TODO check if these subdomains are using Sigle (get profile and check apps entry)
-// TODO get list of stories.json and add them to the sitemap
 // TODO Get from settings if website should not be listed
 
 /** @type {import('next-sitemap').IConfig} */
