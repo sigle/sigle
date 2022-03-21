5dec947c1ebc8f5593a2b935d141667fc065a974
Léo Pradel <pradel.leo@gmail.com>
2022-03-21 21:28:44 +0100

    cleanup code

--- sigle/src/modules/editor/containers/Editor.tsx
+++ sigle/src/modules/editor/containers/Editor.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
 import * as Sentry from '@sentry/nextjs';
 import { Editor as Component } from '../components/Editor';
-import { Story } from '../../../types';
 import { getStoryFile } from '../../../utils';
 import { migrationStory } from '../../../utils/migrations/story';
 
 export const Editor = () => {
   const router = useRouter();
   const { storyId } = router.query as { storyId: string };
-  const [story, setStory] = useState<Story | null>(null);
 
   const { data, isLoading } = useQuery(
     ['story', storyId],
@@ -29,11 +26,5 @@ export const Editor = () => {
     }
   );
 
-  useEffect(() => {
-    if (data) {
-      setStory(data);
-    }
-  }, [data]);
-
-  return <Component loading={isLoading} story={story} />;
+  return <Component loading={isLoading} story={data ?? null} />;
 };
