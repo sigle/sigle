d310c477d6a1f706a6dd37f317237d6a9ca728f0
Greg Ogun <gregogun97@gmail.com>
2022-03-31 18:37:11 +0100

    chore: remove red colors

--- sigle/src/stitches.config.ts
+++ sigle/src/stitches.config.ts
@@ -61,14 +61,6 @@ const orangeDark = {
   orange12: '#FF802B',
 };
 
-const red = {
-  red11: '#F43939',
-};
-
-const redDark = {
-  red11: '#FF6C6C',
-};
-
 export const {
   styled,
   css,
@@ -82,7 +74,6 @@ export const {
     colors: {
       ...gray,
       ...orange,
-      ...red,
     },
     fonts: {
       openSans: "'Open Sans', sans-serif",
@@ -205,7 +196,6 @@ export const darkTheme = createTheme('dark', {
   colors: {
     ...grayDark,
     ...orangeDark,
-    ...redDark,
   },
 });
 
