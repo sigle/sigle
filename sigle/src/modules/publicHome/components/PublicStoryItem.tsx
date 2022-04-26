ee1e6d36b4583acac02074843b8859dbf625b5f7
Greg Ogun <gregogun97@gmail.com>
2022-04-26 12:37:19 +0100

    feat: update public story card text colors

--- sigle/src/modules/publicHome/components/PublicStoryItem.tsx
+++ sigle/src/modules/publicHome/components/PublicStoryItem.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
-import styled, { css } from 'styled-components';
+import styledC, { css } from 'styled-components';
 import tw from 'twin.macro';
 import format from 'date-fns/format';
 import { SubsetStory, SettingsFile } from '../../../types';
 import { sigleConfig } from '../../../config';
 import { sanitizeHexColor } from '../../../utils/security';
+import { Heading, Text } from '../../../ui';
+import { styled } from '../../../stitches.config';
 
-const Container = styled.div`
-  ${tw`py-8 border-b border-solid border-grey-light`};
-`;
+const Container = styled('div', {
+  py: '$8',
+  borderBottom: '1px solid $colors$gray6',
+});
 
-const StoryContainer = styled.div<{ siteColor?: string; featured?: boolean }>`
+const StoryContainer = styledC.div<{ siteColor?: string; featured?: boolean }>`
   ${tw`cursor-pointer`};
 
   ${(props) =>
@@ -35,7 +38,7 @@ const StoryContainer = styled.div<{ siteColor?: string; featured?: boolean }>`
   }
 `;
 
-const StoryContainerImage = styled.div<{ featured?: boolean }>`
+const StoryContainerImage = styledC.div<{ featured?: boolean }>`
   ${tw`mb-4`};
 
   ${(props) =>
@@ -47,12 +50,12 @@ const StoryContainerImage = styled.div<{ featured?: boolean }>`
     `}
 `;
 
-const StoryImage = styled.img`
+const StoryImage = styledC.img`
   display: block;
   max-width: 100%;
 `;
 
-const StoryContainerContent = styled.div<{
+const StoryContainerContent = styledC.div<{
   hasCover: boolean;
   featured?: boolean;
 }>`
@@ -66,15 +69,15 @@ const StoryContainerContent = styled.div<{
     `}
 `;
 
-const StoryTitle = styled.div`
+const StoryTitle = styledC.div`
   ${tw`text-2xl font-bold no-underline transition-colors duration-200 ease-in-out`};
 `;
 
-const StoryDate = styled.div`
+const StoryDate = styledC.div`
   ${tw`mt-1 text-sm text-grey-dark`};
 `;
 
-const StoryText = styled.div`
+const StoryText = styledC.div`
   ${tw`mt-4 text-grey-darker leading-tight`};
 `;
 
@@ -105,18 +108,19 @@ export const PublicStoryItem = ({ username, story, settings }: Props) => {
             hasCover={!!story.coverImage}
             featured={story.featured}
           >
-            <StoryTitle data-testid="story-title" className="sigle-story-title">
+            <Heading size="xl" data-testid="story-title">
               {story.title}
-            </StoryTitle>
-            <StoryDate data-testid="story-date" className="sigle-story-date">
+            </Heading>
+            <Text size="sm" data-testid="story-date">
               {format(story.createdAt, 'dd MMMM yyyy')}
-            </StoryDate>
-            <StoryText
+            </Text>
+            <Text
+              css={{ mt: '$4', color: '$gray10' }}
+              size="sm"
               data-testid="story-content"
-              className="sigle-story-content"
             >
               {story.content}
-            </StoryText>
+            </Text>
           </StoryContainerContent>
         </StoryContainer>
       </Link>
