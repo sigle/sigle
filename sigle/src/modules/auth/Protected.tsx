ad0c17a50a8ea51bd12fd630e92f18c05a4b7dde
Léo Pradel <pradel.leo@gmail.com>
2022-05-04 17:57:16 +0200

    feat: guide for missconfigured BNS

--- sigle/src/modules/auth/Protected.tsx
+++ sigle/src/modules/auth/Protected.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from './AuthContext';
 import { FullScreenLoading } from '../layout/components/FullScreenLoading';
@@ -10,6 +10,26 @@ interface Props {
 export const Protected = ({ children }: Props) => {
   const router = useRouter();
   const { user, loggingIn } = useAuth();
+
+  useEffect(() => {
+    const checkBnsConfiguration = async () => {
+      if (user && user.username) {
+        try {
+          const namesResponse = await fetch(
+            `https://stacks-node-api.stacks.co/v1/names/${user.username}`
+          );
+          const namesJson = (await namesResponse.json()) as {
+            zonefile: string;
+          };
+          if (namesJson.zonefile === '') {
+            router.push('/configure-bns');
+          }
+        } catch (e) {}
+      }
+    };
+
+    checkBnsConfiguration();
+  }, [user]);
 
   // We show a big loading screen while the user is signing in
   if (loggingIn) {
