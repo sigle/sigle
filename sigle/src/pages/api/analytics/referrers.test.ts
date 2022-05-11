5d814ead5460b3f5704147b3e0b3206f933fb44d
Léo Pradel <pradel.leo@gmail.com>
2022-05-11 17:32:43 +0200

    add test for Single story

--- sigle/src/pages/api/analytics/referrers.test.ts
+++ sigle/src/pages/api/analytics/referrers.test.ts
@@ -40,7 +40,7 @@ it('Should throw an error if dateFrom is invalid', async () => {
   expect(json).toBeCalledWith({ error: 'dateFrom is invalid' });
 });
 
-it('Respond with a referrers', async () => {
+it('Respond with referrers', async () => {
   // Set a fake timer so we can verify the end date
   jest
     .useFakeTimers()
@@ -75,6 +75,37 @@ it('Respond with a referrers', async () => {
     } as any
   );
   expect(fathomClient.aggregateReferrers).toBeCalledTimes(25);
+  expect(status).toBeCalledWith(200);
+  expect(json).toMatchSnapshot();
+});
+
+it('Respond with referrers for one story', async () => {
+  // Set a fake timer so we can verify the end date
+  jest
+    .useFakeTimers()
+    .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+  (fathomClient.aggregateReferrers as jest.Mock).mockResolvedValueOnce([
+    {
+      uniques: '5',
+      referrer_hostname: 'https://stacks.co',
+    },
+  ]);
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
+
+  const json = jest.fn();
+  const status = jest.fn(() => ({
+    json,
+  }));
+
+  await analyticsReferrersEndpoint(
+    {
+      query: { dateFrom: '2022-03-15', storyId: 'test' },
+    } as any,
+    {
+      status,
+    } as any
+  );
+  expect(fathomClient.aggregateReferrers).toBeCalledTimes(1);
   expect(status).toBeCalledWith(200);
   expect(json).toMatchSnapshot();
 });
