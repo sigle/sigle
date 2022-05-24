27e69be7c5fb6b10d15d39654bcc6d77072580bd
Léo Pradel <pradel.leo@gmail.com>
2022-05-24 17:05:05 +0200

    add test historical

--- server/src/api/modules/analytics/historical.test.ts
+++ server/src/api/modules/analytics/historical.test.ts
@@ -74,113 +74,93 @@ it('Should throw an error if dateGrouping is day and dateFrom > 2 month', async 
   });
 });
 
-// it('Respond with a formatted time series for days', async () => {
-//   // Set a fake timer so we can verify the end date
-//   jest
-//     .useFakeTimers()
-//     .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
-//     {
-//       visits: '5',
-//       pageviews: '1',
-//       date: '2022-03-20',
-//       pathname: '/test.btc/path-test',
-//     },
-//   ]);
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
-//     {
-//       visits: '7',
-//       pageviews: '3',
-//       date: '2022-03-20',
-//       pathname: '/test.btc/path-test-2',
-//     },
-//   ]);
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
+it('Respond with a formatted time series for days', async () => {
+  // Set a fake timer so we can verify the end date
+  jest
+    .useFakeTimers(fakeTimerConfigDate)
+    .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
+    {
+      visits: '5',
+      pageviews: '1',
+      date: '2022-03-20',
+      pathname: '/test.btc/path-test',
+    },
+  ]);
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
+    {
+      visits: '7',
+      pageviews: '3',
+      date: '2022-03-20',
+      pathname: '/test.btc/path-test-2',
+    },
+  ]);
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
 
-//   const json = jest.fn();
-//   const status = jest.fn(() => ({
-//     json,
-//   }));
+  const response = await server.inject({
+    method: 'GET',
+    url: '/api/analytics/historical?dateFrom=2022-03-15&dateGrouping=day',
+  });
 
-//   await analyticsHistoricalEndpoint(
-//     { query: { dateFrom: '2022-03-15', dateGrouping: 'day' } } as any,
-//     {
-//       status,
-//     } as any
-//   );
-//   expect(fathomClient.aggregatePath).toBeCalledTimes(25);
-//   expect(status).toBeCalledWith(200);
-//   expect(json).toMatchSnapshot();
-// });
+  expect(fathomClient.aggregatePath).toBeCalledTimes(25);
+  expect(response.statusCode).toBe(200);
+  expect(response.json()).toMatchSnapshot();
+});
 
-// it('Respond with a formatted time series for months', async () => {
-//   // Set a fake timer so we can verify the end date
-//   jest
-//     .useFakeTimers()
-//     .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
-//     {
-//       visits: '5',
-//       pageviews: '1',
-//       date: '2022-03',
-//       pathname: '/test.btc/path-test',
-//     },
-//   ]);
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
-//     {
-//       visits: '7',
-//       pageviews: '3',
-//       date: '2022-03',
-//       pathname: '/test.btc/path-test-2',
-//     },
-//   ]);
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
+it('Respond with a formatted time series for months', async () => {
+  // Set a fake timer so we can verify the end date
+  jest
+    .useFakeTimers(fakeTimerConfigDate)
+    .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
+    {
+      visits: '5',
+      pageviews: '1',
+      date: '2022-03',
+      pathname: '/test.btc/path-test',
+    },
+  ]);
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
+    {
+      visits: '7',
+      pageviews: '3',
+      date: '2022-03',
+      pathname: '/test.btc/path-test-2',
+    },
+  ]);
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
 
-//   const json = jest.fn();
-//   const status = jest.fn(() => ({
-//     json,
-//   }));
+  const response = await server.inject({
+    method: 'GET',
+    url: '/api/analytics/historical?dateFrom=2022-01-02&dateGrouping=month',
+  });
 
-//   await analyticsHistoricalEndpoint(
-//     { query: { dateFrom: '2022-01-02', dateGrouping: 'month' } } as any,
-//     {
-//       status,
-//     } as any
-//   );
-//   expect(fathomClient.aggregatePath).toBeCalledTimes(25);
-//   expect(status).toBeCalledWith(200);
-//   expect(json).toMatchSnapshot();
-// });
+  expect(fathomClient.aggregatePath).toBeCalledTimes(25);
+  expect(response.statusCode).toBe(200);
+  expect(response.json()).toMatchSnapshot();
+});
 
-// it('Respond with a formatted time series for one story', async () => {
-//   // Set a fake timer so we can verify the end date
-//   jest
-//     .useFakeTimers()
-//     .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
-//     {
-//       visits: '5',
-//       pageviews: '1',
-//       date: '2022-03-20',
-//       pathname: '/test.btc/path-test',
-//     },
-//   ]);
-//   (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
+it('Respond with a formatted time series for one story', async () => {
+  // Set a fake timer so we can verify the end date
+  jest
+    .useFakeTimers(fakeTimerConfigDate)
+    .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValueOnce([
+    {
+      visits: '5',
+      pageviews: '1',
+      date: '2022-03-20',
+      pathname: '/test.btc/path-test',
+    },
+  ]);
+  (fathomClient.aggregatePath as jest.Mock).mockResolvedValue([]);
 
-//   const json = jest.fn();
-//   const status = jest.fn(() => ({
-//     json,
-//   }));
+  const response = await server.inject({
+    method: 'GET',
+    url: '/api/analytics/historical?dateFrom=2022-03-15&dateGrouping=day&storyId=test',
+  });
 
-//   await analyticsHistoricalEndpoint(
-//     {
-//       query: { dateFrom: '2022-03-15', dateGrouping: 'day', storyId: 'test' },
-//     } as any,
-//     {
-//       status,
-//     } as any
-//   );
-//   expect(fathomClient.aggregatePath).toBeCalledTimes(1);
-//   expect(status).toBeCalledWith(200);
-//   expect(json).toMatchSnapshot();
-// });
+  expect(fathomClient.aggregatePath).toBeCalledTimes(1);
+  expect(response.statusCode).toBe(200);
+  expect(response.json()).toMatchSnapshot();
+});
