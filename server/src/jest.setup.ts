620c4542eca767a7dd9ce2a30f76b5b455d6927a
Léo Pradel <pradel.leo@gmail.com>
2022-05-25 13:50:25 +0200

    fix

--- server/src/jest.setup.ts
+++ server/src/jest.setup.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-const result = dotenv.config({ path: './env.test' });
+const result = dotenv.config({ path: '.env.test' });
 
 if (result.error) {
   throw result.error;
