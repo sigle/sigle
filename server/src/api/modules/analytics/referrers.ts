98f597309331406af7cd92a70e426da62b0e97e3
Léo Pradel <pradel.leo@gmail.com>
2022-05-25 14:40:57 +0200

    feat: cache result of the analytics endpoints (#502)

--- server/src/api/modules/analytics/referrers.ts
+++ server/src/api/modules/analytics/referrers.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { isValid, parse } from 'date-fns';
 import { maxFathomFromDate, getBucketUrl, getPublicStories } from './utils';
 import { fathomClient } from '../../../external/fathom';
+import { redis } from '../../../redis';
 
 interface AnalyticsReferrersParams {
   dateFrom?: string;
@@ -70,6 +71,16 @@ export async function createAnalyticsReferrersEndpoint(
       // TODO take username from session
       const username = 'sigleapp.id.blockstack';
 
+      // Caching mechanism to avoid hitting Fathom too often
+      const cacheKey = storyId
+        ? `referrers:${username}_${dateFrom}_${storyId}`
+        : `referrers:${username}_${dateFrom}`;
+      const cachedResponse = await redis.get(cacheKey);
+      if (cachedResponse) {
+        res.status(200).send(JSON.parse(cachedResponse));
+        return;
+      }
+
       const { profile, bucketUrl } = await getBucketUrl({ username });
       if (!profile || !bucketUrl) {
         throw new Error(`No profile or bucketUrl for ${username}`);
@@ -119,6 +130,14 @@ export async function createAnalyticsReferrersEndpoint(
           return { domain, count: domainValues.count };
         })
         .sort((a, b) => b.count - a.count);
+
+      // Cache response for 1 hour
+      await redis.set(
+        cacheKey,
+        JSON.stringify(referrersResponse),
+        'EX',
+        60 * 60
+      );
 
       res.status(200).send(referrersResponse);
     }
