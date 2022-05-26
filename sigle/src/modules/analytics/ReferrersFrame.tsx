68e107cc372e727fa7a2989cbd2f949184352f8d
Greg Ogun <gregogun97@gmail.com>
2022-05-26 17:02:56 +0100

    feat: add data for referrers

--- sigle/src/modules/analytics/ReferrersFrame.tsx
+++ sigle/src/modules/analytics/ReferrersFrame.tsx
@@ -1,80 +1,28 @@
 import { stagger } from 'motion';
 import { useMotionAnimate } from 'motion-hooks';
 import { useEffect, useMemo, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Box, Flex, Text } from '../../ui';
 import { Pagination } from './Pagination';
+import { ReferrersItemProps, ReferrersResponse } from './stats/types';
 
-interface ReferrersItemProps {
-  name: string;
-  views: number;
-  id: number;
-}
+const FATHOM_MAX_FROM_DATE = '2021-04-01';
 
-const referrersMock: ReferrersItemProps[] = [
-  {
-    name: 'app.blockstack.org',
-    views: 832,
-    id: 1,
-  },
-  {
-    name: 'Facebook',
-    views: 421,
-    id: 2,
-  },
-  {
-    name: 'Twitter',
-    views: 124,
-    id: 3,
-  },
-  {
-    name: 'Instagram',
-    views: 92,
-    id: 4,
-  },
-  {
-    name: 'Medium',
-    views: 85,
-    id: 5,
-  },
-  {
-    name: 'Google',
-    views: 63,
-    id: 6,
-  },
-  {
-    name: 'linktr.ee',
-    views: 60,
-    id: 7,
-  },
-  {
-    name: 'DuckDuckGo',
-    views: 42,
-    id: 8,
-  },
-  {
-    name: 'Linkedin',
-    views: 39,
-    id: 9,
-  },
-  {
-    name: 'Reddit',
-    views: 12,
-    id: 10,
-  },
-  {
-    name: 'Substack',
-    views: 10,
-    id: 11,
-  },
-  {
-    name: 'Yahoo!',
-    views: 6,
-    id: 12,
-  },
-];
+const fetchReferrers = async () => {
+  const url = `http://localhost:3001/api/analytics/referrers?dateFrom=${FATHOM_MAX_FROM_DATE}`;
+
+  const statsRes = await fetch(url);
+  const referrerData: ReferrersResponse = await statsRes.json();
+  console.log(referrerData);
+  return referrerData;
+};
 
 export const ReferrersFrame = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const { data: referrers } = useQuery<ReferrersResponse, Error>(
+    ['fetchReferrers'],
+    fetchReferrers
+  );
   const { play } = useMotionAnimate(
     '.referrer-item',
     { opacity: 1 },
@@ -95,18 +43,20 @@ export const ReferrersFrame = () => {
   const currentReferrers = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * itemSize;
     const lastPageIndex = firstPageIndex + itemSize;
-    return referrersMock.slice(firstPageIndex, lastPageIndex);
+    return referrers?.slice(firstPageIndex, lastPageIndex);
   }, [currentPage]);
 
-  const hasNextPage =
-    currentReferrers[currentReferrers.length - 1] ===
-    referrersMock[referrersMock.length - 1]
-      ? false
-      : true;
+  const currentReferrerLastItem =
+    currentReferrers && currentReferrers[currentReferrers.length - 1];
 
-  const total =
-    referrersMock &&
-    referrersMock.map((item) => item.views).reduce((a, b) => a + b);
+  const referrerLastItem = referrers && referrers[referrers.length - 1];
+
+  const hasNextPage =
+    currentReferrerLastItem === referrerLastItem ? false : true;
+
+  const total = referrers
+    ? referrers.map((item) => item.count).reduce((a, b) => a + b)
+    : 0;
 
   const getPercentage = (views: number) => {
     const percentage = Math.round((100 * views) / total);
@@ -127,7 +77,7 @@ export const ReferrersFrame = () => {
             <Flex
               className="referrer-item"
               css={{ opacity: 0 }}
-              key={referrer.id}
+              key={referrer.domain}
               gap="5"
               justify="between"
               align="center"
@@ -142,13 +92,13 @@ export const ReferrersFrame = () => {
                     p: '$1',
                     br: '$1',
                     backgroundColor: '$gray3',
-                    width: `${getPercentage(referrer.views)}%`,
+                    width: `${getPercentage(referrer.count)}%`,
                   }}
                 >
-                  <Text size="sm">{referrer.name}</Text>
+                  <Text size="sm">{referrer.domain}</Text>
                 </Box>
               </Box>
-              <Text size="sm">{referrer.views}</Text>
+              <Text size="sm">{referrer.count}</Text>
             </Flex>
           ))}
         </Flex>
