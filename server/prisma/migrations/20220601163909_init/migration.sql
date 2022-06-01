d16ae8ea899418531f543ce950cf30c2485028e7
Léo Pradel <pradel.leo@gmail.com>
2022-06-01 18:39:44 +0200

    use uuid and created + updated fields

--- server/prisma/migrations/20220601163909_init/migration.sql
+++ server/prisma/migrations/20220601163909_init/migration.sql
@@ -1,7 +1,9 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "stacksAddress" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
