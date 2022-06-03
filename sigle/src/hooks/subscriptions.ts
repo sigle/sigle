23e63f6c603f6bcfed1e9fbc58a307b117f0cd01
Léo Pradel <pradel.leo@gmail.com>
2022-06-03 15:51:00 +0200

    move mutation to hooks

--- sigle/src/hooks/subscriptions.ts
+++ sigle/src/hooks/subscriptions.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { sigleConfig } from '../config';
 
 export const useGetUserSubscription = () =>
@@ -17,3 +17,30 @@ export const useGetUserSubscription = () =>
       return json as { id: string; nftId: number } | null;
     })
   );
+
+export const useCreateSubscription = () => {
+  const cache = useQueryClient();
+
+  return useMutation(
+    (nftId: number) =>
+      fetch(`${sigleConfig.apiUrl}/api/subscriptions/creatorPlus`, {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({ nftId }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(async (res) => {
+        const json = await res.json();
+        if (!res.ok) {
+          throw json;
+        }
+        return json as { id: string; nftId: number } | null;
+      })
+    // {
+    //   onSuccess: () => {
+    //     cache.invalidateQueries('get-user-subscription');
+    //   },
+    // }
+  );
+};
