5eba22dda22c16b075e1945476697225739628dc
Léo Pradel <pradel.leo@gmail.com>
2022-06-03 15:17:59 +0200

    protect single analytics page

--- sigle/src/pages/analytics/[storyId].page.tsx
+++ sigle/src/pages/analytics/[storyId].page.tsx
@@ -1,7 +1,12 @@
 import { StoryItemAnalytics } from '../../modules/analytics/containers/StoryItemAnalytics';
+import { Protected } from '../../modules/auth/Protected';
 
 export const StoryAnalyticsPage = () => {
-  return <StoryItemAnalytics />;
+  return (
+    <Protected>
+      <StoryItemAnalytics />
+    </Protected>
+  );
 };
 
 export default StoryAnalyticsPage;
