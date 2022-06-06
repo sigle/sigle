b55ccf6dd6aed6b1408c0cb24d77f6ace6c92e87
Greg Ogun <gregogun97@gmail.com>
2022-06-06 11:47:03 +0100

    fix: fix header width on profile page

--- sigle/src/modules/publicHome/components/PublicHome.tsx
+++ sigle/src/modules/publicHome/components/PublicHome.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 import { StoryFile, SettingsFile } from '../../../types';
@@ -8,33 +6,26 @@ import { Container } from '../../../components';
 import { PublicStoryItem } from './PublicStoryItem';
 import { PoweredBy } from '../../publicStory/PoweredBy';
 import { AppHeader } from '../../layout/components/AppHeader';
-import { Box, Flex, Text } from '../../../ui';
+import { Box, Flex, Typography } from '../../../ui';
 import { sigleConfig } from '../../../config';
+import { styled } from '../../../stitches.config';
 
-const StyledContainer = styled(Container)`
-  ${tw`pt-4 pb-16`};
-  max-width: 768px;
-`;
+const StyledContainer = styled(Container, {
+  pt: '$4',
+  pb: '$15',
+  maxWidth: 768,
+});
 
-const NoStories = styled.p`
-  ${tw`mt-8 text-center`};
-`;
+const Header = styled('div', {
+  py: '$10',
+  display: 'flex',
+  flexDirection: 'column',
+  placeItems: 'center',
+});
 
-const Header = styled(Container)`
-  ${tw`py-12 flex flex-col items-center`};
-`;
-
-const HeaderLogo = styled.img`
-  ${tw`mb-4`};
-`;
-
-const HeaderName = styled.div`
-  ${tw`text-3xl font-bold text-center`};
-`;
-
-const HeaderDescription = styled.p`
-  ${tw`text-base mt-2 text-center`};
-`;
+const HeaderLogo = styled('img', {
+  mb: '$4',
+});
 
 const PublicHomeSiteUrl = ({ siteUrl }: { siteUrl: string }) => {
   let displayUrl = siteUrl;
@@ -48,7 +39,7 @@ const PublicHomeSiteUrl = ({ siteUrl }: { siteUrl: string }) => {
   }
 
   return (
-    <Text
+    <Typography
       css={{
         color: '$gray9',
 
@@ -59,14 +50,14 @@ const PublicHomeSiteUrl = ({ siteUrl }: { siteUrl: string }) => {
           color: '$gray12',
         },
       }}
-      size="sm"
+      size="subheading"
       as="a"
       href={fullUrl}
       target="_blank"
       rel="noreferrer"
     >
       {displayUrl}
-    </Text>
+    </Typography>
   );
 };
 
@@ -116,56 +107,61 @@ export const PublicHome = ({ file, settings }: PublicHomeProps) => {
           cardType: 'summary',
         }}
       />
-
-      <Container>
-        <AppHeader />
-        <Header>
-          {settings.siteLogo && (
-            <HeaderLogo src={settings.siteLogo} alt={`${siteName} logo`} />
+      <AppHeader />
+      <Header>
+        {settings.siteLogo && (
+          <HeaderLogo src={settings.siteLogo} alt={`${siteName} logo`} />
+        )}
+        <Typography css={{ fontWeight: 700 }} as="h1" size="h2">
+          {siteName}
+        </Typography>
+        {settings.siteDescription &&
+          settings.siteDescription.split('\n').map((text, index) => (
+            <Typography
+              size="subheading"
+              css={{ mt: '$2', textAlign: 'center' }}
+              key={index}
+            >
+              {text}
+            </Typography>
+          ))}
+        <Flex css={{ pt: '$5' }} gap="3">
+          {settings.siteUrl && <PublicHomeSiteUrl siteUrl={settings.siteUrl} />}
+          {settings.siteUrl && settings.siteTwitterHandle && (
+            <Box css={{ width: '1px', backgroundColor: '$gray9' }} />
           )}
-          <HeaderName>{siteName}</HeaderName>
-          {settings.siteDescription &&
-            settings.siteDescription
-              .split('\n')
-              .map((text, index) => (
-                <HeaderDescription key={index}>{text}</HeaderDescription>
-              ))}
-          <Flex css={{ pt: '$5' }} gap="3">
-            {settings.siteUrl && (
-              <PublicHomeSiteUrl siteUrl={settings.siteUrl} />
-            )}
-            {settings.siteUrl && settings.siteTwitterHandle && (
-              <Box css={{ width: '1px', backgroundColor: '$gray9' }} />
-            )}
-            {settings.siteTwitterHandle && (
-              <Text
-                css={{
-                  color: '$gray9',
+          {settings.siteTwitterHandle && (
+            <Typography
+              css={{
+                color: '$gray9',
 
-                  '&:hover': {
-                    color: '$gray10',
-                  },
-                  '&:active': {
-                    color: '$gray12',
-                  },
-                }}
-                size="sm"
-                as="a"
-                href={`https://twitter.com/${twitterHandle}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {twitterHandle?.includes('@')
-                  ? twitterHandle
-                  : `@${twitterHandle}`}
-              </Text>
-            )}
-          </Flex>
-        </Header>
-      </Container>
+                '&:hover': {
+                  color: '$gray10',
+                },
+                '&:active': {
+                  color: '$gray12',
+                },
+              }}
+              size="subheading"
+              as="a"
+              href={`https://twitter.com/${twitterHandle}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {twitterHandle?.includes('@')
+                ? twitterHandle
+                : `@${twitterHandle}`}
+            </Typography>
+          )}
+        </Flex>
+      </Header>
 
       <StyledContainer>
-        {file.stories.length === 0 && <NoStories>No stories yet</NoStories>}
+        {file.stories.length === 0 && (
+          <Typography css={{ mt: '$8', textAlign: 'center' }}>
+            No stories yet
+          </Typography>
+        )}
         {featuredStoryIndex !== -1 && (
           <PublicStoryItem
             username={username}
