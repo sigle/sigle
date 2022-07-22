fb6b6d21adde930aa3814a488fd48ad0f7784052
Léo Pradel <pradel.leo@gmail.com>
2022-07-22 15:07:38 +0200

    feat: infinite loading explore page

--- server/src/api/modules/users/explore.ts
+++ server/src/api/modules/users/explore.ts
@@ -20,6 +20,7 @@ const getUserExploreResponseSchema = {
 export async function createGetUserExploreEndpoint(fastify: FastifyInstance) {
   return fastify.get<{
     Reply: GetUserExploreResponse;
+    Params: { page?: number };
   }>(
     '/api/users/explore',
     {
@@ -27,17 +28,28 @@ export async function createGetUserExploreEndpoint(fastify: FastifyInstance) {
       schema: {
         description: 'Return a list of users using Sigle.',
         tags: ['user'],
+        params: {
+          page: { type: 'number' },
+        },
         response: {
           200: getUserExploreResponseSchema,
         },
       },
     },
     async (req, res) => {
+      let { page } = req.params;
+      if (!page || page === 0) {
+        page = 1;
+      }
+      // TODO set to 50 before merging
+      const pageSize = 20;
+
       const users = await prisma.user.findMany({
         // Remove the current logged in user from the list
         where: { stacksAddress: { not: req.address } },
         orderBy: { followers: { _count: 'desc' } },
-        take: 150,
+        skip: pageSize * page,
+        take: pageSize,
       });
       return res.send(users);
     }
