57a6558be6f62f997aab21e6e3212918ee6de556
Léo Pradel <pradel.leo@gmail.com>
2022-07-22 12:34:05 +0200

    add some logs

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -6,7 +6,7 @@ import { PrismaClient } from '@prisma/client';
 const main = async () => {
   const prisma = new PrismaClient();
 
-  await prisma.user.createMany({
+  const userResult = await prisma.user.createMany({
     data: [
       {
         stacksAddress: 'SP3VCX5NFQ8VCHFS9M6N40ZJNVTRT4HZ62WFH5C4Q',
@@ -23,8 +23,23 @@ const main = async () => {
       {
         stacksAddress: 'SPQE3J7XMMK0DN0BWJZHGE6B05VDYQRXRMDV734D',
       },
+      {
+        stacksAddress: 'SP779SC9CDWQVMTRXT0HZCEHSDBXCHNGG7BC1H9B',
+      },
+      {
+        stacksAddress: 'SP1VCG4HXMG02BMJCSAZDBS1WR4N2YG3RPHMNP9WR',
+      },
+      {
+        stacksAddress: 'SP19N6NE3EYCM96N0Y173Z2B61MCPNDT8PQEQY166',
+      },
+      {
+        stacksAddress: 'SP13QC2G49PXXA84H083Y1PMFS2PGXM583HQ8TQ9F',
+      },
     ],
   });
+
+  console.log(`------ SEED DATABASE ------`);
+  console.log(`Created ${userResult.count} users`);
 };
 
 main();
