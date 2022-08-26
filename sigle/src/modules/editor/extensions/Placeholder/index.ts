3054b49ba7eab83d34e88a981b31ae3d9da6cd49
Léo Pradel <pradel.leo@gmail.com>
2022-08-26 12:49:11 +0200

    feat: improve editor placeholder behavior

---
+++ sigle/src/modules/editor/extensions/Placeholder/index.ts
@@ -0,0 +1,42 @@
+import TipTapPlaceholder from '@tiptap/extension-placeholder';
+
+export const Placeholder = TipTapPlaceholder.configure({
+  showOnlyWhenEditable: true,
+  includeChildren: true,
+  placeholder: ({ editor, node, pos }) => {
+    if (node.type.name === 'heading') {
+      const level = node.attrs.level as number;
+      if (level === 2) {
+        return 'Big Heading';
+      } else if (level === 3) {
+        return 'Small Heading';
+      }
+    }
+
+    if (node.type.name === 'paragraph') {
+      const parentNode = editor.state.doc.resolve(pos).parent;
+      // When user start to write, only show this when the content is empty
+      if (
+        parentNode.type.name === 'doc' &&
+        editor.getJSON().content?.length === 1
+      ) {
+        return 'Start your story here...';
+      }
+
+      if (parentNode.type.name === 'listItem') {
+        return 'List';
+      }
+
+      if (parentNode.type.name === 'blockquote') {
+        if (parentNode.content.childCount > 1) {
+          return 'Type or hit enter to exit quote';
+        }
+        return 'Quote';
+      }
+
+      return "Type '/' for commands";
+    }
+
+    return '';
+  },
+});
