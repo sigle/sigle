17c0f90e024452006b8d3ce2ee432af9c9598dbc
Léo Pradel <pradel.leo@gmail.com>
2022-09-02 14:44:46 +0200

    fix issue and add loading component

--- sigle/src/modules/editor/extensions/Twitter/TwitterComponent.tsx
+++ sigle/src/modules/editor/extensions/Twitter/TwitterComponent.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { NodeViewProps } from '@tiptap/core';
 import { NodeViewWrapper } from '@tiptap/react';
+import { Box, LoadingSpinner } from '../../../../ui';
 
 const WIDGET_SCRIPT_URL = 'https://platform.twitter.com/widgets.js';
 
@@ -25,25 +26,35 @@ const loadTwitterWidget = async (): Promise<void> => {
 
 export const TwitterComponent = (props: NodeViewProps) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const previousTweetIDRef = useRef<string>();
   const [isTweetLoading, setIsTweetLoading] = useState(false);
 
-  // TODO show loading component
-
-  const tweetId = props.node.attrs['data-twitter-id'];
-  console.log({ tweetId });
+  const tweetID = props.node.attrs['data-twitter-id'];
 
   useEffect(() => {
-    loadTwitterWidget().then(async () => {
-      // @ts-expect-error Twitter is attached to the window.
-      await window.twttr.widgets.createTweet(tweetId, containerRef.current);
-    });
-  }, [tweetId]);
+    if (tweetID !== previousTweetIDRef.current) {
+      setIsTweetLoading(true);
+      loadTwitterWidget().then(async () => {
+        // @ts-expect-error Twitter is attached to the window.
+        await window.twttr.widgets.createTweet(tweetID, containerRef.current);
+        setIsTweetLoading(false);
+      });
+      if (previousTweetIDRef) {
+        previousTweetIDRef.current = tweetID;
+      }
+    }
+  }, [setIsTweetLoading, tweetID]);
 
   return (
     <NodeViewWrapper>
+      {isTweetLoading ? (
+        <Box css={{ height: 200, display: 'flex', justifyContent: 'center' }}>
+          <LoadingSpinner />
+        </Box>
+      ) : null}
       <div
         ref={containerRef}
-        style={{ display: 'inline-block', width: '550px' }}
+        style={{ margin: '0 auto', maxWidth: '550px' }}
       ></div>
     </NodeViewWrapper>
   );
