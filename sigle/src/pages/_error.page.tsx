88c54a95060b26df3330f7cd0d1c411b19850030
Léo Pradel <pradel.leo@gmail.com>
2022-09-07 10:55:05 +0200

    Update _error.page.tsx

--- sigle/src/pages/_error.page.tsx
+++ sigle/src/pages/_error.page.tsx
@@ -218,6 +218,7 @@ MyError.getInitialProps = async (props: NextPageContext) => {
     return { statusCode: 404 };
   }
   if (err) {
+    console.error(err);
     errorInitialProps.sentryErrorId = Sentry.captureException(err);
 
     // Flushing before returning is necessary if deploying to Vercel, see
