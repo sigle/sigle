69671a2525cb7c8f146cb8377b746ace426240c0
Léo Pradel <pradel.leo@gmail.com>
2022-09-15 13:33:43 +0200

    setup swagger

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -4,6 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,15 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Sigle API')
+    // TODO get version from package.json
+    .setVersion('0.1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   const configService = app.get(ConfigService<{ PORT: string }>);
   await app.listen(configService.get('PORT') || 3000);
 }
