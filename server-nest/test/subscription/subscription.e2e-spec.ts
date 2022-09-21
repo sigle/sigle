a2b1e11dfaa1b0b8685586dc93327620c00f50c7
Léo Pradel <pradel.leo@gmail.com>
2022-09-21 11:45:20 +0200

    add tests

---
+++ server-nest/test/subscription/subscription.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCookie from '@fastify/cookie';
+import { AppModule } from '../../src/app.module';
+
+describe('SubscriptionController (e2e)', () => {
+  let app: NestFastifyApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
+
+    // @ts-ignore - types are off
+    app.register(fastifyCookie);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/api/subscriptions (GET) - return null ', async () => {
+    const stacksAddress = 'SP3VCX5NFQ8VCHFS9M6N40ZJNVTRT4HZ62WFH5C4Q';
+    const result = await app.inject({
+      method: 'GET',
+      url: '/api/subscriptions',
+      cookies: {
+        'next-auth.session-token': stacksAddress,
+      },
+    });
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.payload)).toEqual(null);
+  });
+
+  it('/api/subscriptions (GET) - return subscription ', async () => {
+    const stacksAddress = 'SP3VCX5NFQ8VCHFS9M6N40ZJNVTRT4HZ62WFH5C4Q';
+    const result = await app.inject({
+      method: 'GET',
+      url: '/api/subscriptions',
+      cookies: {
+        'next-auth.session-token': stacksAddress,
+      },
+    });
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.payload)).toEqual({
+      id: expect.any(String),
+      nftId: 980,
+    });
+  });
+});
