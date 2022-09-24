3179ac7b0d9c2b4d46c1d0b59feda1afa85449b8
Léo Pradel <pradel.leo@gmail.com>
2022-09-24 17:43:09 +0200

    fix test leak

---
+++ server-nest/src/redis/redis.service.ts
@@ -0,0 +1,29 @@
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+import { EnvironmentVariables } from '../environment/environment.validation';
+
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+  public redis: Redis;
+
+  constructor(
+    private readonly configService: ConfigService<EnvironmentVariables>,
+  ) {}
+
+  async onModuleInit() {
+    if (!this.redis) {
+      this.redis = new Redis(this.configService.get('REDIS_DATABASE_URL'));
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.redis.quit();
+  }
+}
