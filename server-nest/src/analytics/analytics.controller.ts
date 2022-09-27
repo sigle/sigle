4c9fef59df5a29be9032c56ee1b741db7b71d299
Léo Pradel <pradel.leo@gmail.com>
2022-09-27 16:09:31 +0200

    referrers

--- server-nest/src/analytics/analytics.controller.ts
+++ server-nest/src/analytics/analytics.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth.guard';
+import { AnalyticsService } from './analytics.service';
+import { ReferrersQueryDto } from './dto/referrersQuery.dto';
 
-@Controller('analytics')
-export class AnalyticsController {}
+@ApiTags('analytics')
+@Controller()
+export class AnalyticsController {
+  constructor(private readonly analyticsService: AnalyticsService) {}
+  @ApiOperation({
+    description: 'Return the referrer statistics.',
+  })
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    schema: {
+      example: [
+        { domain: 'twitter.com', count: 5 },
+        { domain: 'google.com', count: 2 },
+      ],
+    },
+  })
+  @UseGuards(AuthGuard)
+  @Get('/api/analytics/referrers')
+  getReferrers(@Request() req, @Query() query: ReferrersQueryDto) {
+    return this.analyticsService.referrers({
+      stacksAddress: req.user.stacksAddress,
+      dateFrom: query.dateFrom,
+      storyId: query.storyId,
+    });
+  }
+}
