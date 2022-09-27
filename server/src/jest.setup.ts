2f0b4f26109c10c18b2e6faf7c5441b28b834a8c
Léo Pradel <pradel.leo@gmail.com>
2022-09-27 16:40:01 +0200

    delete old files

--- server/src/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-const result = dotenv.config({ path: '.env.test' });
-
-if (result.error) {
-  throw result.error;
-}
--- server/src/redis.ts
+++
@@ -1,4 +0,0 @@
-import Redis from 'ioredis';
-import { config } from './config';
-
-export const redis = new Redis(config.REDIS_DATABASE_URL);
