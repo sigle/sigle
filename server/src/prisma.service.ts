1d2c0790c7dd705d010e7716e4e337f98a42edec
Léo Pradel <pradel.leo@gmail.com>
2022-09-28 16:51:21 +0200

    fix prisma connection

--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -1,10 +1,22 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
