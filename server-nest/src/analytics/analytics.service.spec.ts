1daa133b4d5615d08bf1ad914bea087e3da9f68e
Léo Pradel <pradel.leo@gmail.com>
2022-09-28 15:36:37 +0200

    test historical

--- server-nest/src/analytics/analytics.service.spec.ts
+++ server-nest/src/analytics/analytics.service.spec.ts
@@ -53,6 +53,8 @@ describe('AnalyticsService', () => {
           provide: PlausibleService,
           useValue: {
             referrers: jest.fn(),
+            timeseries: jest.fn(),
+            pages: jest.fn(),
           },
         },
       ],
@@ -112,6 +114,141 @@ describe('AnalyticsService', () => {
         dateFrom: '2022-05-01',
         dateTo: '2022-04-04',
         paths: ['/sigleapp.id.blockstack/test'],
+      });
+      expect(data).toMatchSnapshot();
+    });
+  });
+
+  describe('historical', () => {
+    it('should return a formatted time series for days', async () => {
+      jest
+        .useFakeTimers()
+        .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+      const stacksAddress = 'SP2BKHGRV8H2YDJK16FP5VASYFDGYN4BPTNFWDKYJ';
+      (plausibleService.timeseries as jest.Mock).mockResolvedValueOnce([
+        {
+          visitors: '5',
+          pageviews: '1',
+          date: '2022-03-20',
+        },
+        {
+          visitors: '7',
+          pageviews: '3',
+          date: '2022-03-20',
+        },
+      ]);
+      (plausibleService.pages as jest.Mock).mockResolvedValueOnce([
+        {
+          visitors: '5',
+          pageviews: '1',
+          page: '/test.btc/path-test',
+        },
+        {
+          visitors: '7',
+          pageviews: '3',
+          page: '/test.btc/path-test-2',
+        },
+      ]);
+
+      const data = await service.historical({
+        stacksAddress,
+        dateFrom: '2022-03-15',
+        dateGrouping: 'day',
+      });
+      expect(plausibleService.timeseries).toBeCalledWith({
+        dateFrom: '2022-05-01',
+        dateGrouping: 'day',
+        dateTo: '2022-04-04',
+        paths: expect.any(Array),
+      });
+      expect(plausibleService.pages).toBeCalledWith({
+        dateFrom: '2022-05-01',
+        dateTo: '2022-04-04',
+        paths: expect.any(Array),
+      });
+      expect(data).toMatchSnapshot();
+    });
+
+    it('should return a formatted time series for months', async () => {
+      jest
+        .useFakeTimers()
+        .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+      const stacksAddress = 'SP2BKHGRV8H2YDJK16FP5VASYFDGYN4BPTNFWDKYJ';
+      (plausibleService.timeseries as jest.Mock).mockResolvedValueOnce([
+        {
+          visitors: '5',
+          pageviews: '1',
+          date: '2022-03-01',
+        },
+      ]);
+      (plausibleService.pages as jest.Mock).mockResolvedValueOnce([
+        {
+          visitors: '5',
+          pageviews: '1',
+          page: '/test.btc/path-test',
+        },
+        {
+          visitors: '7',
+          pageviews: '3',
+          page: '/test.btc/path-test-2',
+        },
+      ]);
+
+      const data = await service.historical({
+        stacksAddress,
+        dateFrom: '2022-01-02',
+        dateGrouping: 'month',
+      });
+      expect(plausibleService.timeseries).toBeCalledWith({
+        dateFrom: '2022-05-01',
+        dateGrouping: 'day',
+        dateTo: '2022-04-04',
+        paths: expect.any(Array),
+      });
+      expect(plausibleService.pages).toBeCalledWith({
+        dateFrom: '2022-05-01',
+        dateTo: '2022-04-04',
+        paths: expect.any(Array),
+      });
+      expect(data).toMatchSnapshot();
+    });
+
+    it('should return a formatted time series for one story', async () => {
+      jest
+        .useFakeTimers()
+        .setSystemTime(new Date('2022-04-04 12:00:00 UTC').getTime());
+      const stacksAddress = 'SP2BKHGRV8H2YDJK16FP5VASYFDGYN4BPTNFWDKYJ';
+      (plausibleService.timeseries as jest.Mock).mockResolvedValueOnce([
+        {
+          visitors: '5',
+          pageviews: '1',
+          date: '2022-03-01',
+        },
+      ]);
+      (plausibleService.pages as jest.Mock).mockResolvedValueOnce([
+        {
+          visitors: '5',
+          pageviews: '1',
+          page: '/test.btc/path-test',
+        },
+      ]);
+
+      const data = await service.historical({
+        stacksAddress,
+        dateFrom: '2022-03-15',
+        dateGrouping: 'day',
+        storyId: 'test',
+      });
+      expect(plausibleService.timeseries).toBeCalledWith({
+        dateFrom: '2022-05-01',
+        dateGrouping: 'day',
+        dateTo: '2022-04-04',
+        paths: expect.any(Array),
+      });
+      expect(plausibleService.pages).toBeCalledWith({
+        dateFrom: '2022-05-01',
+        dateTo: '2022-04-04',
+        paths: expect.any(Array),
       });
       expect(data).toMatchSnapshot();
     });
