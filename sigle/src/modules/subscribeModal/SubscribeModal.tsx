4b1585cf5b5d67d15a89aaf37be828e5421871ae
Greg Ogun <gregogun97@gmail.com>
2022-11-14 16:41:03 +0000

    feat: add email validation and success state

--- sigle/src/modules/subscribeModal/SubscribeModal.tsx
+++ sigle/src/modules/subscribeModal/SubscribeModal.tsx
@@ -1,6 +1,9 @@
+import { FormikErrors, useFormik } from 'formik';
+import Image from 'next/future/image';
+import Link from 'next/link';
+import { useState } from 'react';
 import { styled } from '../../stitches.config';
 import {
-  Box,
   Button,
   Dialog,
   DialogContent,
@@ -8,9 +11,11 @@ import {
   DialogTitle,
   Flex,
   FormHelper,
+  FormHelperError,
   FormInput,
   Typography,
 } from '../../ui';
+import { isValidEmail } from '../../utils/regex';
 
 const HeaderLogoContainer = styled('div', {
   mx: 'auto',
@@ -31,6 +36,10 @@ const HeaderLogo = styled('img', {
   objectFit: 'cover',
 });
 
+interface SettingsFormValues {
+  email: string;
+}
+
 interface SubscribeModalProps {
   open: boolean;
   onClose: () => void;
@@ -44,19 +53,70 @@ export const SubscribeModal = ({
   open,
   onClose,
 }: SubscribeModalProps) => {
+  // temp state to show success state of form
+  const [success, setSuccess] = useState(false);
+  const formik = useFormik<SettingsFormValues>({
+    initialValues: {
+      email: '',
+    },
+    validate: (values) => {
+      const errors: FormikErrors<SettingsFormValues> = {};
+
+      if (!values.email) {
+        errors.email = 'No email has been entered';
+      }
+
+      if (values.email && values.email.length > 100) {
+        errors.email = 'Name too long';
+      }
+
+      if (values.email && !isValidEmail(values.email)) {
+        errors.email = 'Invalid email, enter a new one.';
+      }
+
+      return errors;
+    },
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values, { setSubmitting, validateForm }) => {
+      validateForm();
+      formik.resetForm({ values: { ...values } });
+      setSuccess(true);
+      setSubmitting(false);
+    },
+  });
+
   return (
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent css={{ p: '$5', br: 28 }} closeButton={false}>
-        <Box as="form">
-          <HeaderLogoContainer>
-            <HeaderLogo src={profileImgSrc} alt={`${siteName} logo`} />
-          </HeaderLogoContainer>
+      <DialogContent css={{ p: '$5', br: 28 }} closeButton={success}>
+        <Flex
+          direction="column"
+          css={{
+            alignItems: success ? 'center' : 'start',
+          }}
+          onSubmit={formik.handleSubmit}
+          as="form"
+        >
+          {success ? (
+            <Flex css={{ mb: '$4' }} justify="center">
+              <Image
+                src="/static/img/success.gif"
+                alt="Success GIF"
+                width={92}
+                height={92}
+              />
+            </Flex>
+          ) : (
+            <HeaderLogoContainer>
+              <HeaderLogo src={profileImgSrc} alt={`${siteName} logo`} />
+            </HeaderLogoContainer>
+          )}
           <DialogTitle asChild>
             <Typography
               css={{ fontWeight: 600, mt: '$4', textAlign: 'center' }}
               size="h3"
             >
-              {siteName}
+              {success ? `Successfully subscribed!` : siteName}
             </Typography>
           </DialogTitle>
           <DialogDescription asChild>
@@ -64,28 +124,62 @@ export const SubscribeModal = ({
               css={{ mt: '$1', mb: '$6', textAlign: 'center' }}
               size="subheading"
             >
-              {`            Enter your email to receive ${siteName}'s new stories in your 
-            mailbox`}
+              {success ? (
+                <span>
+                  {`You just subscribed to ${siteName}’s newsletter.`} <br />
+                  See you soon in your mailbox!
+                </span>
+              ) : (
+                `            Enter your email to receive ${siteName}'s new stories in your 
+            mailbox`
+              )}
             </Typography>
           </DialogDescription>
-          <FormInput
-            css={{ width: '100%' }}
-            type="email"
-            placeholder="johndoe@gmail.com"
-          />
-          <FormHelper css={{ mt: '$2', color: '$gray11', fontSize: '$1' }}>
-            Your email address will be saved in the settings for future
-            subscriptions
-          </FormHelper>
-          <Flex justify="end" gap="5" css={{ mt: '$6' }}>
-            <Button size="lg" variant="ghost" color="gray" onClick={onClose}>
-              Cancel
-            </Button>
-            <Button type="submit" size="lg" color="orange">
-              Submit
-            </Button>
-          </Flex>
-        </Box>
+          {success ? (
+            <Link href="/" passHref>
+              <Button as="a">Go back to my dashboard</Button>
+            </Link>
+          ) : (
+            <>
+              <FormInput
+                name="email"
+                maxLength={100}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                css={{ width: '100%' }}
+                type="email"
+                placeholder="johndoe@gmail.com"
+              />
+              {formik.errors.email && !formik.values.email && (
+                <FormHelperError css={{ mt: '$2' }}>
+                  {formik.errors.email}
+                </FormHelperError>
+              )}
+              <FormHelper css={{ mt: '$2', color: '$gray11', fontSize: '$1' }}>
+                Your email address will be saved in the settings for future
+                subscriptions
+              </FormHelper>
+              <Flex gap="5" css={{ mt: '$6', alignSelf: 'end' }}>
+                <Button
+                  size="lg"
+                  variant="ghost"
+                  color="gray"
+                  onClick={onClose}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  disabled={formik.isSubmitting}
+                  type="submit"
+                  size="lg"
+                  color="orange"
+                >
+                  Submit
+                </Button>
+              </Flex>
+            </>
+          )}
+        </Flex>
       </DialogContent>
     </Dialog>
   );
