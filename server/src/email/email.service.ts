83f8a2e1a5964420a58666acf1f75e62ce88e731
Léo Pradel <pradel.leo@gmail.com>
2022-11-21 18:06:24 +0100

    compile template with handlebars

--- server/src/email/email.service.ts
+++ server/src/email/email.service.ts
@@ -1,5 +1,7 @@
+import { readFileSync } from 'fs';
 import { Injectable } from '@nestjs/common';
 import { parse } from 'himalaya';
+import { compile } from 'handlebars';
 import { Story } from '../external/gaia';
 
 const inlineAttributes = (
@@ -33,6 +35,18 @@ const inlineText = (json: any[]): string => {
 
 @Injectable()
 export class EmailService {
+  private templates: { storyEmail: HandlebarsTemplateDelegate };
+
+  constructor() {
+    this.templates = {
+      storyEmail: compile(
+        readFileSync(`${process.cwd()}/src/email/templates/story.handlebars`, {
+          encoding: 'utf-8',
+        }).toString(),
+      ),
+    };
+  }
+
   /**
    * Convert an article HTML content to the corresponding mjml template.
    */
@@ -65,79 +79,19 @@ export class EmailService {
       throw new Error('Story content version 1 not allowed.');
     }
 
-    const username = 'Antoine Lebowitch';
-
-    const template = `<mjml>
-    <mj-head>
-      <mj-font
-        name="Open Sans"
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
-      />
-      <mj-attributes>
-        <mj-text font-family="Open Sans" font-size="15px" line-height="20px" />
-        <mj-divider border-color="#E2E2E2" border-width="1px" />
-      </mj-attributes>
-    </mj-head>
-    <mj-body>
-      <mj-section>
-        <mj-column>
-          ${this.htmlToMJML(story.content)}
-        </mj-column>
-      </mj-section>
-      <mj-section padding="0px 25px">
-        <mj-column
-          background-color="#F6F6F6"
-          border-radius="12px"
-          padding="20px 0px"
-        >
-          <mj-text align="center"
-            ><a href="TODO"
-              ><img
-                src="https://app.sigle.io/img/emails/globe.png"
-                width="15"
-                height="15"
-                style="margin-right: 12px" /></a
-            ><a href="TODO"
-              ><img
-                src="https://app.sigle.io/img/emails/twitter.png"
-                width="15"
-                height="15" /></a
-          ></mj-text>
-          <mj-text align="center" color="#737373"
-            ><a href="TODO" style="color: #737373; font-weight: 600"
-              >Unsubscribe</a
-            >
-            from ${username}’s Newsletter.</mj-text
-          >
-          <mj-text align="center"
-            ><a href="TODO"
-              ><img
-                src="https://app.sigle.io/img/emails/cta.png"
-                width="143"
-                height="28" /></a
-          ></mj-text>
-        </mj-column>
-      </mj-section>
-      <mj-section>
-        <mj-column>
-          <mj-divider />
-          <mj-text color="#737373"
-            >If you were forwarded this newsletter and you like it, you can
-            subscribe
-            <a href="TODO" style="color: #737373; font-weight: 600">here</a
-            >.</mj-text
-          >
-          <mj-divider />
-          <mj-text align="center" color="#737373">© Sigle 2022</mj-text>
-        </mj-column>
-      </mj-section>
-    </mj-body>
-  </mjml>`;
+    return this.templates.storyEmail(
+      {
+        content: this.htmlToMJML(story.content),
+        user: {
+          username: 'antoine.btc',
+          displayName: 'Antoine Lebowitch',
+        },
+      },
+      {},
+    );
 
     // TODO CTA template
     // TODO integrate https://www.npmjs.com/package/mjml-bullet-list for lists
     // TODO unsubscribe link
-
-    return template;
   }
 }
