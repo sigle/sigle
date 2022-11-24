7722378620d2d584a8d05f39bcf5a7f553ba2e31
Léo Pradel <pradel.leo@gmail.com>
2022-11-24 12:59:40 +0100

    Update subscribers.service.ts

--- server/src/subscribers/subscribers.service.ts
+++ server/src/subscribers/subscribers.service.ts
@@ -1,6 +1,6 @@
 import { BadGatewayException, Injectable } from '@nestjs/common';
 import { validate } from 'deep-email-validator';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateSubscriberDto } from './dto/createSubscriber.dto';
 
 @Injectable()
