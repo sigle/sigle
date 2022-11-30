0cf95b7b9559aef8a8af81b836b44174881aa358
Léo Pradel <pradel.leo@gmail.com>
2022-11-30 12:17:49 +0100

    publish, unpublish, delete server

--- server/src/stories/stories.controller.ts
+++ server/src/stories/stories.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { StoriesService } from './stories.service';
 import { PublishStoryDto } from './dto/publishStory.dto';
+import { AuthGuard } from '../auth.guard';
 
 @ApiTags('stories')
 @Controller()
 export class StoriesController {
   constructor(private readonly storiesService: StoriesService) {}
 
+  @UseGuards(AuthGuard)
   @Post('/api/stories/publish')
-  publish(@Body() publishStoryDto: PublishStoryDto) {
-    return this.storiesService.publish(publishStoryDto);
+  publish(@Request() req, @Body() publishStoryDto: PublishStoryDto) {
+    return this.storiesService.publish({
+      stacksAddress: req.user.stacksAddress,
+      gaiaId: publishStoryDto.id,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('/api/stories/unpublish')
+  unpublish(@Request() req, @Body() publishStoryDto: PublishStoryDto) {
+    return this.storiesService.unpublish({
+      stacksAddress: req.user.stacksAddress,
+      gaiaId: publishStoryDto.id,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('/api/stories/delete')
+  delete(@Request() req, @Body() publishStoryDto: PublishStoryDto) {
+    return this.storiesService.delete({
+      stacksAddress: req.user.stacksAddress,
+      gaiaId: publishStoryDto.id,
+    });
   }
 }
