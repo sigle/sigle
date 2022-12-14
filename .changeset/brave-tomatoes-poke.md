34037c8ccba3f96b03209118d3cbbfd675e55888
Léo Pradel <pradel.leo@gmail.com>
2022-12-14 18:25:27 +0100

    fix: fix BNS without profile not being detected

---
+++ .changeset/brave-tomatoes-poke.md
@@ -0,0 +1,5 @@
+---
+'@sigle/app': patch
+---
+
+Fix BNS without profile not being detected.
--- sigle/src/modules/auth/Protected.tsx
+++ sigle/src/modules/auth/Protected.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
+import { parseZoneFile } from 'zone-file';
 import { useAuth } from './AuthContext';
 import { FullScreenLoading } from '../layout/components/FullScreenLoading';
 
@@ -26,6 +27,18 @@ export const Protected = ({ children }: Props) => {
           // If the zonefile is non existant or empty the user needs to configure it
           if (!namesJson.zonefile || namesJson.zonefile === '') {
             router.push('/configure-bns');
+          }
+          // If missing the profile url the user needs to configure it
+          if (namesJson.zonefile) {
+            const parseZoneFileResult: {
+              uri?: { name: string; target: string }[];
+            } = parseZoneFile(namesJson.zonefile);
+            const profileUrl = parseZoneFileResult.uri?.find(
+              (uri) => uri.name === '_http._tcp'
+            );
+            if (!profileUrl) {
+              router.push('/configure-bns');
+            }
           }
         } catch (e) {}
       }
