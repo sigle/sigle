e84d184f46aa1b7b348e03e61cac22a177ae7031
Léo Pradel <pradel.leo@gmail.com>
2023-01-05 13:51:30 +0100

    feat: user setup newsletter settings

--- sigle/src/modules/analytics/NftLockedView.tsx
+++ sigle/src/modules/analytics/NftLockedView.tsx
@@ -5,80 +5,75 @@ import { DashboardLayout } from '../layout';
 
 export const NftLockedView = () => {
   return (
-    <DashboardLayout>
-      <Box
-        css={{
-          display: 'grid',
-          placeItems: 'center',
+    <Box
+      css={{
+        display: 'grid',
+        placeItems: 'center',
 
-          '@xl': {
-            height: '100%',
-          },
-        }}
-      >
-        <Flex gap="5" direction="column" align="center">
-          <Box css={{ position: 'relative' }}>
-            <Image width={144} height={131} src="/static/img/nft_locked.gif" />
-          </Box>
-          <Typography as="h3" size="h3">
-            ✨ Creator plan ✨
+        '@xl': {
+          height: '100%',
+        },
+      }}
+    >
+      <Flex gap="5" direction="column" align="center">
+        <Box css={{ position: 'relative' }}>
+          <Image width={144} height={131} src="/static/img/nft_locked.gif" />
+        </Box>
+        <Typography as="h3" size="h3">
+          ✨ Creator plan ✨
+        </Typography>
+        <Typography css={{ textAlign: 'center' }} size="subheading">
+          Do more with Sigle and access premium features for your Web3 blog
+        </Typography>
+        <Flex
+          as="ul"
+          css={{
+            backgroundColor: '$green2',
+            br: '$2',
+            color: '$green12',
+            py: '$5',
+            px: '$10',
+          }}
+          direction="column"
+          gap="3"
+        >
+          <Typography size="subheading" as="li">
+            <span>✅</span> Analytics page
           </Typography>
-          <Typography css={{ textAlign: 'center' }} size="subheading">
-            Do more with Sigle and access premium features for your Web3 blog
+          <Typography size="subheading" as="li">
+            <span>⚙️</span> Newsletters
           </Typography>
-          <Flex
-            as="ul"
-            css={{
-              backgroundColor: '$green2',
-              br: '$2',
-              color: '$green12',
-              py: '$5',
-              px: '$10',
-            }}
-            direction="column"
-            gap="3"
-          >
-            <Typography size="subheading" as="li">
-              <span>✅</span> Analytics page
-            </Typography>
-            <Typography size="subheading" as="li">
-              <span>⚙️</span> Newsletters
-            </Typography>
-            <Typography size="subheading" as="li">
-              <span>⚙️</span> Monetisation (write & earn)
-            </Typography>
-            <Typography size="subheading" as="li">
-              <span>⚙️</span> Publish on Discover page
-            </Typography>
-            <Typography size="subheading" as="li">
-              <span>⚙️</span> Domain name
-            </Typography>
-            <Typography size="subheading" as="li">
-              <span>⚙️</span> And more...
-            </Typography>
-          </Flex>
-          <Flex direction="column">
-            <Typography
-              size="subheading"
-              css={{ textAlign: 'center', mb: '$2' }}
-            >
-              Unlock the creator plan with your Explorer Guild NFT
-            </Typography>
-            <Link href="/settings/plans" passHref legacyBehavior>
-              <Button
-                as="a"
-                size="lg"
-                color="orange"
-                css={{
-                  alignSelf: 'center',
-                }}
-              >
-                Upgrade
-              </Button>
-            </Link>
-          </Flex>
+          <Typography size="subheading" as="li">
+            <span>⚙️</span> Monetisation (write & earn)
+          </Typography>
+          <Typography size="subheading" as="li">
+            <span>⚙️</span> Publish on Discover page
+          </Typography>
+          <Typography size="subheading" as="li">
+            <span>⚙️</span> Domain name
+          </Typography>
+          <Typography size="subheading" as="li">
+            <span>⚙️</span> And more...
+          </Typography>
         </Flex>
-      </Box>
-    </DashboardLayout>
+        <Flex direction="column">
+          <Typography size="subheading" css={{ textAlign: 'center', mb: '$2' }}>
+            Unlock the creator plan with your Explorer Guild NFT
+          </Typography>
+          <Link href="/settings/plans" passHref legacyBehavior>
+            <Button
+              as="a"
+              size="lg"
+              color="orange"
+              css={{
+                alignSelf: 'center',
+              }}
+            >
+              Upgrade
+            </Button>
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
--- sigle/src/modules/settings/newsletter/Newsletter.tsx
+++ sigle/src/modules/settings/newsletter/Newsletter.tsx
@@ -1,10 +1,28 @@
+import { useGetUserSubscription } from '../../../hooks/subscriptions';
 import { Typography } from '../../../ui';
+import { NftLockedView } from '../../analytics/NftLockedView';
 import { SettingsLayout } from '../SettingsLayout';
 
-export const Newsletter = () => (
-  <SettingsLayout>
-    <Typography css={{ fontWeight: 600 }} size="h4">
-      Newsletter
-    </Typography>
-  </SettingsLayout>
-);
+export const Newsletter = () => {
+  const { isLoading, data: userSubscription } = useGetUserSubscription();
+
+  if (isLoading) {
+    return <SettingsLayout layout="wide">Loading...</SettingsLayout>;
+  }
+
+  if (!isLoading && !userSubscription) {
+    return (
+      <SettingsLayout>
+        <NftLockedView />
+      </SettingsLayout>
+    );
+  }
+
+  return (
+    <SettingsLayout>
+      <Typography css={{ fontWeight: 600 }} size="h4">
+        Newsletter
+      </Typography>
+    </SettingsLayout>
+  );
+};
