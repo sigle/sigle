867bbbf1eb5b83749c4b4b10e5c60654d4cd3ee8
Léo Pradel <pradel.leo@gmail.com>
2023-01-05 14:01:02 +0100

    enable newsletter

--- sigle/src/modules/settings/newsletter/Newsletter.tsx
+++ sigle/src/modules/settings/newsletter/Newsletter.tsx
@@ -1,5 +1,5 @@
 import { useGetUserSubscription } from '../../../hooks/subscriptions';
-import { Typography } from '../../../ui';
+import { Button, Typography, Switch, SwitchThumb, Flex } from '../../../ui';
 import { NftLockedView } from '../../analytics/NftLockedView';
 import { SettingsLayout } from '../SettingsLayout';
 
@@ -20,9 +20,24 @@ export const Newsletter = () => {
 
   return (
     <SettingsLayout>
-      <Typography css={{ fontWeight: 600 }} size="h4">
-        Newsletter
-      </Typography>
+      <Flex justify="between" gap="5">
+        <div>
+          <Typography css={{ fontWeight: 600 }} size="h4">
+            Enable Newsletter
+          </Typography>
+          <Typography size="subheading" css={{ color: '$gray9', mt: '$2' }}>
+            Switching this toggle to "off" will disable the communication
+            between Sigle and Mailjet.
+            <br />
+            You won't be able to send emails anymore.
+          </Typography>
+        </div>
+        <div>
+          <Switch checked={true}>
+            <SwitchThumb />
+          </Switch>
+        </div>
+      </Flex>
     </SettingsLayout>
   );
 };
