a206ad4ed569bea2597b67d3dba7a309a95271cf
Léo Pradel <pradel.leo@gmail.com>
2023-01-06 11:18:13 +0100

    validateAndSetupMailjetConfig

--- server/src/newsletters/newsletters.service.ts
+++ server/src/newsletters/newsletters.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ContactList } from 'node-mailjet';
 import { PrismaService } from '../prisma/prisma.service';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Mailjet = require('node-mailjet');
 
 @Injectable()
 export class NewslettersService {
@@ -21,19 +24,56 @@ export class NewslettersService {
     // TODO validate keys if changed only
     // TODO create list with new keys
     // TODO upsert newsletter with new config
+
+    await this.validateAndSetupMailjetConfig({
+      apiKey,
+      apiSecret,
+    });
   }
 
   /**
-   *
+   * Check that the credentials are correct.
+   * Setup the required Mailjet contact list if it doesn't exist.
    */
-  private validateMailjetConfig({
+  private async validateAndSetupMailjetConfig({
     apiKey,
     apiSecret,
   }: {
     apiKey: string;
     apiSecret: string;
-  }): boolean {
-    // TODO call mailjet api to verify keys are working
-    return true;
+  }): Promise<{ listId: number; listAddress: string }> {
+    const mailjet = new Mailjet({
+      apiKey,
+      apiSecret,
+    });
+    let mailjetList: ContactList.IContactList;
+    try {
+      const data: { body: ContactList.TGetContactListResponse } = await mailjet
+        .get('contactslist?Name=sigle', { version: 'v3' })
+        .request();
+      if (data.body.Count >= 1) {
+        mailjetList = data.body.Data.find((list) => list.Name === 'sigle');
+      }
+      if (!mailjetList) {
+        const contactList: ContactList.IPostContactListBody = {
+          Name: 'sigle',
+        };
+        const data: { body: ContactList.TPostContactListResponse } =
+          await mailjet
+            .post('contactslist', { version: 'v3' })
+            .request(contactList);
+        mailjetList = data.body.Data[0];
+      }
+    } catch (error) {
+      if (error.statusCode === 401) {
+        throw new BadRequestException('Invalid Maijet api and/or secret key.');
+      }
+      throw error;
+    }
+
+    return {
+      listId: mailjetList.ID,
+      listAddress: mailjetList.Address,
+    };
   }
 }
