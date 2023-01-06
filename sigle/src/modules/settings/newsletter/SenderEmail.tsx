31e458efe201033f573830a318724ec996abd440
Léo Pradel <pradel.leo@gmail.com>
2023-01-06 18:55:16 +0100

    ui success

--- sigle/src/modules/settings/newsletter/SenderEmail.tsx
+++ sigle/src/modules/settings/newsletter/SenderEmail.tsx
@@ -1,4 +1,5 @@
 import { ReloadIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { ApiError } from '../../../external/api';
 import {
@@ -8,10 +9,15 @@ import {
 import { Box, Button, Flex, Typography } from '../../../ui';
 
 export const SenderEmail = () => {
+  const [successSenderEmail, setSuccessSenderEmail] = useState(false);
   const { data: userNewsletter, refetch: refetchUserNewsletter } =
     useGetUserNewsletter();
   const { mutate: syncNewsletter, isLoading: isLoadingSyncNewsletter } =
     useSyncSenderNewsletter({
+      onSuccess: async () => {
+        await refetchUserNewsletter();
+        setSuccessSenderEmail(true);
+      },
       onError: (error: Error | ApiError) => {
         let errorMessage = error.message;
         if (error instanceof ApiError && error.body.message) {
@@ -25,43 +31,63 @@ export const SenderEmail = () => {
   // all success icons
 
   return (
-    <Box css={{ backgroundColor: '$gray2', br: '$4', padding: '$5', mt: '$5' }}>
-      <Typography css={{ fontWeight: 600 }} size="h4">
-        Add sender address
-      </Typography>
-      {(!userNewsletter || !userNewsletter.senderEmail) && (
-        <Typography size="subheading" css={{ color: '$gray9', mt: '$2' }}>
-          To be able to send emails, you must add/choose a sender email on your
-          Mailjet account and sync below.
-        </Typography>
-      )}
-      {userNewsletter?.senderEmail && (
-        <Typography size="subheading" css={{ color: '$gray9', mt: '$2' }}>
-          Your newsletter will be sent with{' '}
-          <Typography size="subheading" as="span" css={{ color: '$gray10' }}>
-            {userNewsletter.senderEmail}
-          </Typography>
-        </Typography>
-      )}
-      <Flex css={{ mt: '$5' }} gap="5">
-        <Button
-          css={{ gap: '$2' }}
-          onClick={() => syncNewsletter()}
-          disabled={isLoadingSyncNewsletter}
-        >
-          <span>Sync</span> <ReloadIcon />
-        </Button>
-        <Button
-          color="orange"
-          variant="ghost"
-          as="a"
-          href="https://app.mailjet.com/account/sender"
-          target="_blank"
-          rel="noreferrer"
-        >
+    <>
+      <Box
+        css={{ backgroundColor: '$gray2', br: '$4', padding: '$5', mt: '$5' }}
+      >
+        <Typography css={{ fontWeight: 600 }} size="h4">
           Add sender address
-        </Button>
-      </Flex>
-    </Box>
+        </Typography>
+        {(!userNewsletter || !userNewsletter.senderEmail) && (
+          <Typography size="subheading" css={{ color: '$gray9', mt: '$2' }}>
+            To be able to send emails, you must add/choose a sender email on
+            your Mailjet account and sync below.
+          </Typography>
+        )}
+        {userNewsletter?.senderEmail && (
+          <Typography size="subheading" css={{ color: '$gray9', mt: '$2' }}>
+            Your newsletter will be sent with{' '}
+            <Typography size="subheading" as="span" css={{ color: '$gray10' }}>
+              {userNewsletter.senderEmail}
+            </Typography>
+          </Typography>
+        )}
+        <Flex css={{ mt: '$5' }} gap="5">
+          <Button
+            css={{ gap: '$2' }}
+            onClick={() => syncNewsletter()}
+            disabled={isLoadingSyncNewsletter}
+          >
+            <span>Sync</span> <ReloadIcon />
+          </Button>
+          <Button
+            color="orange"
+            variant="ghost"
+            as="a"
+            href="https://app.mailjet.com/account/sender"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Add sender address
+          </Button>
+        </Flex>
+      </Box>
+
+      {successSenderEmail && (
+        <Box
+          css={{
+            backgroundColor: '$green2',
+            border: '1px solid $green6',
+            br: '$4',
+            padding: '$5',
+            mt: '$5',
+          }}
+        >
+          <Typography size="subheading" css={{ color: '$green11' }}>
+            Your account is setup, you can now send newsletters!
+          </Typography>
+        </Box>
+      )}
+    </>
   );
 };
