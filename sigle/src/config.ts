c1cbe4c323fefab7fc8714bd2c3797bebf387175
Léo Pradel <pradel.leo@gmail.com>
2023-01-07 18:28:49 +0100

    whitelist more users

--- sigle/src/config.ts
+++ sigle/src/config.ts
@@ -24,10 +24,12 @@ export const sigleConfig = {
 export const allowedNewsletterUsers = [
   // sigle.btc
   'SP2EVYKET55QH40RAZE5PVZ363QX0X6BSRP4C7H0W',
+  // leopradel.btc
+  'SP3VCX5NFQ8VCHFS9M6N40ZJNVTRT4HZ62WFH5C4Q',
+  // quentin.btc
+  'SP24GYRG3M7T0S6FZE9RVVP9PNNZQJQ614650G590',
 ];
 if (sigleConfig.env === 'development') {
-  // leopradel.btc
-  allowedNewsletterUsers.push('SP3VCX5NFQ8VCHFS9M6N40ZJNVTRT4HZ62WFH5C4Q');
   // gregogun.btc
   allowedNewsletterUsers.push('SP1F48HCD4SP4HT8BHQPXZ35615764KC80ACNMBDZ');
 }
