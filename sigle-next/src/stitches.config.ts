efa07ba8a5afba9abadd2582225d227c285f4443
Léo Pradel <pradel.leo@gmail.com>
2023-01-12 01:04:03 +0100

    add utils

--- sigle-next/src/stitches.config.ts
+++ sigle-next/src/stitches.config.ts
@@ -1,4 +1,5 @@
 import { createStitches } from '@stitches/react';
+import type * as Stitches from '@stitches/react';
 
 export const { styled, getCssText } = createStitches({
   theme: {
@@ -55,5 +56,64 @@ export const { styled, getCssText } = createStitches({
       '2xl': '1.5rem',
       full: '9999px',
     },
+  },
+  utils: {
+    p: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingTop: value,
+      paddingBottom: value,
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    pt: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingTop: value,
+    }),
+    pr: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingRight: value,
+    }),
+    pb: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingBottom: value,
+    }),
+    pl: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingLeft: value,
+    }),
+    px: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: Stitches.PropertyValue<'padding'>) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+
+    m: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginTop: value,
+      marginBottom: value,
+      marginLeft: value,
+      marginRight: value,
+    }),
+    mt: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginTop: value,
+    }),
+    mr: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginRight: value,
+    }),
+    mb: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginBottom: value,
+    }),
+    ml: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginLeft: value,
+    }),
+    mx: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: Stitches.PropertyValue<'margin'>) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+
+    br: (value: Stitches.PropertyValue<'borderRadius'>) => ({
+      borderRadius: value,
+    }),
   },
 });
