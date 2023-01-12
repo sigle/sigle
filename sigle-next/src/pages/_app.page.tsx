1c81cc6df269b9244a4a7deaa288474a78621889
Léo Pradel <pradel.leo@gmail.com>
2023-01-12 10:37:52 +0100

    use font var

--- sigle-next/src/pages/_app.page.tsx
+++ sigle-next/src/pages/_app.page.tsx
@@ -1,24 +1,31 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
+import { globalCss } from '@stitches/react';
 import '@sigle/tailwind-style/dist/tailwind.css';
 
-const openSans = Inter({
+const inter = Inter({
   subsets: ['latin'],
   style: ['normal'],
   weight: ['400', '600', '700'],
+  variable: '--font-inter',
 });
-// TODO remove main tag and use css variables instead
+
+const globalStyle = globalCss({
+  body: {
+    fontFamily: 'var(--font-inter)',
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyle();
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <main className={openSans.className}>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
