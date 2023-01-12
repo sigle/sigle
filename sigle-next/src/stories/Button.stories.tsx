f5dd45f43967dd1b5308e67175ff95d3333b2c89
Léo Pradel <pradel.leo@gmail.com>
2023-01-12 13:54:09 +0100

    Badge

--- sigle-next/src/stories/Button.stories.tsx
+++ sigle-next/src/stories/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import { TbPlus } from 'react-icons/tb';
 import { Button, ButtonProps } from '@sigle/ui';
 
--- sigle-next/src/ui/index.ts
+++ sigle-next/src/ui/index.ts
@@ -1,3 +1,4 @@
+export * from './Badge';
 export * from './Button';
 export * from './IconButton';
 export * from './Typography';
