44acf3169280d681af6bb72f5c026839b31a322e
Léo Pradel <pradel.leo@gmail.com>
2023-01-17 10:17:57 +0100

    chain polygon

--- sigle-next/src/components/WagmiProvider.tsx
+++ sigle-next/src/components/WagmiProvider.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from 'react';
 import { createClient, WagmiConfig } from 'wagmi';
+import { polygon } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 
 // TODO setup alchemy / infura for production
 // TODO setup allowed providers
+const chains = [polygon];
+
 const client = createClient(
   getDefaultClient({
     appName: 'Sigle',
+    chains,
   })
 );
 
