9226df3eef48de0b039fb5a2a8b87607425aa8a6
Léo Pradel <pradel.leo@gmail.com>
2023-01-27 17:40:40 +0100

    delete

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { StacksService } from '../stacks/stacks.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, StacksService],
+  providers: [UserService],
   imports: [],
 })
 export class UserModule {}
