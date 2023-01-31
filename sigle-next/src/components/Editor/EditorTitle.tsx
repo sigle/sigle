e2067dc8de89e91d4bc69c48a2307ba02d7725bd
Léo Pradel <pradel.leo@gmail.com>
2023-01-31 16:45:29 +0100

    title

--- sigle-next/src/components/Editor/EditorTitle.tsx
+++ sigle-next/src/components/Editor/EditorTitle.tsx
@@ -1,3 +1,29 @@
+import { styled } from '@sigle/stitches.config';
+import { useEditorStore } from './store';
+
+const StyledInput = styled('input', {
+  // Here we copy the styles of the prose h1
+  fontSize: '$3xl',
+  lineHeight: '$3xl',
+  backgroundColor: 'transparent',
+  outline: 'none',
+  width: '100%',
+
+  '::placeholder': {
+    color: '$gray8',
+  },
+});
+
 export const EditorTitle = () => {
-  return null;
+  const storyTitle = useEditorStore((state) => state.story?.title);
+
+  return (
+    <StyledInput
+      placeholder="Title..."
+      value={storyTitle}
+      onChange={(e) =>
+        useEditorStore.setState({ story: { title: e.target.value } })
+      }
+    />
+  );
 };
