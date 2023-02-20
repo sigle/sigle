abcb8006ba2dd8a9b26fe38db3e365c56fb48ead
Léo Pradel <pradel.leo@gmail.com>
2023-02-20 18:31:04 +0100

    feat: create home page

--- sigle-next/src/pages/index.page.tsx
+++ sigle-next/src/pages/index.page.tsx
@@ -1,10 +1,25 @@
 import { TooltipProvider } from '@radix-ui/react-tooltip';
+import { Suspense } from 'react';
+import { Container } from '@sigle/ui';
+import { StoryCardPublishedSkeleton } from '@/components/StoryCard/StoryCardPublishedSkeleton';
 import { DashboardLayout } from '../components/Dashboard/DashboardLayout';
 
 export default function Home() {
   return (
     <TooltipProvider>
-      <DashboardLayout> </DashboardLayout>
+      <Suspense
+        fallback={
+          <DashboardLayout>
+            <Container css={{ maxWidth: 680, py: '$5' }}>
+              <StoryCardPublishedSkeleton />
+              <StoryCardPublishedSkeleton />
+              <StoryCardPublishedSkeleton />
+            </Container>
+          </DashboardLayout>
+        }
+      >
+        <DashboardLayout> </DashboardLayout>
+      </Suspense>
     </TooltipProvider>
   );
 }
