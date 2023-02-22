90853fd92e10b4521b6b3bd7623f5e3d23f85b28
Léo Pradel <pradel.leo@gmail.com>
2023-02-22 13:54:00 +0100

    fix

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -4,7 +4,7 @@ import {
   OnModuleDestroy,
   OnModuleInit,
 } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '.prisma/client';
 
 @Injectable()
 export class PrismaService
