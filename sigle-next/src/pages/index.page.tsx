57217b122d4468d396b84f021fe10612cb72a7b8
Léo Pradel <pradel.leo@gmail.com>
2023-02-22 18:35:23 +0100

    infinite loading feed

--- sigle-next/src/pages/index.page.tsx
+++ sigle-next/src/pages/index.page.tsx
@@ -1,13 +1,32 @@
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import Link from 'next/link';
-import { Button, Container, Flex, Typography } from '@sigle/ui';
+import { useInView } from 'react-cool-inview';
+import { Button, Container, Flex, LoadingSpinner, Typography } from '@sigle/ui';
 import { StoryCardPublishedSkeleton } from '@/components/StoryCard/StoryCardPublishedSkeleton';
 import { trpc } from '@/utils/trpc';
 import { StoryCardPublished } from '@/components/StoryCard/StoryCardPublished';
 import { DashboardLayout } from '../components/Dashboard/DashboardLayout';
 
 const HopePostList = () => {
-  const postList = trpc.postList.useQuery({});
+  const postList = trpc.postList.useInfiniteQuery(
+    { limit: 4 },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  const { observe } = useInView({
+    rootMargin: '50px 0px',
+    onEnter: async ({ observe, unobserve }) => {
+      // Pause observe when loading data
+      unobserve();
+
+      const data = await postList.fetchNextPage();
+      if (data.hasNextPage) {
+        observe();
+      }
+    },
+  });
 
   if (postList.isLoading) {
     return (
@@ -26,16 +45,24 @@ const HopePostList = () => {
 
   return (
     <>
-      {postList.data.items.map((post) => {
-        return (
-          <StoryCardPublished
-            key={post.id}
-            isViewer={false}
-            post={post}
-            profile={post.profile}
-          />
-        );
+      {postList.data?.pages.map((page) => {
+        return page.items.map((item) => {
+          return (
+            <StoryCardPublished
+              key={item.id}
+              isViewer={false}
+              post={item}
+              profile={item.profile}
+            />
+          );
+        });
       })}
+      <div ref={observe} />
+      {postList.isFetching && (
+        <Flex justify="center" mt="4">
+          <LoadingSpinner />
+        </Flex>
+      )}
     </>
   );
 };
