4eaf8fa5fa7db375dc66d7727d82ec3b974b421c
Léo Pradel <pradel.leo@gmail.com>
2023-02-23 17:40:58 +0100

    create delete dialog

---
+++ sigle-next/src/components/StoryCard/DeleteDialog.tsx
@@ -0,0 +1,110 @@
+import { graphql, useMutation } from 'react-relay';
+import * as Sentry from '@sentry/nextjs';
+import { useState } from 'react';
+import {
+  Button,
+  Flex,
+  Typography,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from '@sigle/ui';
+import { trpc } from '@/utils/trpc';
+import { DeleteDialogDeletePostMutation } from '@/__generated__/relay/DeleteDialogDeletePostMutation.graphql';
+
+interface DeleteDialogProps {
+  postId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const DeleteDialog = ({
+  postId,
+  open,
+  onOpenChange,
+}: DeleteDialogProps) => {
+  const utils = trpc.useContext();
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const [commit, isLoadingDeletePost] =
+    useMutation<DeleteDialogDeletePostMutation>(
+      graphql`
+        mutation DeleteDialogDeletePostMutation($input: UpdatePostInput!) {
+          updatePost(input: $input) {
+            clientMutationId
+            document {
+              id
+              status
+              title
+              content
+            }
+          }
+        }
+      `
+    );
+
+  const handleDelete = () => {
+    commit({
+      variables: {
+        input: {
+          id: postId,
+          content: {
+            status: 'DELETED',
+            title: '',
+            content: '',
+          },
+        },
+      },
+      onCompleted: (data, errors) => {
+        if (errors) {
+          // TODO toast error
+          Sentry.captureMessage('Error updating story', {
+            extra: { errors },
+          });
+          return;
+        }
+        if (data.updatePost) {
+          setIsDeleting(true);
+          utils.invalidate();
+        }
+      },
+    });
+  };
+
+  const isLoading = isLoadingDeletePost || isDeleting;
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogTitle asChild>
+          <Typography size="xl">Delete my story</Typography>
+        </DialogTitle>
+        <DialogDescription asChild>
+          <Flex mt="3" direction="column">
+            <Typography>
+              Story content will still be viewable on IPFS. Remove entry only
+              hides it from your post list.
+            </Typography>
+          </Flex>
+        </DialogDescription>
+        <Flex justify="end" gap="6" css={{ mt: '$6' }}>
+          <DialogClose asChild>
+            <Button size="lg" variant="ghost" disabled={isLoading}>
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button
+            size="lg"
+            color="indigo"
+            disabled={isLoading}
+            onClick={handleDelete}
+          >
+            {isLoading ? 'Deleting ...' : 'Confirm'}
+          </Button>
+        </Flex>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- sigle-next/src/components/StoryCard/StoryCardDraft.tsx
+++ sigle-next/src/components/StoryCard/StoryCardDraft.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { TbDots } from 'react-icons/tb';
 import { format } from 'date-fns';
+import { useState } from 'react';
 import {
   Badge,
   DropdownMenu,
@@ -14,6 +15,7 @@ import {
 } from '@sigle/ui';
 import { styled } from '@sigle/stitches.config';
 import { CeramicPost } from '@/types/ceramic';
+import { DeleteDialog } from './DeleteDialog';
 
 const StyledDropdownMenuItem = styled(DropdownMenuItem, {
   color: '$orange11',
@@ -29,58 +31,69 @@ interface StoryCardDraftProps {
 
 export const StoryCardDraft = ({ post }: StoryCardDraftProps) => {
   const router = useRouter();
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
   const postEditorLink = `/editor/${post.id}`;
 
   return (
-    <Flex
-      direction="column"
-      css={{
-        borderTop: '1px solid $gray6',
-        py: '$6',
-        '&:first-child': {
-          borderTop: 'none',
-        },
-      }}
-    >
-      <Link href={postEditorLink}>
-        <Typography size="lg" fontWeight="bold" lineClamp={2}>
-          {post.title}
-        </Typography>
-      </Link>
-      <Link href={postEditorLink}>
-        <Typography size="sm" color="gray9" css={{ mt: '$2' }} lineClamp={3}>
-          Blockchain has completely changed the way we think about money and
-          created a true era of digital property. Bitcoin is the first
-          successful use case for blockchain. Some creative developers believe
-          Bitcoin is the first successful use case for blockchain. Some creative
-          developers believe Bitcoin is the first successful use case for
-          blockchain.
-        </Typography>
-      </Link>
-      <Flex justify="between" align="center" css={{ mt: '$9' }}>
-        <Typography size="xs" color="gray9">
-          {format(new Date(post.createdAt), "MMM dd, yyyy 'at' h:mmaaa")}
-        </Typography>
-        <Flex align="center" gap="2">
-          <Badge>DRAFT</Badge>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <IconButton size="xs" variant="ghost">
-                <TbDots />
-              </IconButton>
-            </DropdownMenuTrigger>
-            <StyledDropdownMenuContent side="left" align="end">
-              <DropdownMenuItem onSelect={() => router.push(postEditorLink)}>
-                Edit
-              </DropdownMenuItem>
-              <StyledDropdownMenuItem onSelect={() => alert('TODO')}>
-                Delete
-              </StyledDropdownMenuItem>
-            </StyledDropdownMenuContent>
-          </DropdownMenu>
+    <>
+      <Flex
+        direction="column"
+        css={{
+          borderTop: '1px solid $gray6',
+          py: '$6',
+          '&:first-child': {
+            borderTop: 'none',
+          },
+        }}
+      >
+        <Link href={postEditorLink}>
+          <Typography size="lg" fontWeight="bold" lineClamp={2}>
+            {post.title}
+          </Typography>
+        </Link>
+        <Link href={postEditorLink}>
+          <Typography size="sm" color="gray9" css={{ mt: '$2' }} lineClamp={3}>
+            Blockchain has completely changed the way we think about money and
+            created a true era of digital property. Bitcoin is the first
+            successful use case for blockchain. Some creative developers believe
+            Bitcoin is the first successful use case for blockchain. Some
+            creative developers believe Bitcoin is the first successful use case
+            for blockchain.
+          </Typography>
+        </Link>
+        <Flex justify="between" align="center" css={{ mt: '$9' }}>
+          <Typography size="xs" color="gray9">
+            {format(new Date(post.createdAt), "MMM dd, yyyy 'at' h:mmaaa")}
+          </Typography>
+          <Flex align="center" gap="2">
+            <Badge>DRAFT</Badge>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <IconButton size="xs" variant="ghost">
+                  <TbDots />
+                </IconButton>
+              </DropdownMenuTrigger>
+              <StyledDropdownMenuContent side="left" align="end">
+                <DropdownMenuItem onSelect={() => router.push(postEditorLink)}>
+                  Edit
+                </DropdownMenuItem>
+                <StyledDropdownMenuItem
+                  onSelect={() => setIsDeleteDialogOpen(true)}
+                >
+                  Delete
+                </StyledDropdownMenuItem>
+              </StyledDropdownMenuContent>
+            </DropdownMenu>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+
+      <DeleteDialog
+        postId={post.id}
+        open={isDeleteDialogOpen}
+        onOpenChange={(isOpen) => setIsDeleteDialogOpen(isOpen)}
+      />
+    </>
   );
 };
