a22a99db4f082ec75be724330909d8630908bb96
Léo Pradel <pradel.leo@gmail.com>
2023-03-10 01:29:43 +0100

    simple sign

--- sigle/src/pages/inscribe/[storyId].page.tsx
+++ sigle/src/pages/inscribe/[storyId].page.tsx
@@ -1,16 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import * as Sentry from '@sentry/nextjs';
 import { toast } from 'react-toastify';
 import { Protected } from '../../modules/auth/Protected';
 import { getStoryFile } from '../../utils';
 import { migrationStory } from '../../utils/migrations/story';
 import { DashboardLayout } from '../../modules/layout';
-import { Typography } from '../../ui';
+import { Button, Flex, Typography } from '../../ui';
 import { useAuth } from '../../modules/auth/AuthContext';
 import { appConfig } from '../../utils/blockstack';
 import { styled } from '../../stitches.config';
+import { useConnect } from '@stacks/connect-react';
+import { StacksMainnet } from '@stacks/network';
 
 const StyledCode = styled('code', {
   display: 'block',
@@ -29,6 +31,8 @@ const Inscribe = () => {
   const { user } = useAuth();
   const router = useRouter();
   const { storyId } = router.query as { storyId: string };
+  const { sign } = useConnect();
+  const [signedData, setSignedData] = useState<string | null>(null);
 
   const { data } = useQuery(
     ['story', storyId],
@@ -53,18 +57,52 @@ const Inscribe = () => {
   const parsedData = {
     p: 'ons',
     op: 'post',
-    title: data.title,
-    url: `${appConfig.appDomain}/${user?.username}/${storyId}`,
     author: user?.username,
+    authorAdress: user?.profile?.stxAddress?.mainnet,
+    title: data.title,
     body: data.content,
+    url: `${appConfig.appDomain}/${user?.username}/${storyId}`,
+    signature: signedData ?? undefined,
   };
 
-  console.log('data', data);
+  const handleSign = async () => {
+    await sign({
+      network: new StacksMainnet(),
+      message: JSON.stringify(parsedData),
+      onFinish: async ({ signature }) => {
+        setSignedData(signature);
+      },
+    });
+  };
+
+  const handleCopyClipboard = () => {
+    navigator.clipboard.writeText(JSON.stringify(parsedData, null, 2));
+    toast.success('Copied to clipboard');
+  };
 
   return (
     <DashboardLayout>
       <Typography size="h2">Inscribe</Typography>
       <StyledCode>{JSON.stringify(parsedData, null, 2)}</StyledCode>
+      <Flex gap="5">
+        {!signedData ? (
+          <>
+            <Typography size="subheading">
+              To prove the ownership of the content, we include your address as
+              well as a signature. Please sign the message with your wallet.
+            </Typography>
+            <Button onClick={handleSign}>Sign message</Button>
+          </>
+        ) : (
+          <>
+            <Typography size="subheading">
+              You can upload an inscription using ord or through a service that
+              supports text inscriptions.
+            </Typography>
+            <Button onClick={handleCopyClipboard}>Copy to clipboard</Button>
+          </>
+        )}
+      </Flex>
     </DashboardLayout>
   );
 };
