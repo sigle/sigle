dab7bf715613de7c364445cff7816adb0d7e1465
Léo Pradel <pradel.leo@gmail.com>
2023-03-14 20:44:51 +0100

    fix: allow 464295 to be linked again

--- sigle/src/pages/inscribe/[storyId].page.tsx
+++ sigle/src/pages/inscribe/[storyId].page.tsx
@@ -185,7 +185,7 @@ const Inscribe = () => {
     toast.success('Inscription linked');
   };
 
-  if (data.inscriptionId) {
+  if (data.inscriptionId && data.inscriptionNumber !== 464295) {
     return (
       <DashboardLayout>
         <Typography size="h2">Inscription #{data.inscriptionNumber}</Typography>
