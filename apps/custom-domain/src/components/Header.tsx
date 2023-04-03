0c7780af6fd8ed8cc5894e56ea27cb96f38f421c
Léo Pradel <pradel.leo@gmail.com>
2023-04-03 16:12:34 +0200

    sheet logic

--- apps/custom-domain/src/components/Header.tsx
+++ apps/custom-domain/src/components/Header.tsx
@@ -6,6 +6,15 @@ import { useEffect, useState } from 'react';
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { SiteSettings } from '@/types';
+import { IconMenu2 } from '@tabler/icons-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/ui/Sheet';
 
 interface HeaderProps {
   settings: SiteSettings;
@@ -40,7 +49,7 @@ export const Header = ({ settings }: HeaderProps) => {
     <header
       className={twMerge(
         clsx(
-          'h-16 px-8 flex justify-between items-center bg-sigle-background sticky top-0 transition-all duration-300 ease-in-out z-10 border-b border-gray-150 md:px-16',
+          'h-16 px-4 flex justify-between items-center bg-sigle-background sticky top-0 transition-all duration-300 ease-in-out z-10 border-b border-gray-150 md:px-16',
           {
             ['-top-16']: scrollDirection === 'down',
           }
@@ -49,6 +58,49 @@ export const Header = ({ settings }: HeaderProps) => {
     >
       <Link href="/">
         <div className="flex items-center gap-2">
+          <Sheet>
+            <SheetTrigger asChild>
+              <button className="md:hidden p-2">
+                <IconMenu2 size={15} />
+              </button>
+            </SheetTrigger>
+            <SheetContent position="left" size="xl">
+              <SheetHeader>
+                <SheetTitle>
+                  <div className="flex items-center gap-2">
+                    <div className="relative object-cover rounded overflow-hidden">
+                      <Image
+                        src={settings.avatar}
+                        alt="Avatar"
+                        priority
+                        height={22}
+                        width={22}
+                      />
+                    </div>
+                    <div className="text-sm font-semibold">
+                      {settings.name}{' '}
+                      <span className="font-normal text-gray-500">| Blog</span>
+                    </div>
+                  </div>
+                </SheetTitle>
+                <SheetDescription>
+                  <ul className="mt-4 flex flex-col gap-5 items-start">
+                    {settings.links?.map(({ href, label }) => (
+                      <li key={href}>
+                        <Link
+                          href={href}
+                          className="text-sm text-gray-800 hover:text-gray-500"
+                        >
+                          {label}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+
           <div className="relative object-cover rounded overflow-hidden">
             <Image
               src={settings.avatar}
@@ -68,7 +120,7 @@ export const Header = ({ settings }: HeaderProps) => {
       <nav>
         <ul className="flex gap-8 items-center">
           {settings.links?.map(({ href, label }) => (
-            <li key={href}>
+            <li key={href} className="hidden md:block">
               <Link
                 href={href}
                 className="text-sm text-gray-800 hover:text-gray-500"
