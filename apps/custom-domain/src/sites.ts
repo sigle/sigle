7565a3f7b9c507b2d387eb458c141e0be1dce4f6
Léo Pradel <pradel.leo@gmail.com>
2023-04-03 12:56:44 +0200

    add nftbot

--- apps/custom-domain/src/sites.ts
+++ apps/custom-domain/src/sites.ts
@@ -16,6 +16,15 @@ export const sites: {
     avatar:
       'https://gaia.blockstack.org/hub/1KwTnsTj6Rqm26zRfhJuNdQUG63ieozxbB/photos/settings/1656339625902-logo_dapp_com.jpg',
   },
+  ['blog.nftbot.app']: {
+    username: 'nftbotapp.btc',
+    name: 'NFTBOT.app',
+    description:
+      'A fast and affordable multi-chain service: NFTBOT is the best way to automate sales alerts for your NFT project on Twitter.',
+    banner: '/websites/blog.nftbot.app/banner.png',
+    avatar:
+      'https://gaia.blockstack.org/hub/1P4Ktt6EbjmBWRxnZUFU7pjvWQcTxXdtfe/photos/settings/1680265571505-pfp_2.jpg',
+  },
 };
 
 // Add localhost to sites for development
