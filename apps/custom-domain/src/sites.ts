b85c9ba7be31a072c48686506f9e08060867c535
Léo Pradel <pradel.leo@gmail.com>
2023-04-04 13:58:51 +0200

    feat: custom domain take settings from Gaia

--- apps/custom-domain/src/sites.ts
+++ apps/custom-domain/src/sites.ts
@@ -1,16 +1,14 @@
-import { SiteSettings } from './types';
-
 export const sites: {
-  [key: string]: SiteSettings;
+  [key: string]: {
+    username: string;
+    banner: string;
+    links: { href: string; label: string }[];
+    cta?: { href: string; label: string };
+  };
 } = {
   ['blog.sigle.io']: {
     username: 'sigle.btc',
-    name: 'Sigle',
-    description:
-      'The official Sigle blog, an open-source writing platform for web3 content creators.',
     banner: '/websites/blog.sigle.io/banner.png',
-    avatar:
-      'https://gaia.blockstack.org/hub/1KwTnsTj6Rqm26zRfhJuNdQUG63ieozxbB/photos/settings/1656339625902-logo_dapp_com.jpg',
     links: [
       { href: 'https://www.sigle.io/', label: 'Home' },
       { href: 'https://app.sigle.io/explore', label: 'Explore' },
@@ -19,12 +17,7 @@ export const sites: {
   },
   ['blog.nftbot.app']: {
     username: 'nftbotapp.btc',
-    name: 'NFTBOT.app',
-    description:
-      'A fast and affordable multi-chain service: NFTBOT is the best way to automate sales alerts for your NFT project on Twitter.',
     banner: '/websites/blog.nftbot.app/banner.png',
-    avatar:
-      'https://gaia.blockstack.org/hub/1P4Ktt6EbjmBWRxnZUFU7pjvWQcTxXdtfe/photos/settings/1680265571505-pfp_2.jpg',
     links: [
       { href: 'https://www.nftbot.app/#features', label: 'Features' },
       { href: 'https://www.nftbot.app/#pricing', label: 'Pricing' },
