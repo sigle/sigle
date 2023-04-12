9720f1f83ea64d71bae832257973269421061d1d
Léo Pradel <pradel.leo@gmail.com>
2023-04-12 14:04:04 +0200

    feat: improve custom domain page handling (#874)

--- apps/custom-domain/src/app/_sites/[site]/page/[page]/page.tsx
+++ apps/custom-domain/src/app/_sites/[site]/page/[page]/page.tsx
@@ -1,3 +1,55 @@
-import Home from '../../page';
+import { Pagination } from '@/components/Pagination';
+import { PostCard } from '@/components/PostCard';
+import { getAbsoluteUrl } from '@/utils/vercel';
 
-export default Home;
+export const runtime = 'edge';
+// Revalidate this page every 60 seconds
+export const revalidate = 60;
+
+async function getPosts({
+  site,
+  page,
+}: {
+  site: string;
+  page: number;
+}): Promise<{
+  count: number;
+  posts: {
+    id: string;
+    coverImage?: string;
+    title: string;
+    content: string;
+    createdAt: number;
+  }[];
+}> {
+  const res = await fetch(
+    `${getAbsoluteUrl()}/api/posts?site=${site}&page=${page}`
+  );
+  return res.json();
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { site: string; page: string };
+}) {
+  const { site } = params;
+
+  const page = parseInt(params.page, 10);
+  const posts = await getPosts({
+    site,
+    page,
+  });
+
+  return (
+    <div className="container">
+      <h2 className="mt-6 text-2xl font-bold">Latest articles</h2>
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-10">
+        {posts.posts.map((post) => {
+          return <PostCard key={post.id} post={post} />;
+        })}
+      </div>
+      <Pagination page={page} total={posts.count} itemsPerPage={15} />
+    </div>
+  );
+}
