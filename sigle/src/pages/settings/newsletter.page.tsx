1b4307ef788f04ec7fcbeff37dd358996a59ac72
Léo Pradel <pradel.leo@gmail.com>
2023-04-19 10:38:27 +0200

    feat: remove whitelist for newsletter (#884)

--- sigle/src/pages/settings/newsletter.page.tsx
+++ sigle/src/pages/settings/newsletter.page.tsx
@@ -1,23 +1,8 @@
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { allowedNewsletterUsers } from '../../config';
-import { useAuth } from '../../modules/auth/AuthContext';
+import React from 'react';
 import { Protected } from '../../modules/auth/Protected';
 import { Newsletter } from '../../modules/settings/newsletter/Newsletter';
 
 const NewsletterPage = () => {
-  const router = useRouter();
-  const { user, loggingIn } = useAuth();
-
-  useEffect(() => {
-    const isNewsletterWhitelisted = allowedNewsletterUsers.includes(
-      user?.profile.stxAddress.mainnet || ''
-    );
-    if (!loggingIn && !isNewsletterWhitelisted) {
-      router.push('/');
-    }
-  }, []);
-
   return (
     <Protected>
       <Newsletter />
