e1fb057e140c36bb3ab026d924be3a0f2cf53a5a
Léo Pradel <pradel.leo@gmail.com>
2023-04-28 13:20:59 +0200

    revert

--- apps/sigle-next/src/app/api/image-upload/route.api.ts
+++ apps/sigle-next/src/app/api/image-upload/route.api.ts
@@ -1,9 +1,7 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth].api';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getServerSession } from 'next-auth';
-import { optimizeImage } from 'next/dist/server/image-optimizer';
 import { NextResponse } from 'next/server';
-import sharp from 'sharp';
+import { authOptions } from '@/pages/api/auth/[...nextauth].api';
 
 const client = new S3Client({
   endpoint: 'https://s3.filebase.com',
@@ -15,15 +13,6 @@ const client = new S3Client({
 });
 
 const allowedFormats = ['image/jpeg', 'image/png', 'image/webp', 'image/gif'];
-
-function toBuffer(arrayBuffer: ArrayBuffer) {
-  const buffer = Buffer.alloc(arrayBuffer.byteLength);
-  const view = new Uint8Array(arrayBuffer);
-  for (let i = 0; i < buffer.length; ++i) {
-    buffer[i] = view[i];
-  }
-  return buffer;
-}
 
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
@@ -51,58 +40,44 @@ export async function POST(request: Request) {
   }
 
   const fileArrayBuffer = await file.arrayBuffer();
-  // let sharpImage = sharp(fileArrayBuffer);
-  // const meta = await sharpImage.metadata();
 
-  // console.log(meta);
-  // const optimisedImage = await sharpImage
-  //   .webp({ quality: 20, lossless: true, alphaQuality: 80 })
-  //   .toBuffer();
+  // const command = new PutObjectCommand({
+  //   Bucket: process.env.FILEBASE_BUCKET!,
+  //   // TODO generate unique key
+  //   Key: 'hello-s3.txt',
+  //   Body: optimisedImage,
+  // });
 
-  const optimisedImage = await optimizeImage({
-    buffer: toBuffer(fileArrayBuffer),
-    contentType: file.type,
-    quality: 75,
-    width: 1200,
-  });
+  // // Inject CID returned from Filebase into response
+  // command.middlewareStack.add(
+  //   (next) => async (args) => {
+  //     // Check if request is incoming as middleware works both ways
+  //     const response: any = await next(args);
+  //     if (!response.response.statusCode) return response;
 
-  const command = new PutObjectCommand({
-    Bucket: process.env.FILEBASE_BUCKET!,
-    // TODO generate unique key
-    Key: 'hello-s3.txt',
-    Body: optimisedImage,
-  });
+  //     // Get cid from headers
+  //     const cid = response.response.headers['x-amz-meta-cid'];
+  //     response.output.cid = cid;
+  //     return response;
+  //   },
+  //   {
+  //     step: 'build',
+  //     name: 'addCidToOutput',
+  //   }
+  // );
 
-  // Inject CID returned from Filebase into response
-  command.middlewareStack.add(
-    (next) => async (args) => {
-      // Check if request is incoming as middleware works both ways
-      const response: any = await next(args);
-      if (!response.response.statusCode) return response;
-
-      // Get cid from headers
-      const cid = response.response.headers['x-amz-meta-cid'];
-      response.output.cid = cid;
-      return response;
-    },
-    {
-      step: 'build',
-      name: 'addCidToOutput',
-    }
-  );
-
-  try {
-    const response = await client.send(command);
-    // @ts-expect-error cid is injected by middleware
-    const cid = response.cid;
-    return NextResponse.json({
-      cid,
-      url: `ipfs://${cid}`,
-    });
-  } catch (err) {
-    return NextResponse.json({
-      error: true,
-      message: 'Failed to upload file.',
-    });
-  }
+  // try {
+  //   const response = await client.send(command);
+  //   // @ts-expect-error cid is injected by middleware
+  //   const cid = response.cid;
+  //   return NextResponse.json({
+  //     cid,
+  //     url: `ipfs://${cid}`,
+  //   });
+  // } catch (err) {
+  //   return NextResponse.json({
+  //     error: true,
+  //     message: 'Failed to upload file.',
+  //   });
+  // }
 }
