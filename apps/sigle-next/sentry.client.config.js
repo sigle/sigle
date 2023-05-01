d21a4fb9dca4b221e2118926c349e025a1eaa6b8
Léo Pradel <pradel.leo@gmail.com>
2023-05-01 11:40:21 +0200

    sentry posthog

--- apps/sigle-next/sentry.client.config.js
+++ apps/sigle-next/sentry.client.config.js
@@ -3,10 +3,19 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
+import posthog from 'posthog-js';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const sentryProjectId = SENTRY_DSN ? SENTRY_DSN.split('/')[3] : '';
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
+  integrations: [
+    new posthog.SentryIntegration(
+      posthog,
+      process.env.NEXT_PUBLIC_SENTRY_ORG,
+      sentryProjectId
+    ),
+  ],
 });
