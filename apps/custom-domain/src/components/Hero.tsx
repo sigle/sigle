0d9b3094178d5d25a7294d2449d1217ed2c101e1
Léo Pradel <pradel.leo@gmail.com>
2023-05-29 11:48:20 +0200

    create form logic

--- apps/custom-domain/src/components/Hero.tsx
+++ apps/custom-domain/src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { SiteSettings } from '@/types';
+import { HeroSubscribe } from './HeroSubscribe';
 
 interface HeroProps {
   settings: SiteSettings;
@@ -32,24 +33,7 @@ export const Hero = ({ settings, newsletter }: HeroProps) => {
         <h1 className="mt-5 text-center text-4xl font-bold">{settings.name}</h1>
         <h2 className="mt-3 text-center text-lg">{settings.description}</h2>
       </div>
-      {newsletter && (
-        <form className="container mt-5 flex max-w-md flex-row items-center justify-center">
-          <input
-            className="h-10 w-full rounded-l-lg border border-gray-300 bg-transparent px-4 text-sm focus:border-gray-400 focus:ring-0"
-            aria-label="Enter your email to subscribe"
-            name="email_address"
-            placeholder="Enter your email"
-            required
-            type="email"
-          />
-          <button
-            className="h-10 rounded-r-lg bg-gray-950 px-5 text-sm text-white"
-            type="submit"
-          >
-            Subscribe
-          </button>
-        </form>
-      )}
+      {newsletter && <HeroSubscribe newsletter={newsletter} />}
     </div>
   );
 };
---
+++ apps/custom-domain/src/components/HeroSubscribe.tsx
@@ -0,0 +1,64 @@
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+interface HeroSubscribeProps {
+  newsletter: { id: string };
+}
+
+const subscribeSchema = z.object({
+  email: z.string().email(),
+});
+
+type SubscribeFormData = z.infer<typeof subscribeSchema>;
+
+export const HeroSubscribe = ({ newsletter }: HeroSubscribeProps) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<SubscribeFormData>({
+    resolver: zodResolver(subscribeSchema),
+  });
+
+  const onSubmit = handleSubmit((formValues) => {
+    console.log(formValues);
+    fetch('https://app.sigle.io/api/subscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formValues),
+    }).then((res) => {
+      if (res.ok) {
+        setValue('email', '');
+        // TODO: Show success message
+      }
+    });
+  });
+
+  return (
+    <form
+      className="container mt-5 flex max-w-md flex-row items-center justify-center"
+      onSubmit={onSubmit}
+    >
+      <input
+        className="h-10 w-full rounded-l-lg border border-gray-300 bg-transparent px-4 text-sm focus:border-gray-400 focus:ring-0"
+        aria-label="Enter your email to subscribe"
+        placeholder="Enter your email"
+        required
+        type="email"
+        {...register('email')}
+      />
+      <button
+        className="h-10 rounded-r-lg bg-gray-950 px-5 text-sm text-white"
+        type="submit"
+      >
+        Subscribe
+      </button>
+      {/* TODO show error message */}
+    </form>
+  );
+};
