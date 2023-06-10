8c146c069df443484fb20e3d18da40f07e7a7fe1
Léo Pradel <pradel.leo@gmail.com>
2023-06-10 17:03:26 +0200

    dynamic id

--- sigle/src/app/twitter-embed/[id]/page.page.tsx
+++ sigle/src/app/twitter-embed/[id]/page.page.tsx
@@ -1,18 +1,20 @@
 import { Tweet } from 'react-tweet';
 
-// export const revalidate = 3600;
+export const revalidate = 3600;
 export const runtime = 'edge';
+
+type Props = {
+  params: { id: string };
+};
 
 /**
  * We generate tweets on a separate page so they can then be screenshotted, and inserted as images
  * in the emails sent to subscribers.
  */
-export default function Page() {
-  const id = '1634630749650317314';
-
+export default function Page({ params }: Props) {
   return (
     <div data-theme="light">
-      <Tweet id={id} />
+      <Tweet id={params.id} />
     </div>
   );
 }
