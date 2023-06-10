65b604968fef032527ccc87e86e7100db4f4abe0
Léo Pradel <pradel.leo@gmail.com>
2023-06-10 16:47:22 +0200

    up

--- sigle/src/app/twitter-embed/page.page.tsx
+++ sigle/src/app/twitter-embed/page.page.tsx
@@ -3,6 +3,10 @@ import { Tweet } from 'react-tweet';
 // export const revalidate = 3600;
 export const runtime = 'edge';
 
+/**
+ * We generate tweets on a separate page so they can then be screenshotted, and inserted as images
+ * in the emails sent to subscribers.
+ */
 export default function Page() {
   const id = '1634630749650317314';
 
