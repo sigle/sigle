2271c91fa0d40bd6d69e1f6a36795f5477a98290
Léo Pradel <pradel.leo@gmail.com>
2023-06-11 13:03:19 +0200

    try with puppeteer

--- sigle/src/app/twitter-embed-image/[id]/route.api.ts
+++ sigle/src/app/twitter-embed-image/[id]/route.api.ts
@@ -1,4 +1,5 @@
-import { chromium as playwright } from 'playwright-core';
+// import { chromium as playwright } from 'playwright-core';
+import puppeteer from 'puppeteer-core';
 import chromium from '@sparticuz/chromium';
 
 // Cache for 1 day
@@ -11,16 +12,22 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const browser = await playwright.launch({
+  const browser = await puppeteer.launch({
     args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(),
-    headless: chromium.headless as any,
+    headless: chromium.headless,
   });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+
+  const page = await browser.newPage();
 
   await page.goto(`${process.env.APP_URL}/twitter-embed/${params.id}`);
-  const buffer = await page.locator('.react-tweet-theme').screenshot();
+  const element = await page.$('.react-tweet-theme');
+  if (!element) {
+    return new Response('Element not found', { status: 404 });
+  }
+  // const buffer = await page.locator('.react-tweet-theme').screenshot();
+  const buffer = await element.screenshot();
 
   return new Response(buffer, {
     headers: {
