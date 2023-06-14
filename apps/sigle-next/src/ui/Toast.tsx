02f6f4663eb2d962fd9d144644570083568ccbb6
Léo Pradel <pradel.leo@gmail.com>
2023-06-14 16:38:11 +0200

    fix tast style

--- apps/sigle-next/src/ui/Toast.tsx
+++ apps/sigle-next/src/ui/Toast.tsx
@@ -60,7 +60,7 @@ const ToastAction = React.forwardRef<
   <ToastPrimitives.Action
     ref={ref}
     className={cn(
-      'inline-flex h-8 shrink-0 text-gray1 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-gray10 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+      '[grid-area:_action] inline-flex h-8 shrink-0 text-gray1 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-gray10 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
       className
     )}
     {...props}
