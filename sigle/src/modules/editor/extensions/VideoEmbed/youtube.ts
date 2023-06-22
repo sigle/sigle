5166fe8db385a3a889c150f71f5eaaa4a92875f7
Léo Pradel <pradel.leo@gmail.com>
2023-06-22 15:35:55 +0200

    update utils with latest version

--- sigle/src/modules/editor/extensions/VideoEmbed/youtube.ts
+++ sigle/src/modules/editor/extensions/VideoEmbed/youtube.ts
@@ -1,7 +1,7 @@
 export const YOUTUBE_REGEX =
-  /^(https?:\/\/)?(www\.|music\.)?(youtube\.com|youtu\.be)(.+)?$/;
+  /^(https?:\/\/)?(www\.|music\.)?(youtube\.com|youtu\.be)(?!.*\/channel\/)(?!\/@)(.+)?$/;
 export const YOUTUBE_REGEX_GLOBAL =
-  /^(https?:\/\/)?(www\.|music\.)?(youtube\.com|youtu\.be)(.+)?$/g;
+  /^(https?:\/\/)?(www\.|music\.)?(youtube\.com|youtu\.be)(?!.*\/channel\/)(?!\/@)(.+)?$/g;
 
 export const isValidYoutubeUrl = (url: string) => {
   return url.match(YOUTUBE_REGEX);
@@ -10,15 +10,18 @@ export const isValidYoutubeUrl = (url: string) => {
 export interface GetEmbedUrlOptions {
   url: string;
   controls?: boolean;
+  nocookie?: boolean;
   startAt?: number;
 }
 
-export const getYoutubeEmbedUrl = () => {
-  return 'https://www.youtube.com/embed/';
+export const getYoutubeEmbedUrl = (nocookie?: boolean) => {
+  return nocookie
+    ? 'https://www.youtube-nocookie.com/embed/'
+    : 'https://www.youtube.com/embed/';
 };
 
-export const getEmbedURLFromYoutubeURL = (options: GetEmbedUrlOptions) => {
-  const { url, controls, startAt } = options;
+export const getEmbedUrlFromYoutubeUrl = (options: GetEmbedUrlOptions) => {
+  const { url, nocookie, startAt } = options;
 
   // if is already an embed url, return it
   if (url.includes('/embed/')) {
@@ -32,7 +35,7 @@ export const getEmbedURLFromYoutubeURL = (options: GetEmbedUrlOptions) => {
     if (!id) {
       return null;
     }
-    return `${getYoutubeEmbedUrl()}${id}`;
+    return `${getYoutubeEmbedUrl(nocookie)}${id}`;
   }
 
   const videoIdRegex = /v=([-\w]+)/gm;
@@ -42,13 +45,9 @@ export const getEmbedURLFromYoutubeURL = (options: GetEmbedUrlOptions) => {
     return null;
   }
 
-  let outputUrl = `${getYoutubeEmbedUrl()}${matches[1]}`;
+  let outputUrl = `${getYoutubeEmbedUrl(nocookie)}${matches[1]}`;
 
   const params = [];
-
-  if (!controls) {
-    params.push('controls=0');
-  }
 
   if (startAt) {
     params.push(`start=${startAt}`);
