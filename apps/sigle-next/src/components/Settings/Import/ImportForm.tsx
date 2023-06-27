a6ea18d221fec64f9fb19b1eff4d4c4367e8be3d
Léo Pradel <pradel.leo@gmail.com>
2023-06-27 13:10:00 +0200

    list of posts

--- apps/sigle-next/src/components/Settings/Import/ImportForm.tsx
+++ apps/sigle-next/src/components/Settings/Import/ImportForm.tsx
@@ -1,3 +1,4 @@
+import { lookupProfile } from 'micro-stacks/storage';
 import { Button, Input, LoadingSpinner, Typography } from '@sigle/ui';
 import { useImportStore } from './store';
 
@@ -5,29 +6,48 @@ export const ImportForm = () => {
   const status = useImportStore((state) => state.status);
   const setStatus = useImportStore((state) => state.setStatus);
   const processed = useImportStore((state) => state.processed);
+  const setProcessed = useImportStore((state) => state.setProcessed);
   const total = useImportStore((state) => state.total);
   const setTotal = useImportStore((state) => state.setTotal);
 
   // TODO take it from the form
-  const url = 'https://app.sigle.io/sigle.btc';
+  const url = 'sigle.btc';
 
   const handleSigleBlogMigration = async (e) => {
     e.preventDefault();
-    console.log('migrating', url);
-    // TODO show user progress of the migration
+
     setStatus('loading');
-    setTotal(100);
+
+    // 1. Get the list of posts from the API
+    const userProfile = await lookupProfile({ username: url });
+    const appUrl = 'https://app.sigle.io';
+    const bucketUrl = userProfile?.apps?.[appUrl];
+    const data = await fetch(`${bucketUrl}publicStories.json`);
+    const posts = await data.json();
+    // TODO handle status 404
+    setTotal(posts.stories.length);
+
+    // 2. Migrate the posts one by one to the new format
+    for (const [index, story] of posts.stories.entries()) {
+      // 3. Save them to ceramic
+      setProcessed(index + 1);
+      // Sleep for 2 seconds to avoid rate limiting
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
   };
 
   if (status === 'loading') {
     return (
       <div className="mt-10 flex flex-col items-center text-center">
         <LoadingSpinner />
-        <Typography className="mt-4" size="sm">
+        <Typography className="mt-6" size="sm">
           Migrating your blog to Sigle. This may take a while...
-          <br />
-          {processed}/{total} posts migrated
         </Typography>
+        {total > 0 && (
+          <Typography className="mt-1" size="sm">
+            {processed}/{total} posts migrated
+          </Typography>
+        )}
       </div>
     );
   }
