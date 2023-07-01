4dd120cbcc7efd99f8c8ab4a5c6ce815b700268f
Léo Pradel <pradel.leo@gmail.com>
2023-07-01 13:15:25 +0200

    update health throttle

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
+  @Throttle(150, 60)
   @Get('/health')
   getHello() {
     return {
