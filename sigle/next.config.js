778ddadd5eca02f56d1f0de457dcfc6595f7e06f
Léo Pradel <pradel.leo@gmail.com>
2023-07-13 11:42:09 +0200

    try

--- sigle/next.config.js
+++ sigle/next.config.js
@@ -79,12 +79,7 @@ const nextConfig = {
   // SWC is wrongly included in the final bundle causing function to be > 50mb
   // https://github.com/vercel/next.js/issues/42641#issuecomment-1615901228
   outputFileTracingExcludes: {
-    '*': [
-      './**/@swc/core-linux-x64-gnu*',
-      './**/@swc/core-linux-x64-musl*',
-      './**/@esbuild*',
-      './**/webpack*',
-    ],
+    '*': ['./**/@swc*', './**/@swc*', './**/@esbuild*', './**/webpack*'],
   },
 };
 
