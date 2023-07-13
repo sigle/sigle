6b016f652452f20dd51a7c788ff1370f8a51fd87
Léo Pradel <pradel.leo@gmail.com>
2023-07-13 12:12:18 +0200

    Update next.config.js

--- sigle/next.config.js
+++ sigle/next.config.js
@@ -1,5 +1,4 @@
 const dotenv = require('dotenv');
-const withPlugins = require('next-compose-plugins');
 const { withSentryConfig } = require('@sentry/nextjs');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
