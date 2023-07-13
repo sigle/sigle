7109d33bea5357b94a0dde493a27d303a7854b71
Léo Pradel <pradel.leo@gmail.com>
2023-07-13 11:43:06 +0200

    external

--- sigle/next.config.js
+++ sigle/next.config.js
@@ -23,7 +23,18 @@ const getVercelPreviewUrl = () => {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  experimental: {
+    // SWC is wrongly included in the final bundle causing function to be > 50mb
+    // https://github.com/vercel/next.js/issues/42641#issuecomment-1615901228
+    outputFileTracingExcludes: {
+      '*': [
+        './**/@swc/core-linux-x64-gnu*',
+        './**/@swc/core-linux-x64-musl*',
+        './**/@esbuild*',
+        './**/webpack*',
+      ],
+    },
+  },
   reactStrictMode: false,
   swcMinify: true,
   env: {
@@ -75,11 +86,6 @@ const nextConfig = {
       '@stacks/wallet-sdk': '@stacks/wallet-sdk/dist/esm',
     };
     return config;
-  },
-  // SWC is wrongly included in the final bundle causing function to be > 50mb
-  // https://github.com/vercel/next.js/issues/42641#issuecomment-1615901228
-  outputFileTracingExcludes: {
-    '*': ['./**/@swc*', './**/@swc*', './**/@esbuild*', './**/webpack*'],
   },
 };
 
