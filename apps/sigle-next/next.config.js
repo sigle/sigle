3a98691c174c3ca3cc581103d495da15466084f0
Léo Pradel <pradel.leo@gmail.com>
2023-07-31 19:02:58 +0200

    chore: fix vercel bundle by excluding swc

--- apps/sigle-next/next.config.js
+++ apps/sigle-next/next.config.js
@@ -16,6 +16,16 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+
+    // SWC is wrongly included in the final bundle causing function to be > 50mb
+    // https://github.com/vercel/next.js/issues/42641#issuecomment-1615901228
+    outputFileTracingExcludes: {
+      '*': [
+        './**/@swc/core-linux-x64-gnu*',
+        './**/@swc/core-linux-x64-musl*',
+        './**/@esbuild/linux-x64*',
+      ],
+    },
   },
   pageExtensions: [
     // `.page.tsx` for page components
