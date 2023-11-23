89747eee50ce49716c809183c9a4c90c7d85b021
Léo Pradel <pradel.leo@gmail.com>
2023-11-23 11:18:42 +0100

    lint

--- sigle/src/__generated__/sigle-api/sigleApiContext.ts
+++ sigle/src/__generated__/sigle-api/sigleApiContext.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import type { QueryKey, UseQueryOptions } from '@tanstack/react-query';
 import { QueryOperation } from './sigleApiComponents';
 
--- sigle/src/__generated__/sigle-api/sigleApiFetcher.ts
+++ sigle/src/__generated__/sigle-api/sigleApiFetcher.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { sigleConfig } from '@/config';
 import { SigleApiContext } from './sigleApiContext';
 
