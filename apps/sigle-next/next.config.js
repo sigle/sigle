3c3d831c3bfbbd8500d75095041b62b07c33f844
Léo Pradel <pradel.leo@gmail.com>
2023-11-23 19:12:40 +0100

    Update next.config.js

--- apps/sigle-next/next.config.js
+++ apps/sigle-next/next.config.js
@@ -3,6 +3,17 @@ const { withSentryConfig } = require('@sentry/nextjs');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // SWC is wrongly included in the final bundle causing function to be > 50mb
+    // https://github.com/vercel/next.js/issues/42641#issuecomment-1615901228
+    outputFileTracingExcludes: {
+      '*': [
+        './**/@swc/core-linux-x64-gnu*',
+        './**/@swc/core-linux-x64-musl*',
+        './**/@esbuild/linux-x64*',
+      ],
+    },
+  },
   pageExtensions: [
     // `.page.tsx` for page components
     'page.tsx',
