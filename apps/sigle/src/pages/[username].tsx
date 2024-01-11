b1d95fc521daf11b4622d58fbe1cc035dcc0b2ce
Léo Pradel <pradel.leo@gmail.com>
2024-01-11 23:26:35 +0100

    format

--- apps/sigle/src/pages/[username].tsx
+++ apps/sigle/src/pages/[username].tsx
@@ -36,38 +36,56 @@ export const PublicHomePage: NextPage<PublicHomePageProps> = ({
   return <PublicHome file={file!} settings={settings!} userInfo={userInfo!} />;
 };
 
-export const getServerSideProps: GetServerSideProps<
-  PublicHomePageProps
-> = async ({ req, res, params }) => {
-  const username = params?.username as string;
+export const getServerSideProps: GetServerSideProps<PublicHomePageProps> =
+  async ({ req, res, params }) => {
+    const username = params?.username as string;
 
-  // Redirect blog when user move from .id.blocktack to .btc
-  if (redirectUsernameMap[username]) {
-    return {
-      redirect: {
-        statusCode: 301,
-        destination: `/${redirectUsernameMap[username]}`,
-      },
-      props: {},
-    };
-  }
+    // Redirect blog when user move from .id.blocktack to .btc
+    if (redirectUsernameMap[username]) {
+      return {
+        redirect: {
+          statusCode: 301,
+          destination: `/${redirectUsernameMap[username]}`,
+        },
+        props: {},
+      };
+    }
 
-  let file: StoryFile | null = null;
-  let settings: SettingsFile | null = null;
-  let statusCode: boolean | number = false;
-  let errorMessage: string | null = null;
-  let userProfile: undefined | { apps?: Record<string, string> };
-  let nameInfo: BnsGetNameInfoResponse | null = null;
-  try {
-    const stacksNamesApi = new NamesApi();
-    nameInfo = await stacksNamesApi.getNameInfo({ name: username });
-  } catch (error) {
-    // This will happen if there is no Stacks user with this name
-    if (error.status === 404) {
-      statusCode = 404;
-    } else {
+    let file: StoryFile | null = null;
+    let settings: SettingsFile | null = null;
+    let statusCode: boolean | number = false;
+    let errorMessage: string | null = null;
+    let userProfile: undefined | { apps?: Record<string, string> };
+    let nameInfo: BnsGetNameInfoResponse | null = null;
+    try {
+      const stacksNamesApi = new NamesApi();
+      nameInfo = await stacksNamesApi.getNameInfo({ name: username });
+    } catch (error) {
+      // This will happen if there is no Stacks user with this name
+      if (error.status === 404) {
+        statusCode = 404;
+      } else {
+        statusCode = 500;
+        errorMessage = `Failed to fetch name info: ${error.message}`;
+        Sentry.withScope((scope) => {
+          scope.setExtras({
+            username,
+          });
+          Sentry.captureException(error);
+        });
+      }
+    }
+
+    try {
+      if (nameInfo) {
+        userProfile = await resolveZoneFileToProfile(
+          nameInfo.zonefile,
+          nameInfo.address,
+        );
+      }
+    } catch (error) {
       statusCode = 500;
-      errorMessage = `Failed to fetch name info: ${error.message}`;
+      errorMessage = `resolveZoneFileToProfile returned error: ${error.message}`;
       Sentry.withScope((scope) => {
         scope.setExtras({
           username,
@@ -75,73 +93,54 @@ export const getServerSideProps: GetServerSideProps<
         Sentry.captureException(error);
       });
     }
-  }
 
-  try {
-    if (nameInfo) {
-      userProfile = await resolveZoneFileToProfile(
-        nameInfo.zonefile,
-        nameInfo.address,
-      );
-    }
-  } catch (error) {
-    statusCode = 500;
-    errorMessage = `resolveZoneFileToProfile returned error: ${error.message}`;
-    Sentry.withScope((scope) => {
-      scope.setExtras({
-        username,
-      });
-      Sentry.captureException(error);
-    });
-  }
+    // If deployed on vercel we want to get the deployment url to be able to test unmerged pr's
+    // If client side we use window.location.origin
+    let appUrl = req.headers['x-forwarded-host']
+      ? `${req.headers['x-forwarded-proto']}://${req.headers['x-forwarded-host']}`
+      : sigleConfig.appUrl;
 
-  // If deployed on vercel we want to get the deployment url to be able to test unmerged pr's
-  // If client side we use window.location.origin
-  let appUrl = req.headers['x-forwarded-host']
-    ? `${req.headers['x-forwarded-proto']}://${req.headers['x-forwarded-host']}`
-    : sigleConfig.appUrl;
-
-  // If we are in production and the appUrl is localhost we use the one from the config
-  // This is used for the e2e tests
-  if (
-    sigleConfig.env === 'production' &&
-    ['http://localhost:3000', 'http://127.0.0.1:3000'].includes(appUrl)
-  ) {
-    appUrl = sigleConfig.appUrl;
-  }
-
-  const bucketUrl = userProfile?.apps?.[appUrl];
-  // If the user already used the app we try to get the public list
-  if (bucketUrl) {
-    const [dataPublicStories, dataSettings] = await Promise.all([
-      fetchPublicStories(bucketUrl),
-      fetchSettings(bucketUrl),
-    ]);
-
-    file = dataPublicStories.file;
-    if (dataPublicStories.statusCode) {
-      statusCode = dataPublicStories.statusCode;
+    // If we are in production and the appUrl is localhost we use the one from the config
+    // This is used for the e2e tests
+    if (
+      sigleConfig.env === 'production' &&
+      ['http://localhost:3000', 'http://127.0.0.1:3000'].includes(appUrl)
+    ) {
+      appUrl = sigleConfig.appUrl;
     }
 
-    settings = dataSettings.file;
-  } else if (!statusCode) {
-    statusCode = 404;
-  }
+    const bucketUrl = userProfile?.apps?.[appUrl];
+    // If the user already used the app we try to get the public list
+    if (bucketUrl) {
+      const [dataPublicStories, dataSettings] = await Promise.all([
+        fetchPublicStories(bucketUrl),
+        fetchSettings(bucketUrl),
+      ]);
 
-  // If statusCode is not false we set the http response code
-  if (statusCode && res) {
-    res.statusCode = statusCode as number;
-  }
+      file = dataPublicStories.file;
+      if (dataPublicStories.statusCode) {
+        statusCode = dataPublicStories.statusCode;
+      }
 
-  return {
-    props: {
-      statusCode,
-      errorMessage,
-      file,
-      settings,
-      userInfo: nameInfo ? { address: nameInfo.address, username } : null,
-    },
+      settings = dataSettings.file;
+    } else if (!statusCode) {
+      statusCode = 404;
+    }
+
+    // If statusCode is not false we set the http response code
+    if (statusCode && res) {
+      res.statusCode = statusCode as number;
+    }
+
+    return {
+      props: {
+        statusCode,
+        errorMessage,
+        file,
+        settings,
+        userInfo: nameInfo ? { address: nameInfo.address, username } : null,
+      },
+    };
   };
-};
 
 export default PublicHomePage;
