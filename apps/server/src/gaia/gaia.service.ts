ec6563d8d9baa4b06610526e2958ab0f4b9dc7ed
Léo Pradel <pradel.leo@gmail.com>
2024-05-20 21:24:26 +0200

    cache gaia bucket

--- apps/server/src/gaia/gaia.service.ts
+++ apps/server/src/gaia/gaia.service.ts
@@ -1,15 +1,17 @@
 import { StacksService } from '@/stacks/stacks.service';
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class GaiaService {
-  constructor(private readonly stacksService: StacksService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+    private readonly stacksService: StacksService,
+  ) {}
 
   async getUserStories({ username }: { username: string }) {
-    // TODO caching mechanism
-    const { bucketUrl } = await this.stacksService.getBucketUrl({
-      username,
-    });
+    const bucketUrl = await this.getCachedBucketUrl(username);
 
     if (!bucketUrl) {
       return [];
@@ -20,5 +22,28 @@ export class GaiaService {
     });
 
     return publicStoriesFile;
+  }
+
+  private async getCachedBucketUrl(
+    username: string,
+  ): Promise<string | undefined> {
+    const cacheKey = `bucket-url:${username}`;
+
+    const cachedResponse = await this.cacheManager.get<string>(cacheKey);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    const { profile, bucketUrl } = await this.stacksService.getBucketUrl({
+      username,
+    });
+    if (!profile) {
+      return undefined;
+    }
+
+    // Cache response for 1 hour
+    await this.cacheManager.set(cacheKey, bucketUrl, 60 * 60);
+
+    return bucketUrl;
   }
 }
