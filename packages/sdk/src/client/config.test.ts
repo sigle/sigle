a22457da2924cc550221633ddddb1d53fc2cf401
Léo Pradel <pradel.leo@gmail.com>
2024-12-30 11:37:46 +0100

    Update config.test.ts

--- packages/sdk/src/client/config.test.ts
+++ packages/sdk/src/client/config.test.ts
@@ -1,18 +1,18 @@
-import { expect, test } from "vitest";
-import { fixedMintFeeFree, fixedMintFeePaid } from "./config.js";
+import { expect, test } from 'vitest';
+import { fixedMintFeeFree, fixedMintFeePaid } from './config.js';
 
-test("should have free fixed mint fee", () => {
+test('should have free fixed mint fee', () => {
   expect(fixedMintFeeFree.total).toBe(
     fixedMintFeeFree.protocol +
       fixedMintFeeFree.creator +
-      fixedMintFeeFree.mintReferrer
+      fixedMintFeeFree.mintReferrer,
   );
 });
 
-test("should have paid fixed mint fee", () => {
+test('should have paid fixed mint fee', () => {
   expect(fixedMintFeePaid.total).toBe(
     fixedMintFeePaid.protocol +
       fixedMintFeePaid.creator +
-      fixedMintFeePaid.mintReferrer
+      fixedMintFeePaid.mintReferrer,
   );
 });
