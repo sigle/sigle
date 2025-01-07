db7f9cf0ae999e7ec362dd6691629c5a5d80a0d8
Léo Pradel <pradel.leo@gmail.com>
2025-01-07 20:48:20 +0100

    setup sentry

--- apps/sigle-next/next.config.ts
+++ apps/sigle-next/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import { withSentryConfig } from '@sentry/nextjs';
 
 const nextConfig: NextConfig = {
   images: {
@@ -7,4 +8,14 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  telemetry: false,
+  sourcemaps: {
+    disable: true,
+  },
+  // Upload large source maps to Sentry
+  widenClientFileUpload: true,
+  // Disable logger to save bundle size
+  disableLogger: true,
+});
