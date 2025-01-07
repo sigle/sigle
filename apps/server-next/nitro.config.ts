faff331d10e99477db2bb0056ad949762eb4ceb7
Léo Pradel <pradel.leo@gmail.com>
2025-01-07 17:10:24 +0100

    fix build

--- apps/server-next/nitro.config.ts
+++ apps/server-next/nitro.config.ts
@@ -1,11 +1,13 @@
 export default defineNitroConfig({
   srcDir: 'src',
   compatibilityDate: '2024-11-02',
+  esbuild: {
+    options: {
+      target: 'es2022',
+    },
+  },
   experimental: {
     openAPI: true,
     tasks: true,
-  },
-  openAPI: {
-    production: 'prerender',
   },
 });
