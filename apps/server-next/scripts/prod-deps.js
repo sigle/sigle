b0d297da6300c02c7f81129a0b814d1040786154
Léo Pradel <pradel.leo@gmail.com>
2025-01-07 20:17:23 +0100

    chore: server-next run prisma migration on deploy

--- apps/server-next/scripts/prod-deps.js
+++ apps/server-next/scripts/prod-deps.js
@@ -12,6 +12,11 @@ const fs = require('fs').promises;
 
     const clean = {
       ...pj,
+      scripts: {
+        ...pj.scripts,
+        // Remove the nitro prepare script as we already built the server
+        prepare: undefined,
+      },
       dependencies: {
         prisma: pj.dependencies.prisma,
       },
