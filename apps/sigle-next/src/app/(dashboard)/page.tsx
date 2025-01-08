507b4301e4527fbc8bb7b1a171365a4e1f033040
Léo Pradel <pradel.leo@gmail.com>
2025-01-08 18:18:40 +0100

    feat: add simple section homepage

--- apps/sigle-next/src/app/(dashboard)/page.tsx
+++ apps/sigle-next/src/app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@
 
 import { useStacksLogin } from '@/hooks/useStacksLogin';
 import { Routes } from '@/lib/routes';
-import { Button, Callout, Container, Heading } from '@radix-ui/themes';
+import { Button, Callout, Container, Heading, Text } from '@radix-ui/themes';
 import { IconInfoCircle } from '@tabler/icons-react';
 import { useSession } from 'next-auth/react';
 import NextLink from 'next/link';
@@ -45,6 +45,38 @@ export default function Home() {
             on Discord.
           </Callout.Text>
         </Callout.Root>
+      </Container>
+
+      <Container size="4" className="mt-20">
+        <section className="grid grid-cols-1 gap-x-6 gap-y-4 md:grid-cols-3">
+          <div className="flex flex-col items-center text-center gap-4 p-6 bg-white rounded-xl shadow-md">
+            <Heading as="h3" size="4">
+              ⛓️ Write on Bitcoin
+            </Heading>
+            <Text as="p" color="gray" size="3" className="text-balance">
+              {/* Every article lives <Text weight="medium">on-chain</Text>. */}
+              Inscribe your words as Ordinals and make them live forever.
+            </Text>
+          </div>
+          <div className="flex flex-col items-center text-center gap-4 p-6 bg-white rounded-xl shadow-md">
+            <Heading as="h3" size="4">
+              💰 Earn Bitcoin
+            </Heading>
+            <Text as="p" color="gray" size="3" className="text-balance">
+              Turn readers into collectors with direct{' '}
+              <Text weight="medium">sBTC</Text> payments to your wallet.
+            </Text>
+          </div>
+          <div className="flex flex-col items-center text-center gap-4 p-6 bg-white rounded-xl shadow-md">
+            <Heading as="h3" size="4">
+              ⚡ Full Control
+            </Heading>
+            <Text as="p" color="gray" size="3" className="text-balance">
+              Your content, your rules. Build anything with our{' '}
+              <Text weight="medium">API, SDKs, and developer tools</Text>.
+            </Text>
+          </div>
+        </section>
       </Container>
     </div>
   );
