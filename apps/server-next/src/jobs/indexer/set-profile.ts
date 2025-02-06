04cf90d44da10a65bcb50815107aafadd684cc3d
Léo Pradel <pradel.leo@gmail.com>
2025-02-06 10:05:29 +0100

    feat: user profile images blurash (#1082)

--- apps/server-next/src/jobs/indexer/set-profile.ts
+++ apps/server-next/src/jobs/indexer/set-profile.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 import { consola } from '~/lib/consola';
 import { defineJob } from '~/lib/jobs';
 import { prisma } from '~/lib/prisma';
+import { generateImageBlurhashJob } from '../generate-image-blurhash';
 
 export const indexerSetProfileJob = defineJob('indexer-set-profile')
   .input(
@@ -32,24 +33,67 @@ export const indexerSetProfileJob = defineJob('indexer-set-profile')
       ...metadataWithoutId
     } = profileMetadata.data;
 
-    // TODO get blurhash from images cover and picture
-
     await prisma.profile.upsert({
       where: {
         id: job.data.address,
       },
       update: {
         ...metadataWithoutId,
-        pictureUri: profileMetadata.data.picture,
-        coverPictureUri: profileMetadata.data.coverPicture,
       },
       create: {
         ...metadataWithoutId,
         id: job.data.address,
-        pictureUri: profileMetadata.data.picture,
-        coverPictureUri: profileMetadata.data.coverPicture,
       },
     });
+
+    if (profileMetadata.data.picture || profileMetadata.data.coverPicture) {
+      // We do a separate update query here as for some reason prisma doesn't let us update the relationship in the upsert
+      await prisma.profile.update({
+        where: {
+          id: job.data.address,
+        },
+        data: {
+          pictureUri: profileMetadata.data.picture
+            ? {
+                connectOrCreate: {
+                  where: {
+                    id: profileMetadata.data.picture,
+                  },
+                  create: {
+                    id: profileMetadata.data.picture,
+                    mimeType: 'unknown',
+                  },
+                },
+              }
+            : undefined,
+          coverPictureUri: profileMetadata.data.coverPicture
+            ? {
+                connectOrCreate: {
+                  where: {
+                    id: profileMetadata.data.coverPicture,
+                  },
+                  create: {
+                    id: profileMetadata.data.coverPicture,
+                    mimeType: 'unknown',
+                  },
+                },
+              }
+            : undefined,
+        },
+      });
+
+      if (profileMetadata.data.picture) {
+        await generateImageBlurhashJob.emit({
+          imageId: profileMetadata.data.picture,
+        });
+      }
+
+      if (profileMetadata.data.coverPicture) {
+        await generateImageBlurhashJob.emit({
+          imageId: profileMetadata.data.coverPicture,
+        });
+      }
+    }
 
     consola.debug('indexer.set-profile', {
       id: job.data.address,
