70f3983d06f079faf1d4d47ec1b8fe6a876ae75e
Léo Pradel <pradel.leo@gmail.com>
2025-02-06 19:22:44 +0100

    parseSiwsMessage

--- packages/sign-in-with-stacks/src/types.ts
+++ packages/sign-in-with-stacks/src/types.ts
@@ -1,3 +1,15 @@
+/**
+ * @description Combines members of an intersection into a readable type.
+ *
+ * @see {@link https://twitter.com/mattpocockuk/status/1622730173446557697?s=20&t=NdpAcmEFXY01xkqU3KO0Mg}
+ * @example
+ * Prettify<{ a: string } & { b: string } & { c: number, d: bigint }>
+ * => { a: string, b: string, c: number, d: bigint }
+ */
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
 export type ExactPartial<type> = {
   [key in keyof type]?: type[key] | undefined;
 };
