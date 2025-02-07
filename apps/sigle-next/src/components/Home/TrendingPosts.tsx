b51f38fdc72263467bcaf8c433ad5f631991dfe6
Léo Pradel <pradel.leo@gmail.com>
2025-02-07 18:20:08 +0100

    chore: refactor post card

--- apps/sigle-next/src/components/Home/TrendingPosts.tsx
+++ apps/sigle-next/src/components/Home/TrendingPosts.tsx
@@ -23,6 +23,7 @@ import { format } from 'date-fns';
 import Image from 'next/image';
 import { resolveImageUrl } from '@/lib/images';
 import { NextLink } from '../Shared/NextLink';
+import { PostCard } from '../Shared/Post/Card';
 
 export const HomeTrendingPosts = () => {
   const { data: posts } = sigleApiClient.useSuspenseQuery(
@@ -47,72 +48,7 @@ export const HomeTrendingPosts = () => {
         <CarouselContent>
           {posts.map((post) => (
             <CarouselItem key={post.id} className="md:basis-1/2 lg:basis-1/4">
-              <Card size="2">
-                <Inset clip="padding-box" side="top" pb="current">
-                  <NextLink href={Routes.post({ postId: post.id })}>
-                    <div className="overflow-hidden bg-gray-2">
-                      <AspectRatio ratio={16 / 10}>
-                        {post.coverImage ? (
-                          <Image
-                            src={resolveImageUrl(post.coverImage.id)}
-                            alt="Cover card"
-                            className="size-full object-cover"
-                            placeholder={
-                              post.coverImage.blurhash ? 'blur' : 'empty'
-                            }
-                            blurDataURL={post.coverImage.blurhash}
-                            width={post.coverImage.width}
-                            height={post.coverImage.height}
-                          />
-                        ) : null}
-                      </AspectRatio>
-                    </div>
-                  </NextLink>
-                </Inset>
-                <div className="flex-1 space-y-2">
-                  <NextLink
-                    href={Routes.post({ postId: post.id })}
-                    className="block"
-                  >
-                    <Heading
-                      size="4"
-                      className="line-clamp-2 break-words"
-                      style={{
-                        wordBreak: 'break-word',
-                      }}
-                    >
-                      {post.metaTitle || post.title}
-                    </Heading>
-                  </NextLink>
-                  <NextLink
-                    href={Routes.post({ postId: post.id })}
-                    className="block"
-                  >
-                    <Text
-                      as="p"
-                      size="2"
-                      className="line-clamp-1 md:line-clamp-2"
-                    >
-                      {post.metaDescription || post.excerpt}
-                    </Text>
-                  </NextLink>
-                  <div className="mt-3">
-                    <Text as="p" color="gray" size="1">
-                      By{' '}
-                      <Link asChild>
-                        <NextLink
-                          href={Routes.userProfile({ username: post.user.id })}
-                        >
-                          {post.user.profile?.displayName
-                            ? post.user.profile?.displayName
-                            : formatReadableAddress(post.user.id)}
-                        </NextLink>
-                      </Link>{' '}
-                      • {format(new Date(post.createdAt), 'MMM dd, yyyy')}
-                    </Text>
-                  </div>
-                </div>
-              </Card>
+              <PostCard post={post} />
             </CarouselItem>
           ))}
         </CarouselContent>
