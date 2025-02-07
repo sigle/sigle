56b9081b366cd0e5bff51f427e6be87824175c15
Léo Pradel <pradel.leo@gmail.com>
2025-02-07 10:18:50 +0100

    feat: improve homepage features style

--- apps/sigle-next/src/app/(dashboard)/page.tsx
+++ apps/sigle-next/src/app/(dashboard)/page.tsx
@@ -37,9 +37,9 @@ export default function Home() {
         )}
       </Container>
 
-      <Container size="4" className="mt-10 px-4">
+      <Container size="4" className="px-4">
         <section className="grid grid-cols-1 gap-x-6 md:grid-cols-3">
-          <div className="flex flex-col items-center text-center gap-4 p-6 rounded-xl shadow-md">
+          <div className="flex flex-col items-center text-center gap-4 p-6 bg-gray-2 rounded-3">
             <Heading as="h3" size="4">
               ⛓️ Write on-chain
             </Heading>
@@ -48,7 +48,7 @@ export default function Home() {
               Inscribe your words as Ordinals and make them live forever.
             </Text>
           </div>
-          <div className="flex flex-col items-center text-center gap-4 p-6 rounded-xl shadow-md">
+          <div className="flex flex-col items-center text-center gap-4 p-6 bg-gray-2 rounded-3">
             <Heading as="h3" size="4">
               💰 Earn Bitcoin
             </Heading>
@@ -57,7 +57,7 @@ export default function Home() {
               <Text weight="medium">sBTC</Text> payments to your wallet.
             </Text>
           </div>
-          <div className="flex flex-col items-center text-center gap-4 p-6 rounded-xl shadow-md">
+          <div className="flex flex-col items-center text-center gap-4 p-6 bg-gray-2 rounded-3">
             <Heading as="h3" size="4">
               ⚡ Full Control
             </Heading>
