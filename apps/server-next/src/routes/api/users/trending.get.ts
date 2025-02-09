4d615b49c02251515c02aab9f11898250b015e04
Léo Pradel <pradel.leo@gmail.com>
2025-02-09 19:38:58 +0100

    cache

--- apps/server-next/src/routes/api/users/trending.get.ts
+++ apps/server-next/src/routes/api/users/trending.get.ts
@@ -96,42 +96,47 @@ defineRouteMeta({
 
 const NUMBER_OF_USERS = 20;
 
-export default defineEventHandler(async () => {
-  // First get count of all users with posts
-  const totalUsers = await prisma.user.count({
-    where: {
-      posts: {
-        some: {},
-      },
-    },
-  });
-
-  // Calculate random offset
-  const randomSkip = Math.floor(
-    Math.random() * Math.max(0, totalUsers - NUMBER_OF_USERS),
-  );
-
-  const users = await prisma.user.findMany({
-    select: {
-      ...SELECT_PUBLIC_USER_FIELDS,
-      _count: {
-        select: {
-          posts: {},
+export default defineCachedEventHandler(
+  async () => {
+    // First get count of all users with posts
+    const totalUsers = await prisma.user.count({
+      where: {
+        posts: {
+          some: {},
         },
       },
-    },
-    where: {
-      posts: {
-        some: {},
-      },
-    },
-    skip: randomSkip,
-    take: NUMBER_OF_USERS,
-  });
+    });
 
-  return users.map((user) => ({
-    ...user,
-    postsCount: user._count.posts,
-    _count: undefined,
-  }));
-});
+    // Calculate random offset
+    const randomSkip = Math.floor(
+      Math.random() * Math.max(0, totalUsers - NUMBER_OF_USERS),
+    );
+
+    const users = await prisma.user.findMany({
+      select: {
+        ...SELECT_PUBLIC_USER_FIELDS,
+        _count: {
+          select: {
+            posts: {},
+          },
+        },
+      },
+      where: {
+        posts: {
+          some: {},
+        },
+      },
+      skip: randomSkip,
+      take: NUMBER_OF_USERS,
+    });
+
+    return users.map((user) => ({
+      ...user,
+      postsCount: user._count.posts,
+      _count: undefined,
+    }));
+  },
+  {
+    maxAge: 60 * 5, // 5 minutes
+  },
+);
