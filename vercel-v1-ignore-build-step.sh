80a793cd7ceef1e35dafd663d3bf81edc63f3e85
Léo Pradel <pradel.leo@gmail.com>
2025-02-11 09:57:37 +0100

    debug

--- vercel-v1-ignore-build-step.sh
+++ vercel-v1-ignore-build-step.sh
@@ -2,7 +2,7 @@
 
 # In Vercel, ignore the build of the v1 apps if it's not from the v1 branch
 # Get current Git branch name
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=$VERCEL_GIT_COMMIT_REF
 echo "Current branch: $BRANCH"
 
 # Check if branch name starts with "v1-" or is exactly "v1"
