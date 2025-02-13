cdec52c6500729644fbae19ddc4eb1e844461ce1
Léo Pradel <pradel.leo@gmail.com>
2025-02-13 20:26:13 +0100

    index posts

---
+++ apps/server-next/src/jobs/sync-meilisearch.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+import { consola } from "~/lib/consola";
+import { defineJob } from "~/lib/jobs";
+import { meilisearchClient } from "~/lib/meilisearch";
+import { prisma } from "~/lib/prisma";
+
+const BATCH_SIZE = 200;
+
+export const syncMeilisearchJob = defineJob("sync-meilisearch")
+  .input(z.object({}))
+  .options({})
+  .work(async () => {
+    consola.debug("Starting Meilisearch sync...");
+
+    // Sync Posts
+    let skip = 0;
+    while (true) {
+      const posts = await prisma.post.findMany({
+        take: BATCH_SIZE,
+        skip,
+        select: {
+          id: true,
+          title: true,
+          excerpt: true,
+          createdAt: true,
+          userId: true,
+        },
+      });
+
+      if (posts.length === 0) break;
+
+      const formattedPosts = posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+        excerpt: post.excerpt,
+        createdAt: post.createdAt,
+        userId: post.userId,
+      }));
+
+      await meilisearchClient.index("posts").addDocuments(formattedPosts);
+      consola.debug(`Synced ${posts.length} posts (offset: ${skip})`);
+
+      skip += BATCH_SIZE;
+    }
+
+    consola.debug("sync-meilisearch", {
+      nbUsers: await prisma.user.count(),
+      nbPosts: await prisma.post.count(),
+    });
+  });
---
+++ apps/server-next/src/lib/meilisearch.ts
@@ -0,0 +1,6 @@
+import { MeiliSearch } from "meilisearch";
+
+export const meilisearchClient = new MeiliSearch({
+  host: "http://127.0.0.1:7700",
+  apiKey: "1JmkmCKvLxP0XkyJNoRttzbC62oDWCh4V4CTypSN8kY",
+});
