3fc5bdc41001ec7c353cd2d403e55b44da9a3613
Léo Pradel <pradel.leo@gmail.com>
2025-02-21 18:30:27 +0100

    handle state

--- apps/sigle/src/components/Layout/Search.tsx
+++ apps/sigle/src/components/Layout/Search.tsx
@@ -1,4 +1,7 @@
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
+import { Dialog, TextField, VisuallyHidden } from "@radix-ui/themes";
+import { IconSearch } from "@tabler/icons-react";
+import { useState } from "react";
 import { Configure, Hits, Index, InstantSearch } from "react-instantsearch";
 import {
   CommandDialog,
@@ -17,28 +20,52 @@ const { searchClient } = instantMeiliSearch(
 );
 
 export const Search = () => {
-  return (
-    <CommandDialog open={true}>
-      <InstantSearch indexName="users" searchClient={searchClient as any}>
-        <CustomSearchBox />
+  const [open, setOpen] = useState(false);
 
-        <CommandList className="min-h-[300px]">
-          <CommandEmpty>No results found.</CommandEmpty>
-          <Index indexName="users">
-            <CommandGroup heading="Users">
-              <Configure hitsPerPage={5} />
-              <Hits hitComponent={(props) => <CustomHitsUser {...props} />} />
-            </CommandGroup>
-          </Index>
-          <CommandSeparator />
-          <Index indexName="posts">
-            <CommandGroup heading="Posts">
-              <Configure hitsPerPage={5} />
-              <Hits hitComponent={(props) => <CustomHitsUser {...props} />} />
-            </CommandGroup>
-          </Index>
-        </CommandList>
-      </InstantSearch>
-    </CommandDialog>
+  return (
+    <>
+      <TextField.Root
+        placeholder="Search"
+        variant="soft"
+        color="gray"
+        size="2"
+        className="w-full"
+        onClick={() => setOpen(true)}
+      >
+        <TextField.Slot>
+          <IconSearch height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <VisuallyHidden>
+          <Dialog.Title>Search</Dialog.Title>
+          <Dialog.Description>Search for users and posts</Dialog.Description>
+        </VisuallyHidden>
+        <InstantSearch
+          indexName="users"
+          searchClient={searchClient as any}
+          future={{ preserveSharedStateOnUnmount: true }}
+        >
+          <CustomSearchBox />
+
+          <CommandList className="min-h-[300px]">
+            <CommandEmpty>No results found.</CommandEmpty>
+            <Index indexName="users">
+              <CommandGroup heading="Users">
+                <Configure hitsPerPage={5} />
+                <Hits hitComponent={(props) => <CustomHitsUser {...props} />} />
+              </CommandGroup>
+            </Index>
+            <CommandSeparator />
+            <Index indexName="posts">
+              <CommandGroup heading="Posts">
+                <Configure hitsPerPage={5} />
+                <Hits hitComponent={(props) => <CustomHitsUser {...props} />} />
+              </CommandGroup>
+            </Index>
+          </CommandList>
+        </InstantSearch>
+      </CommandDialog>
+    </>
   );
 };
