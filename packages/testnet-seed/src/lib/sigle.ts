e7b17c16c2670a8c38be87b763faf271a889047f
Léo Pradel <pradel.leo@gmail.com>
2025-02-28 15:59:30 +0100

    Update sigle.ts

--- packages/testnet-seed/src/lib/sigle.ts
+++ packages/testnet-seed/src/lib/sigle.ts
@@ -1,5 +1,5 @@
-import { createClient } from '@sigle/sdk';
-import { STACKS_TESTNET } from '@stacks/network';
+import { createClient } from "@sigle/sdk";
+import { STACKS_TESTNET } from "@stacks/network";
 
 export const sigleClient = createClient({
   network: STACKS_TESTNET,
