31e8a2b2fc1ad574e90b3496f2e3d1487ce65a60
Léo Pradel <pradel.leo@gmail.com>
2025-03-07 09:07:38 +0100

    Update health.get.ts

--- apps/server/src/routes/health.get.ts
+++ apps/server/src/routes/health.get.ts
@@ -1,3 +1,30 @@
+defineRouteMeta({
+  openAPI: {
+    summary: "Health check",
+    description: "Check the health of the server",
+    tags: ["Health"],
+    operationId: "healthCheck",
+    responses: {
+      "200": {
+        description: "OK",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                success: {
+                  type: "boolean",
+                  description: "Whether the server is healthy",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
 export default defineEventHandler(() => {
   return { success: true };
 });
