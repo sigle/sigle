9b9c9ed44a3bdd6c0aa20532f0ef873395febe7f
Léo Pradel <pradel.leo@gmail.com>
2025-03-25 19:24:02 +0100

    try

--- apps/server/src/lib/filebase.ts
+++ apps/server/src/lib/filebase.ts
@@ -1,23 +1,17 @@
-import {
-  PutObjectCommand,
-  S3Client,
-  S3ServiceException,
-} from "@aws-sdk/client-s3";
 import { create } from "@web3-storage/w3up-client";
 import type { H3Event } from "h3";
 import { env } from "~/env";
-import { consola } from "./consola";
 
 const w3upClient = await create();
 
-const client = new S3Client({
-  endpoint: "https://s3.filebase.com",
-  region: "us-east-1",
-  credentials: {
-    accessKeyId: env.FILEBASE_ACCESS_KEY,
-    secretAccessKey: env.FILEBASE_SECRET_KEY,
-  },
-});
+// const client = new S3Client({
+//   endpoint: "https://s3.filebase.com",
+//   region: "us-east-1",
+//   credentials: {
+//     accessKeyId: env.FILEBASE_ACCESS_KEY,
+//     secretAccessKey: env.FILEBASE_SECRET_KEY,
+//   },
+// });
 
 export const ipfsUploadFile = async (
   event: H3Event,
@@ -29,35 +23,10 @@ export const ipfsUploadFile = async (
     content: Buffer;
   },
 ) => {
-  let cid: string | undefined;
-  const command = new PutObjectCommand({
-    Bucket: env.FILEBASE_BUCKET,
-    Key: path,
-    Body: content,
-  });
-
-  command.middlewareStack.add(
-    (next) => async (args) => {
-      // Check if request is incoming as middleware works both ways
-      const data = await next(args);
-      const response: {
-        statusCode?: number;
-        headers?: Record<string, string>;
-      } = data.response as any;
-      if (!response.statusCode) return data;
-
-      // Get cid from headers
-      cid = response.headers?.["x-amz-meta-cid"];
-      return data;
-    },
-    {
-      step: "build",
-      name: "addCidToOutput",
-    },
-  );
-
   try {
-    await client.send(command);
+    const cid = (await w3upClient.uploadFile(new Blob([content]))).toString();
+
+    console.log("cid", cid);
 
     if (!cid) {
       throw createError({
@@ -68,11 +37,6 @@ export const ipfsUploadFile = async (
 
     return { cid };
   } catch (error) {
-    if (error instanceof S3ServiceException) {
-      consola.error(
-        `Error from S3 while uploading object. ${error.name}: ${error.message}`,
-      );
-    }
     const sentryId = event.context.$sentry.captureException(error, {
       extra: {
         path,
