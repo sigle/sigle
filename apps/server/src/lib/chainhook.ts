30653b57df1ed4644df3cc1f87c73beab1c49bfb
Léo Pradel <pradel.leo@gmail.com>
2025-03-25 18:34:35 +0100

    fix

--- apps/server/src/lib/chainhook.ts
+++ apps/server/src/lib/chainhook.ts
@@ -48,12 +48,10 @@ export const preparePredicate = (predicate: Predicate) => {
   const predicateBody = predicate.networks[env.STACKS_ENV];
   if (predicateBody && "http_post" in predicateBody.then_that) {
     predicateBody.then_that.http_post.url =
-      env.NODE_ENV === "development"
-        ? env.WEBHOOK_PROXY_URL!
-        : predicateBody.then_that.http_post.url.replace(
-            "{__BASE_URL__}",
-            env.API_URL,
-          );
+      predicateBody.then_that.http_post.url.replace(
+        "{__BASE_URL__}",
+        env.NODE_ENV === "development" ? env.WEBHOOK_PROXY_URL! : env.API_URL,
+      );
 
     predicateBody.then_that.http_post.authorization_header =
       predicateBody.then_that.http_post.authorization_header.replace(
