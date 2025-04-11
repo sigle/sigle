525e04fd7d77a5b7c8205d84719f25d2afd2d855
Léo Pradel <pradel.leo@gmail.com>
2025-04-11 17:25:14 +0200

    Update auth-hooks.ts

--- apps/sigle/src/lib/auth-hooks.ts
+++ apps/sigle/src/lib/auth-hooks.ts
@@ -1,6 +1,10 @@
 import { authClient } from "@/lib/auth-client";
 import { createAuthHooks } from "@daveyplate/better-auth-tanstack";
 
+/**
+ * Using better-auth useSession is causing hydration errors on the homepage.
+ * This plugin is the only way I found to fix those problems so far.
+ */
 const authHooks = createAuthHooks(authClient);
 
 export const {
