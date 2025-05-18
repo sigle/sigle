85fb1da3a8abd7acc0fe820e43e6e286cd604826
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 10:30:12 +0200

    add

--- apps/custom-domain/tailwind.config.ts
+++ apps/custom-domain/tailwind.config.ts
@@ -26,7 +26,7 @@ const config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
