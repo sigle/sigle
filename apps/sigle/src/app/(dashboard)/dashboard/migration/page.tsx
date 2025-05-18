7be8d50b55963caffcacf56cab0b0a942873b4d6
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 16:39:58 +0200

    list

--- apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
+++ apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
@@ -1,35 +1,41 @@
 "use client";
 
-import { useSession } from "@/lib/auth-hooks";
-import { Heading } from "@radix-ui/themes";
+import { Heading, Spinner, Text } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 export default function MigrationPage() {
-  const { data: session } = useSession();
   // TODO input for username
   const [username, setUsername] = useState<string>("sigle.btc");
 
-  const fetchPosts = async () => {
+  const fetchPosts = async (): Promise<{ id: string; title: string }[]> => {
     const res = await fetch(`/api/migration/list?username=${username}`);
     const data = await res.json();
     return data;
   };
 
-  const { data } = useQuery({
+  const { data: posts, isLoading } = useQuery({
     queryKey: ["migration"],
     queryFn: fetchPosts,
   });
 
-  if (!session) {
-    return null;
-  }
-
-  console.log(data);
-
   return (
-    <div className="space-y-8 py-10">
+    <div className="py-10">
       <Heading>Migration</Heading>
+      <Text as="p" color="gray" size="2" className="mt-2">
+        List of posts that you can migrate for {username}
+      </Text>
+
+      <div className="mt-5">
+        {isLoading ? <Spinner /> : null}
+        {posts?.map((post) => (
+          <div key={post.id} className="mt-4">
+            <Heading size="2" title={post.id}>
+              {post.title}
+            </Heading>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
