726c10c38fba7a33c75165444fb3597ef7752369
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 10:07:33 +0200

    Update not-found.tsx

--- apps/custom-domain/src/app/not-found.tsx
+++ apps/custom-domain/src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="container py-16">
       <div className="flex flex-col justify-center text-center">
-        <h2 className="text-5xl	font-bold">404 Not Found</h2>
+        <h2 className="text-5xl font-bold">404 Not Found</h2>
         <p className="mt-2">Sorry, but this page doesn’t exist.</p>
         <Link
           className="mx-auto mt-5 flex items-center justify-center rounded-lg bg-gray-900 px-4 py-3 text-white"
