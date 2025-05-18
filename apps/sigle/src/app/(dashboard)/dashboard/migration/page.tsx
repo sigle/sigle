e10e68151fba85e5b9300b1cd521db375882a86d
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 16:34:12 +0200

    function api

--- apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
+++ apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
@@ -2,23 +2,30 @@
 
 import { useSession } from "@/lib/auth-hooks";
 import { Heading } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 export default function MigrationPage() {
   const { data: session } = useSession();
+  // TODO input for username
   const [username, setUsername] = useState<string>("sigle.btc");
 
   const fetchPosts = async () => {
-    const res = await fetch(
-      `https://api.sigle.io/api/gaia/${username}/stories`,
-    );
+    const res = await fetch(`/api/migration/list?username=${username}`);
     const data = await res.json();
-    console.log(data);
+    return data;
   };
+
+  const { data } = useQuery({
+    queryKey: ["migration"],
+    queryFn: fetchPosts,
+  });
 
   if (!session) {
     return null;
   }
+
+  console.log(data);
 
   return (
     <div className="space-y-8 py-10">
