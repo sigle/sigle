0c6dd5ebc727cd8a7e494821fcdb71c2b4f39561
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 10:24:47 +0200

    fix sitemap

--- apps/custom-domain/src/app/s/[domain]/sitemap.ts
+++ apps/custom-domain/src/app/s/[domain]/sitemap.ts
@@ -1,15 +1,23 @@
 import { resolveImageUrl } from "@/lib/images";
 import { sigleApiFetchClient } from "@/lib/sigle";
-import { sites } from "@/sites";
 import type { MetadataRoute } from "next";
 import { headers } from "next/headers";
+import { notFound } from "next/navigation";
 
 export default async function sitemap() {
   // next.js sitemap function doesn't pass the params, so we use headers as a workaround
   const headersList = await headers();
   const domain = headersList.get("host") || "";
-  const site = sites[domain];
-  const { origin } = new URL(`https://${domain}`);
+  const { data: site } = await sigleApiFetchClient.GET("/api/sites/{domain}", {
+    params: {
+      path: {
+        domain,
+      },
+    },
+  });
+  if (!site) {
+    notFound();
+  }
 
   const { data: posts } = await sigleApiFetchClient.GET("/api/posts/list", {
     params: {
@@ -23,13 +31,13 @@ export default async function sitemap() {
   const sitemap: MetadataRoute.Sitemap = [
     // Static pages
     {
-      url: origin,
+      url: site.url,
       lastModified: new Date().toISOString(),
     },
 
     // Post dynamic pages
     ...(posts?.results.map((post) => ({
-      url: `${origin}/posts/${post.id}`,
+      url: `${site.url}/posts/${post.id}`,
       lastModified: new Date(post.updatedAt).toISOString(),
       images: post.coverImage ? [resolveImageUrl(post.coverImage.id)] : [],
     })) || []),
