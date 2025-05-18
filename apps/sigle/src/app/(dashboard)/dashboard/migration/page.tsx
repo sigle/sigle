cd4e35b30dbd6c18db5fc7aff21c35892d29d4f7
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 16:46:36 +0200

    Update page.tsx

--- apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
+++ apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { Heading, Separator, Spinner, Text } from "@radix-ui/themes";
+import { Button, Heading, Separator, Spinner, Text } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
 import { useEffect, useState } from "react";
 
 export default function MigrationPage() {
   // TODO input for username
   const [username, setUsername] = useState<string>("sigle.btc");
 
-  const fetchPosts = async (): Promise<{ id: string; title: string }[]> => {
+  const fetchPosts = async (): Promise<
+    { id: string; title: string; createdAt: number }[]
+  > => {
     const res = await fetch(`/api/migration/list?username=${username}`);
     const data = await res.json();
     return data;
@@ -18,6 +21,8 @@ export default function MigrationPage() {
     queryKey: ["migration"],
     queryFn: fetchPosts,
   });
+
+  console.log(posts);
 
   return (
     <div className="py-10">
@@ -30,10 +35,18 @@ export default function MigrationPage() {
         {isLoading ? <Spinner /> : null}
         {posts?.map((post) => (
           <div key={post.id}>
-            <div className="py-4">
-              <Heading size="2" weight="medium" title={post.id}>
-                {post.title}
-              </Heading>
+            <div className="py-4 flex items-center justify-between">
+              <div>
+                <Heading size="2" weight="medium" title={post.id}>
+                  {post.title}
+                </Heading>
+                <Text as="p" color="gray" size="2" className="mt-2">
+                  {format(post.createdAt, "MMM dd yyy")}
+                </Text>
+              </div>
+              <Button color="gray" variant="outline" highContrast>
+                Migrate
+              </Button>
             </div>
             <Separator size="4" />
           </div>
