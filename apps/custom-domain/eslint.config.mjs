0a275755bed5bbaf0652333f423e578e8c30a41b
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 10:31:49 +0200

    add eslint tailwind plugin

--- apps/custom-domain/eslint.config.mjs
+++ apps/custom-domain/eslint.config.mjs
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended",
+  ),
+  {
+    settings: {
+      tailwindcss: {
+        callees: ["classnames", "clsx", "ctl", "cva", "tv", "cn"],
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
