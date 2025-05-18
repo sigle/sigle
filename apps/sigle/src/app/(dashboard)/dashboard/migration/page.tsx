1e6621001a6fb5516b9226a76e109fe1fcba140d
Léo Pradel <pradel.leo@gmail.com>
2025-05-18 16:53:11 +0200

    cleanup

--- apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
+++ apps/sigle/src/app/(dashboard)/dashboard/migration/page.tsx
@@ -1,25 +1,42 @@
 "use client";
 
-import { Button, Heading, Separator, Spinner, Text } from "@radix-ui/themes";
+import {
+  Button,
+  Heading,
+  Separator,
+  Spinner,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function MigrationPage() {
-  // TODO input for username
-  const [username, setUsername] = useState<string>("sigle.btc");
+  const [username, setUsername] = useState<{ value: string; ready: boolean }>({
+    value: "",
+    ready: false,
+  });
 
   const fetchPosts = async (): Promise<
     { id: string; title: string; createdAt: number }[]
   > => {
-    const res = await fetch(`/api/migration/list?username=${username}`);
+    const res = await fetch(`/api/migration/list?username=${username.value}`);
     const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data.message);
+    }
     return data;
   };
 
-  const { data: posts, isLoading } = useQuery({
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["migration"],
     queryFn: fetchPosts,
+    enabled: !!username.ready,
   });
 
   console.log(posts);
@@ -31,8 +48,40 @@ export default function MigrationPage() {
         One click migration for your old posts
       </Text>
 
+      {!username.ready ? (
+        <div className="mt-5">
+          <TextField.Root
+            type="text"
+            size="2"
+            placeholder="Enter your username (.btc, .id.stx etc...)"
+            value={username.value}
+            onChange={(e) =>
+              setUsername({
+                value: e.target.value,
+                ready: false,
+              })
+            }
+          />
+          <Button
+            color="gray"
+            variant="outline"
+            highContrast
+            className="mt-4"
+            onClick={() => setUsername({ value: username.value, ready: true })}
+          >
+            Get posts
+          </Button>
+        </div>
+      ) : null}
+
       <div className="mt-5">
         {isLoading ? <Spinner /> : null}
+        {error ? <Text color="red">{error.message}</Text> : null}
+        {posts?.length === 0 ? (
+          <Text color="gray" size="2" className="mt-2">
+            No posts found
+          </Text>
+        ) : null}
         {posts?.map((post) => (
           <div key={post.id}>
             <div className="py-4 flex items-center justify-between">
