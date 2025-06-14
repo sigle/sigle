c3df105e8eb2a3080cb612d875eb38ea08299287
Léo Pradel <pradel.leo@gmail.com>
2025-06-14 18:11:55 +0200

    fix

--- apps/sigle/eslint.config.mjs
+++ apps/sigle/eslint.config.mjs
@@ -11,11 +11,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:tailwindcss/recommended",
-  ),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     settings: {
       "better-tailwindcss": {
