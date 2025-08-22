1bfc1ffe4a934b9bb257055e3da630ba68bd20a5
Léo Pradel <pradel.leo@gmail.com>
2025-08-22 17:17:32 +0200

    server logic

--- apps/server/src/lib/users.ts
+++ apps/server/src/lib/users.ts
@@ -1,4 +1,10 @@
+import { env } from "~/env";
+
+const addresses =
+  env.STACKS_ENV === "mainnet"
+    ? []
+    : ["ST1MPBXQMVM63FSWM55Z2EQP1NKHB3P0RP13APXCP"];
+
 export const isUserWhitelisted = (address: string) => {
-  // TODO logic
-  return false;
+  return addresses.includes(address);
 };
