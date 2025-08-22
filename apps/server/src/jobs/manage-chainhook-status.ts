5c14f14453e17aa47a067feac500913aa0229192
Léo Pradel <pradel.leo@gmail.com>
2025-08-22 16:36:18 +0200

    feat: retry chainhooks when they error

---
+++ apps/server/src/jobs/manage-chainhook-status.ts
@@ -0,0 +1,42 @@
+import {
+  getChainhookStatus,
+  getChainhooks,
+  predicatePrefix,
+} from "~/lib/chainhook";
+import { consola } from "~/lib/consola";
+import { defineJob } from "~/lib/jobs";
+
+export const manageChainhookStatusJob = defineJob("manage-chainhook-status")
+  .options({
+    priority: 1,
+    retryLimit: 2,
+    retryDelay: 60000,
+  })
+  .work(async () => {
+    const chainhooks = await getChainhooks();
+
+    for (const chainhook of chainhooks) {
+      if (!chainhook.name.startsWith(predicatePrefix)) {
+        continue;
+      }
+      const chainhookStatus = await getChainhookStatus(chainhook.uuid);
+      if (chainhookStatus.status === "error") {
+        console.error(
+          "Chainhook API error",
+          chainhook.uuid,
+          chainhookStatus.message,
+        );
+      } else if (chainhookStatus.status.type === "interrupted") {
+        console.error(
+          "Chainhook interrupted",
+          chainhook.uuid,
+          chainhookStatus.status.info,
+        );
+      }
+      console.log(chainhookStatus);
+    }
+
+    consola.debug("manage-chainhook-status", {
+      chainhooks: chainhooks.length,
+    });
+  });
--- apps/server/src/plugins/jobs.ts
+++ apps/server/src/plugins/jobs.ts
@@ -3,12 +3,14 @@ import PgBoss from "pg-boss";
 import { env } from "~/env";
 import { generateImageBlurhashJob } from "~/jobs/generate-image-blurhash";
 import { indexerJob } from "~/jobs/indexer";
+import { manageChainhookStatusJob } from "~/jobs/manage-chainhook-status";
 import { JobManager } from "~/lib/jobs";
 
 export default defineNitroPlugin(async () => {
   const boss = new PgBoss(env.DATABASE_URL);
   const jobs = new JobManager(boss)
     .register(indexerJob)
-    .register(generateImageBlurhashJob);
+    .register(generateImageBlurhashJob)
+    .register(manageChainhookStatusJob);
   await jobs.start();
 });
