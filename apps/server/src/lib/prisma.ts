1834e887bcca16b819b7d38f6fe0f8b1b6472014
Léo Pradel <pradel.leo@gmail.com>
2025-08-22 12:38:55 +0200

    chore: up prisma 6.14 (#1188)

--- apps/server/src/lib/prisma.ts
+++ apps/server/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "~/__generated__/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
