c8a07dd9e987f377621019b086b3bc290b603e82
Léo Pradel <pradel.leo@gmail.com>
2026-01-23 13:25:04 +0100

    TODO

--- apps/sigle/eslint.config.mjs
+++ apps/sigle/eslint.config.mjs
@@ -1,28 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+// TODO oxlint react plugin
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   {
-    settings: {
-      "better-tailwindcss": {
-        entryPoint: "src/app/globals.css",
-      },
-    },
-    plugins: {
-      "better-tailwindcss": eslintPluginBetterTailwindcss,
-    },
     rules: {
-      ...eslintPluginBetterTailwindcss.configs["recommended-error"].rules,
-      "better-tailwindcss/enforce-consistent-line-wrapping": "off",
-
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
       "react-hooks/set-state-in-effect": "off",
       "react-hooks/use-memo": "off",
