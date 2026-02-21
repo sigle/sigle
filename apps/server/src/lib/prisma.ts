b034add9ee5f71b9cf8940466640ed454621920b
Léo Pradel <pradel.leo@gmail.com>
2026-02-21 15:28:32 +0100

    up prisma

--- apps/server/src/lib/prisma.ts
+++ apps/server/src/lib/prisma.ts
@@ -1,8 +1,12 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "~/__generated__/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
