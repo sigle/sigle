ab8f116d1d7970aec3d7a9522d42efcffe44fa3a
Léo Pradel <pradel.leo@gmail.com>
2026-02-21 12:19:23 +0100

    fix

---
+++ apps/server/prisma/migrations/20260221111916_init/migration.sql
@@ -0,0 +1,242 @@
+-- CreateEnum
+CREATE TYPE "public"."UserFlag" AS ENUM ('NONE', 'VERIFIED');
+
+-- CreateTable
+CREATE TABLE "public"."user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "email_verified" BOOLEAN NOT NULL DEFAULT false,
+    "image" TEXT,
+    "last_login_at" TIMESTAMP(3),
+    "flag" "public"."UserFlag" NOT NULL DEFAULT 'NONE',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."session" (
+    "id" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "token" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "ip_address" TEXT,
+    "user_agent" TEXT,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."account" (
+    "id" TEXT NOT NULL,
+    "account_id" TEXT NOT NULL,
+    "provider_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "access_token" TEXT,
+    "refresh_token" TEXT,
+    "id_token" TEXT,
+    "access_token_expires_at" TIMESTAMP(3),
+    "refresh_token_expires_at" TIMESTAMP(3),
+    "scope" TEXT,
+    "password" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."verification" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "verification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."walletAddress" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "chain_id" INTEGER NOT NULL,
+    "is_primary" BOOLEAN DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "walletAddress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."rate_limiter_flexible" (
+    "key" TEXT NOT NULL,
+    "points" INTEGER NOT NULL,
+    "expire" TIMESTAMP(3),
+
+    CONSTRAINT "rate_limiter_flexible_pkey" PRIMARY KEY ("key")
+);
+
+-- CreateTable
+CREATE TABLE "public"."profile" (
+    "id" TEXT NOT NULL,
+    "display_name" TEXT,
+    "description" TEXT,
+    "website" TEXT,
+    "twitter" TEXT,
+    "picture_uri_id" TEXT,
+    "cover_picture_uri_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."draft" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "meta_title" TEXT,
+    "meta_description" TEXT,
+    "cover_image" TEXT,
+    "canonical_uri" TEXT,
+    "tx_id" TEXT,
+    "tx_status" TEXT,
+    "collect_price_type" TEXT,
+    "collect_price" BIGINT,
+    "collect_limit_type" TEXT,
+    "collect_limit" INTEGER,
+    "tags" TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "draft_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."post" (
+    "id" TEXT NOT NULL,
+    "version" INTEGER NOT NULL,
+    "address" TEXT NOT NULL,
+    "tx_id" TEXT NOT NULL,
+    "collected" INTEGER NOT NULL,
+    "max_supply" INTEGER NOT NULL,
+    "metadata_uri" TEXT NOT NULL,
+    "enabled" BOOLEAN NOT NULL,
+    "open_edition" BOOLEAN NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "excerpt" TEXT NOT NULL,
+    "meta_title" TEXT,
+    "meta_description" TEXT,
+    "cover_image_id" TEXT,
+    "tags" TEXT[],
+    "canonical_uri" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."minter_fixed_price" (
+    "id" TEXT NOT NULL,
+    "price" BIGINT NOT NULL,
+    "start_block" BIGINT NOT NULL,
+    "end_block" BIGINT NOT NULL,
+
+    CONSTRAINT "minter_fixed_price_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."media_image" (
+    "id" TEXT NOT NULL,
+    "mime_type" TEXT NOT NULL,
+    "alt" TEXT,
+    "width" INTEGER,
+    "height" INTEGER,
+    "size" INTEGER,
+    "blurhash" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "media_image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."post_nft" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "post_id" TEXT NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "minter_id" TEXT NOT NULL,
+
+    CONSTRAINT "post_nft_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "public"."user"("email");
+
+-- CreateIndex
+CREATE INDEX "session_user_id_idx" ON "public"."session"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "session_token_key" ON "public"."session"("token");
+
+-- CreateIndex
+CREATE INDEX "account_user_id_idx" ON "public"."account"("user_id");
+
+-- CreateIndex
+CREATE INDEX "verification_identifier_idx" ON "public"."verification"("identifier");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "post_address_key" ON "public"."post"("address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "post_tx_id_key" ON "public"."post"("tx_id");
+
+-- AddForeignKey
+ALTER TABLE "public"."session" ADD CONSTRAINT "session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."account" ADD CONSTRAINT "account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."walletAddress" ADD CONSTRAINT "walletAddress_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."profile" ADD CONSTRAINT "profile_id_fkey" FOREIGN KEY ("id") REFERENCES "public"."user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."profile" ADD CONSTRAINT "profile_picture_uri_id_fkey" FOREIGN KEY ("picture_uri_id") REFERENCES "public"."media_image"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."profile" ADD CONSTRAINT "profile_cover_picture_uri_id_fkey" FOREIGN KEY ("cover_picture_uri_id") REFERENCES "public"."media_image"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."draft" ADD CONSTRAINT "draft_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."post" ADD CONSTRAINT "post_cover_image_id_fkey" FOREIGN KEY ("cover_image_id") REFERENCES "public"."media_image"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."post" ADD CONSTRAINT "post_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."minter_fixed_price" ADD CONSTRAINT "minter_fixed_price_id_fkey" FOREIGN KEY ("id") REFERENCES "public"."post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."post_nft" ADD CONSTRAINT "post_nft_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "public"."post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."post_nft" ADD CONSTRAINT "post_nft_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "public"."user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."post_nft" ADD CONSTRAINT "post_nft_minter_id_fkey" FOREIGN KEY ("minter_id") REFERENCES "public"."user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
