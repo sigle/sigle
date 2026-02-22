572c2c7acd3fd5097f56e93de0480d58a2cf7767
Léo Pradel <pradel.leo@gmail.com>
2026-02-22 11:52:18 +0100

    client

--- apps/sigle/src/lib/auth-client.ts
+++ apps/sigle/src/lib/auth-client.ts
@@ -1,10 +1,20 @@
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { siwsClient } from "sign-in-with-stacks/plugins/better-auth/client";
 import { env } from "@/env";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_API_URL,
-  plugins: [siwsClient()],
+  plugins: [
+    siwsClient(),
+    inferAdditionalFields({
+      user: {
+        address: {
+          type: "string",
+        },
+      },
+    }),
+  ],
 });
 
 export const { signOut } = authClient;
