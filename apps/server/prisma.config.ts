497029c7ccd14fef5aeb780f38e121b2ec5ce136
Léo Pradel <pradel.leo@gmail.com>
2026-02-24 15:05:30 +0100

    chore: prisma 7

--- apps/server/prisma.config.ts
+++ apps/server/prisma.config.ts
@@ -1,9 +1,12 @@
 import path from "node:path";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
   migrations: {
     path: path.join("prisma", "migrations"),
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
   },
 });
