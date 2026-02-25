2c2ddfe956c1f05daaa1ea74ff7eb88c62913855
Léo Pradel <pradel.leo@gmail.com>
2026-02-25 09:11:46 +0100

    fix

--- packages/contracts-source/src/index.ts
+++ packages/contracts-source/src/index.ts
@@ -1,4 +1,4 @@
-import siglePostV0 from "raw-loader!../../../apps/contracts/contracts/sigle-post-v001.clar";
+import siglePostV0 from "raw-loader!../../../apps/contracts/contracts/WIP/sigle-post-v001.clar";
 
 const contracts = { siglePostV0 };
 
