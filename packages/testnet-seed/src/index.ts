28bac8d06d8ec5786d3346bc479832ac6b6e67e1
Léo Pradel <pradel.leo@gmail.com>
2026-02-26 11:31:44 +0100

    chore: testnet-seed publish-post (#1213)

--- packages/testnet-seed/src/index.ts
+++ packages/testnet-seed/src/index.ts
@@ -1,29 +1,13 @@
-import { createId } from "@paralleldrive/cuid2";
-import { parseBTC } from "@sigle/sdk";
-import { sigleClient } from "./lib/sigle.js";
-import { deployContract } from "./lib/stacks.js";
+import { publishPost } from "./lib/stacks.js";
 
 const main = async () => {
-  await deployContract({
-    contractName: createId(),
-    codeBody: sigleClient.generatePostContract({
-      collectInfo: {
-        amount: 0,
-        maxSupply: 100,
-      },
-      metadata: "ar://M5FWeRhC84c4RjX0EwsgijX7j0Jj0kftM3c33qbaHOg",
-    }).contract,
+  await publishPost({
+    metadataUri: "ar://M5FWeRhC84c4RjX0EwsgijX7j0Jj0kftM3c33qbaHOg",
     accountIndex: 1,
   });
 
-  await deployContract({
-    contractName: createId(),
-    codeBody: sigleClient.generatePostContract({
-      collectInfo: {
-        amount: parseBTC("0.00042"),
-      },
-      metadata: "ar://VrLnBatCVd46e5Iy4ksspzmFUnqnFergrNGB8yx7bsU",
-    }).contract,
+  await publishPost({
+    metadataUri: "ar://VrLnBatCVd46e5Iy4ksspzmFUnqnFergrNGB8yx7bsU",
     accountIndex: 2,
   });
 };
