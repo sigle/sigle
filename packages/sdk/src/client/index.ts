302ec589badcbc380afe4de483c9f67db6657fce
Léo Pradel <pradel.leo@gmail.com>
2026-02-26 18:08:53 +0100

    fix

--- packages/sdk/src/client/index.ts
+++ packages/sdk/src/client/index.ts
@@ -59,7 +59,6 @@ export const createClient = (options: CreateClientOptions) => {
     setProfile: (params: SetProfileParams) =>
       setProfile({
         params,
-        network: options.network,
         networkName,
       }),
   };
