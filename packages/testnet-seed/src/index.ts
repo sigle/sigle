165bb4f10d02bf00b751fe41314f2ae6d5080f08
Léo Pradel <pradel.leo@gmail.com>
2026-03-04 12:57:49 +0100

    chore: testnet seed await txs (#1220)

--- packages/testnet-seed/src/index.ts
+++ packages/testnet-seed/src/index.ts
@@ -6,8 +6,6 @@ const main = async () => {
     accountIndex: 1,
   });
 
-  // TODO await that the block is mined before publishing the next one
-
   await publishPost({
     metadataUri: "ar://VrLnBatCVd46e5Iy4ksspzmFUnqnFergrNGB8yx7bsU",
     accountIndex: 2,
