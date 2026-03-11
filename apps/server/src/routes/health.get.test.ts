9fd22ade6dd11b221ea5701a910567a082300db8
Léo Pradel <pradel.leo@gmail.com>
2026-03-11 12:34:19 +0100

    Update health.get.test.ts

--- apps/server/src/routes/health.get.test.ts
+++ apps/server/src/routes/health.get.test.ts
@@ -1,16 +1,19 @@
-import { createError, defineEventHandler } from "h3";
+import type { H3Event } from "h3";
 import { describe, expect, it, vi } from "vitest";
 
-vi.mock("nitropack/runtime", () => ({
-  defineRouteMeta: vi.fn(),
-}));
+vi.mock<typeof import("nitropack/runtime")>(
+  import("nitropack/runtime"),
+  () => ({
+    defineRouteMeta: vi.fn(),
+  }),
+);
 
 const { default: handler } = await import("./health.get");
 
 describe("health.get", () => {
   it("returns success true", async () => {
-    const result = await handler({} as any);
+    const result = await handler({} as H3Event);
 
-    expect(result).toEqual({ success: true });
+    expect(result).toStrictEqual({ success: true });
   });
 });
