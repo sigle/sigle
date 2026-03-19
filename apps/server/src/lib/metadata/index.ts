60f4135232a73940052b7ad533ca1a9513f6c1f2
Léo Pradel <pradel.leo@gmail.com>
2026-03-19 20:44:10 +0100

    getProfileMetadataFromUri

--- apps/server/src/lib/metadata/index.ts
+++ apps/server/src/lib/metadata/index.ts
@@ -1,4 +1,9 @@
-import { PostMetadataSchema, type MediaImageMetadata } from "@sigle/sdk";
+import {
+  PostMetadataSchema,
+  ProfileMetadataSchema,
+  type MediaImageMetadata,
+  type ProfileMetadata,
+} from "@sigle/sdk";
 import { Result, type UnhandledException } from "better-result";
 import { resolveImageUrl } from "../images";
 import { InvalidMetadataError, type MetadataFetchFailedError } from "./errors";
@@ -72,4 +77,32 @@ export async function getMetadataFromUri(
   };
 
   return Result.ok(metadata);
+}
+
+export async function getProfileMetadataFromUri(
+  baseTokenUri: string,
+): Promise<
+  Result<
+    ProfileMetadata,
+    MetadataFetchFailedError | InvalidMetadataError | UnhandledException
+  >
+> {
+  const url = resolveImageUrl(baseTokenUri);
+  const fetchResult = await fetchMetadata(url);
+
+  if (fetchResult.isErr()) {
+    return fetchResult;
+  }
+
+  const profileMetadata = ProfileMetadataSchema.safeParse(fetchResult.value);
+  if (!profileMetadata.success) {
+    return Result.err(
+      new InvalidMetadataError({
+        error: `Invalid metadata: ${profileMetadata.error.issues.length} validation error(s)`,
+      }),
+    );
+  }
+  const postData = profileMetadata.data;
+
+  return Result.ok(postData);
 }
