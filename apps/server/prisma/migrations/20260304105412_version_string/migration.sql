cedbd365fb576d4ac935f86d932de17ec6627fb9
Léo Pradel <pradel.leo@gmail.com>
2026-03-28 18:13:39 +0100

    feat: postgres v18 (#1246)

--- apps/server/prisma/migrations/20260304105412_version_string/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "post" ALTER COLUMN "version" SET DATA TYPE TEXT;
--- apps/server/src/lib/ipfs.ts
+++ apps/server/src/lib/ipfs.ts
@@ -4,7 +4,7 @@ import { sha256 } from "multiformats/hashes/sha2";
 import { parse } from "multiformats/link";
 
 export const createCIDv1FromBuffer = async (buffer: Buffer) => {
-  const hash = await sha256.digest(buffer as unknown as Uint8Array);
+  const hash = await sha256.digest(buffer);
   const cidv1 = CID.create(1, code, hash);
   return cidv1.toString();
 };
