d00ae4f25e76de96f91cc0e66aaab45fa62bc4e3
Léo Pradel <pradel.leo@gmail.com>
2026-03-28 10:24:41 +0100

    Update MultiStepToast.tsx

--- apps/sigle/src/components/Shared/MultiStepToast/MultiStepToast.tsx
+++ apps/sigle/src/components/Shared/MultiStepToast/MultiStepToast.tsx
@@ -18,44 +18,42 @@ interface MultiStepToastProps {
 
 export const MultiStepToast = ({ steps }: MultiStepToastProps) => {
   return (
-    <div>
-      <div className="flex flex-col gap-2">
-        {steps.map((step) => {
-          return (
-            <div key={step.id}>
-              <div className="flex items-center gap-2">
-                {step.status === "pending" ? (
-                  <Spinner className="size-4" />
-                ) : (
-                  <div className="flex size-4 items-center justify-center rounded-full">
-                    {step.status === "idle" && (
-                      <IconCircle size={16} className="text-muted-foreground" />
-                    )}
-                    {step.status === "success" && <IconCircleCheck size={16} />}
-                    {step.status === "error" && (
-                      <IconAlertCircle size={16} className="text-destructive" />
-                    )}
-                  </div>
-                )}
-                <p
-                  className={cn("text-sm", {
-                    "text-muted-foreground": step.status === "idle",
-                    "font-medium": step.status === "pending",
-                    "text-destructive": step.status === "error",
-                  })}
-                >
-                  {step.title}
-                </p>
-              </div>
-              {step.status === "error" && step.errorMessage ? (
-                <p className="ml-6 text-xs text-destructive">
-                  {step.errorMessage}
-                </p>
-              ) : null}
+    <div className="flex flex-col gap-2">
+      {steps.map((step) => {
+        return (
+          <div key={step.id}>
+            <div className="flex items-center gap-2">
+              {step.status === "pending" ? (
+                <Spinner className="size-4" />
+              ) : (
+                <div className="flex size-4 items-center justify-center rounded-full">
+                  {step.status === "idle" && (
+                    <IconCircle size={16} className="text-muted-foreground" />
+                  )}
+                  {step.status === "success" && <IconCircleCheck size={16} />}
+                  {step.status === "error" && (
+                    <IconAlertCircle size={16} className="text-destructive" />
+                  )}
+                </div>
+              )}
+              <p
+                className={cn("text-sm", {
+                  "text-muted-foreground": step.status === "idle",
+                  "font-medium": step.status === "pending",
+                  "text-destructive": step.status === "error",
+                })}
+              >
+                {step.title}
+              </p>
             </div>
-          );
-        })}
-      </div>
+            {step.status === "error" && step.errorMessage ? (
+              <p className="ml-6 text-xs text-destructive">
+                {step.errorMessage}
+              </p>
+            ) : null}
+          </div>
+        );
+      })}
     </div>
   );
 };
