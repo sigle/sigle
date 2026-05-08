c0d07355b63b85d3884ff793f0135464a1ac2faa
Léo Pradel <pradel.leo@gmail.com>
2026-05-08 16:15:10 +0200

    Update ipfs.ts

--- apps/server/src/lib/ipfs.ts
+++ apps/server/src/lib/ipfs.ts
@@ -1,7 +1,6 @@
 import { CID } from "multiformats/cid";
 import { code } from "multiformats/codecs/raw";
 import { sha256 } from "multiformats/hashes/sha2";
-import { parse } from "multiformats/link";
 
 export const createCIDv1FromBuffer = async (buffer: Buffer) => {
   const hash = await sha256.digest(buffer);
