f47788bddf3e561d16b12677f9fc2508c7bbaba7
Léo Pradel <pradel.leo@gmail.com>
2026-07-16 18:11:44 +0200

    chore: rspack 2

--- packages/contracts-source-next/src/index.ts
+++ packages/contracts-source-next/src/index.ts
@@ -1,4 +1,4 @@
-import siglePostV0 from "raw-loader!../../../apps/contracts-next/contracts/sigle-post-v001.clar";
+import siglePostV0 from "../../../apps/contracts-next/contracts/sigle-post-v001.clar";
 
 const contracts = { siglePostV0 };
 
