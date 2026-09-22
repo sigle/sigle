8f8b192db8ea722e5fe3e443f1c3fa054fd94911
Léo Pradel <pradel.leo@gmail.com>
2026-09-22 09:33:07 +0200

    feat: migrate docs site to astro starlight

---
+++ .changeset/docs-astro-starlight.md
@@ -0,0 +1,5 @@
+---
+"@sigle/docs": patch
+---
+
+Migrate the docs site from Next.js/Nextra to Astro Starlight.
--- apps/docs/src/content/build-on-sigle/_meta.js
+++
@@ -1,6 +0,0 @@
-export default {
-  intro: "Introduction",
-  sdk: "SDK",
-  api: "API",
-  testnet: "Testnet",
-};
